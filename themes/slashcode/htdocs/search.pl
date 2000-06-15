496013aeef248bffd83f74e798b86484020dcca7
Cliff Wood <clifton.wood@gmail.com>
2000-06-15 21:12:18 +0000

    NUMEROUS fixes and slight improvements to search.pl
    tracks internal commit 5d8af786daf1e6f1a1f5d4ecb803997b41b4fb8b

--- themes/slashcode/htdocs/search.pl
+++ themes/slashcode/htdocs/search.pl
@@ -42,17 +42,27 @@ sub main {
 	$I{F}{'last'}	||= $I{F}{min} + $I{F}{max};
 
 	# get rid of bad characters
-	$I{F}{query} =~ s/[^A-Z0-9'. ]//gi;
+	$I{F}{query} =~ s/[^A-Z0-9\'. ]//gi;
+
+	countSearchHits($I{F}{op} || 'stories') if ! $I{F}{hitcount};
+	my $hitcount = "($I{F}{hitcount} total matches)" if $I{F}{hitcount};
 
 	header("$I{sitename}: Search $I{F}{query}", $I{F}{section});
-	titlebar("99%", "Searching $I{F}{query}");
+	titlebar("99%", "Searching $I{F}{query} $hitcount");
 
 	searchForm();
 
-	if	($I{F}{op} eq "comments")	{ commentSearch()	}
-	elsif	($I{F}{op} eq "users")		{ userSearch()		}
-	else					{ storySearch()		}
-	writelog("search", $I{F}{query});
+	if ($I{F}{query} && ! $I{F}{hitcount}) {
+		print "No Matches Found for your query.";
+	} else {
+		if		($I{F}{op} eq "comments")	{ commentSearch()	}
+		elsif	($I{F}{op} eq "users")		{ userSearch()		}
+		elsif	($I{F}{op} eq "stories")	{ storySearch()		}
+		else	{
+			print "Invalid operation!<BR>";
+		}
+		writelog("search", $I{F}{query}) if $I{F}{op} =~ /^(comments|stories|users)$/;
+	}
 	footer();	
 }
 
@@ -61,10 +71,10 @@ sub linkSearch {
 	my $C = shift;
 	my $r;
 
-	foreach (qw[threshold query min author op sid topic section total]) {
+	foreach (qw[threshold query min author op sid topic section total hitcount]) {
 		my $x = "";
 		$x =  $C->{$_} if defined $C->{$_};
-		$x =  $I{F}{$_} if defined $I{F}{$_} && !$x;
+		$x =  $I{F}{$_} if defined $I{F}{$_} && $x eq "";
 		$x =~ s/ /+/g;
 		$r .= "$_=$x&" unless $x eq "";
 	}
@@ -97,9 +107,62 @@ sub keysearch {
 	return $sql;
 }
 
+
+sub countSearchHits {
+	my $searchtype = shift;
+	my($sqlquery, $kw) = ('SELECT count(*) ', '');
+
+	if ($searchtype eq 'comments') {
+		$kw = keysearch($I{F}{query}, 'subject', 'comment');
+		$sqlquery .= "FROM newstories, comments WHERE ";
+		$sqlquery .= "newstories.sid=" . $I{dbh}->quote($I{F}{sid}) . " AND "
+			if $I{F}{sid};
+		# There should be a threshhold value for comment searches on the form, yes?
+		$sqlquery .= "points >= $I{U}{threshold} ";
+		$sqlquery .= "AND section=" . $I{dbh}->quote($I{F}{section})
+			if $I{F}{section};
+		$sqlquery .= " AND" if $I{F}{query};
+	} elsif ($searchtype eq 'users') {
+		$kw = keysearch($I{F}{query}, 'nickname', 'fakeemail');
+		$sqlquery .= "FROM users ";
+		$sqlquery .= "WHERE " if $I{F}{query};
+	} elsif ($searchtype eq 'stories') {
+		$kw = keysearch($I{F}{query}, 'title', 'introtext');
+		$sqlquery .= '  FROM stories ';
+		$sqlquery .= $I{F}{section} ? <<EOT : 'WHERE displaystatus >= 0';
+WHERE ((displaystatus = 0 and "$I{F}{section}"="")
+        OR (section="$I{F}{section}" and displaystatus>=0))
+EOT
+
+		$sqlquery .= "   AND time < now() AND writestatus >= 0  ";
+		$sqlquery .= "   AND aid=" . $I{dbh}->quote($I{F}{author})
+			if $I{F}{author};
+		$sqlquery .= "   AND section="	. $I{dbh}->quote($I{F}{section})
+			if $I{F}{section};
+		$sqlquery .= "   AND tid="	. $I{dbh}->quote($I{F}{topic})
+			if $I{F}{topic};
+		$sqlquery .= " AND " if $I{F}{query};
+	}
+	if ($I{F}{query}) {
+		$kw =~ s/as kw$//;
+		$kw =~ s/\+/ OR /g;
+		$sqlquery .= " ($kw)";
+	}
+
+	my $cursor = $I{dbh}->prepare($sqlquery);
+	$cursor->execute;
+	($I{F}{hitcount}) = $cursor->fetchrow;
+
+#	$I{F}{mycountingquery} = "($I{F}{hitcount}) <= $sqlquery<BR>";
+}
+
 #################################################################
 sub searchForm {
 	my $SECT = getSection($I{F}{section});
+
+	# Count the number of hits for the entire search if we haven't
+	# done so and we have keywords to search with.
+#	print "<TT>$I{F}{mycountingquery}</TT><BR>" if $I{F}{mycountingquery};
 
 	my $t = lc $I{sitename};
 	$t = $I{F}{topic} if $I{F}{topic};
@@ -154,8 +217,7 @@ each of the last 30,000 or so comments posted.  Older comments
 are removed and currently only visible as static HTML.<P>
 EOT
 	
-	$I{F}{min} = int $I{F}{min};
-	my $prev = $I{F}{min} - 20;
+	my $prev = $I{F}{min} - $I{F}{max};
 	print linkSearch({
 		'link'	=> "<B>$I{F}{min} previous matches...</B>",
 		min	=> $prev
@@ -164,18 +226,16 @@ EOT
 	# select comment ID, comment Title, Author, Email, link to comment
 	# and SID, article title, type and a link to the article
 	my $sqlquery = "SELECT section, newstories.sid, aid, title, pid, subject, writestatus," .
-		getDateFormat("time","d") . ",".
-		getDateFormat("date","t") . ", 
-		uid, cid, ";
+		getDateFormat("time","d") . "," .
+		getDateFormat("date","t") . ", uid, cid, ";
 
-	$sqlquery .= "	  " . keysearch($I{F}{query}, "subject", "comment") if $I{F}{query};
-	$sqlquery .= "	  1 as kw " unless $I{F}{query};
-	$sqlquery .= "	  FROM newstories, comments
-			 WHERE newstories.sid=comments.sid ";
-	$sqlquery .= "     AND newstories.sid=" . $I{dbh}->quote($I{F}{sid}) if $I{F}{sid};
-	$sqlquery .= "     AND points >= $I{U}{threshold} ";
-	$sqlquery .= "     AND section=" . $I{dbh}->quote($I{F}{section}) if $I{F}{section};
-	$sqlquery .= " ORDER BY kw DESC, date DESC, time DESC LIMIT $I{F}{min},20 ";
+	$sqlquery .= keysearch($I{F}{query}, 'subject', 'comment') if $I{F}{query};
+	$sqlquery .= " 1 as kw " unless $I{F}{query};
+	$sqlquery .= " FROM newstories, comments WHERE newstories.sid=comments.sid ";
+	$sqlquery .= " AND newstories.sid=" . $I{dbh}->quote($I{F}{sid}) if $I{F}{sid};
+	$sqlquery .= " AND points >= $I{U}{threshold} ";
+	$sqlquery .= " AND section=" . $I{dbh}->quote($I{F}{section}) if $I{F}{section};
+	$sqlquery .= " ORDER BY kw DESC, date DESC, time DESC LIMIT $I{F}{min},$I{F}{max} ";
 
 	if ($I{F}{sid}) {
 		my($t) = sqlSelect("title", "newstories",
@@ -216,9 +276,9 @@ EOT
 
 	$cursor->finish;
 
-	print "No Matches Found for your query" unless $x > 0 || $I{F}{query};
+#	print "No Matches Found for your query" unless $x > 0 || $I{F}{query};
 
-	my $remaining = "";
+	my $remaining = $I{F}{hitcount} - $I{F}{'last'};
 	print "<P>", linkSearch({
 		'link'	=> "<B>$remaining Matches Left</B>",
 		min	=> $x
@@ -232,27 +292,37 @@ sub userSearch {
 		'link'	=> "<B>$I{F}{min} previous matches...</B>",
 		min	=> $prev
 	}), "<P>" if $prev >=0;
-	
-	my $c = sqlSelectMany("fakeemail,nickname,uid," .
-		keysearch($I{F}{query},"nickname"),
-		"users", "", "ORDER BY kw DESC LIMIT 10"
-	) if $I{F}{query};
+
+	# userSearch REALLY doesn't need to be ordered by keyword since you 
+	# only care if the substring is found.
+	my $sqlquery = "SELECT fakeemail,nickname,uid ";
+	$sqlquery .= " FROM users WHERE uid > 0 ";
+	if ($I{F}{query}) {
+		my $kw = keysearch($I{F}{query}, 'nickname', 'ifnull(fakeemail,"")');
+		$kw =~ s/as kw$//;
+		$kw =~ s/\+/ OR /g;
+		$sqlquery .= "AND ($kw) ";
+	}
+	$sqlquery .= "ORDER BY uid LIMIT $I{F}{min}, $I{F}{max}";
+	my $c = $I{dbh}->prepare($sqlquery);
+	undef $c unless $c->execute;
+
+#	print "<P><TT>$sqlquery</TT><BR>";
 	return unless $c;
 
 	my $total = $c->{rows};
 	my($x, $cnt) = 0;
 
 	while(my $N = $c->fetchrow_hashref) {
-		last unless $N->{kw};
 		my $ln = $N->{nickname};
 		$ln =~ s/ /+/g;
 
 		my $fake = $N->{fakeemail} ? <<EOT : '';
-	<A HREF="mailto:$N->{fakeemail}">$N->{fakeemail}</A>
+	email: <A HREF="mailto:$N->{fakeemail}">$N->{fakeemail}</A>
 EOT
 		print <<EOT;
-<LI><A HREF="$I{rootdir}/users.pl?nick=$ln">$N->{nickname}</A> &nbsp;
-$fake	($N->{uid})</LI>
+<A HREF="$I{rootdir}/users.pl?nick=$ln">$N->{nickname}</A> &nbsp;
+($N->{uid}) $fake<BR>
 EOT
 
 		$x++;
@@ -260,13 +330,15 @@ EOT
 
 	$c->finish;
 
-	print "No Matches Found for your query" if $x < 1;
+#	print "No Matches Found for your query" if $x < 1;
 
-	my $remaining = $total - $I{F}{'last'};
+	my $remaining = $I{F}{hitcount} - $I{F}{'last'};
+	print "<P>";
 	print linkSearch({
 		'link'	=> "<B>$remaining matches left</B>",
-		min	=> $I{F}{'last'}
-	}) unless $x < $I{F}{max};
+		min	=> $I{F}{'last'},
+		hitcount => $I{F}{hitcount}
+	}) unless $I{F}{'last'} >= $I{F}{hitcount};
 }
 
 #################################################################
@@ -277,14 +349,13 @@ sub storySearch {
 		min	=> $prev
 	}), "<P>" if $prev >= 0;
 
-	my $sqlquery = "SELECT aid,title,sid," .
-			getDateFormat("time","t") .
-			", commentcount,section ";
+	my $sqlquery = "SELECT aid,title,sid," . getDateFormat("time","t") .
+		", commentcount,section ";
 	$sqlquery .= "," . keysearch($I{F}{query}, "title", "introtext") . " "
 		if $I{F}{query};
 	$sqlquery .="	,0 " unless $I{F}{query};
 
-	if ($I{F}{query}) {
+	if ($I{F}{query} || $I{F}{topic}) {
 		$sqlquery .= "  FROM stories ";
 	} else {
 		$sqlquery .= "  FROM newstories ";
@@ -297,12 +368,13 @@ EOT
 
 	$sqlquery .= "   AND time < now() AND writestatus >= 0  ";
 	$sqlquery .= "   AND aid="	. $I{dbh}->quote($I{F}{author})  if $I{F}{author};
-	$sqlquery .= "   AND section="	. $I{dbh}->quote($I{F}{section}) if $I{F}{section};
+	$sqlquery .= "   AND section="	. $I{dbh}->quote($I{F}{section})
+		if $I{F}{section};
 	$sqlquery .= "   AND tid="	. $I{dbh}->quote($I{F}{topic})   if $I{F}{topic};
 
 	$sqlquery .= " ORDER BY ";
 	$sqlquery .= " kw DESC, " if $I{F}{query};
-	$sqlquery .= " time DESC LIMIT $I{F}{min},$I{F}{max} ";
+	$sqlquery .= " time DESC LIMIT $I{F}{min},$I{F}{max}";
 
 #	print "<P><TT>$sqlquery</TT></P>";
 
@@ -310,12 +382,11 @@ EOT
 	$cursor->execute;
 
 	my($x, $cnt) = 0;
-	# print $sqlquery if $I{U}{uid}==1;
 	print " ";
 
 	while (my($aid, $title, $sid, $time, $commentcount, $section, $cnt) = 
 		$cursor->fetchrow) {
-		last if $cnt == 0 && $I{F}{query};
+		last unless $cnt || ! $I{F}{query};
 		print $cnt ? $cnt :  $x + $I{F}{min};
 		print " ";
 		print linkStory({
@@ -328,13 +399,13 @@ EOT
 
 	$cursor->finish;
 
-	print "No Matches Found for your query" if $x < 1;
+#	print "No Matches Found for your query." if $x < 1;
 
-	my $remaining = "";
+	my $remaining = $I{F}{hitcount} - $I{F}{'last'};
 	print "<P>", linkSearch({
-		'link'	=> "<B>$remaining matches left</B>",
+		'link'	=> $I{F}{hitcount} ? "<B>$remaining matches left</B>" : "<B>More Articles</B>",
 		min	=> $I{F}{'last'}
-	}) unless $x < $I{F}{max};
+	}) unless $I{F}{'last'} >= $I{F}{hitcount};
 }
 
 main;
