197f106f83cd659575788f2febdf0b3ad5a10316
pudge <projects@pudge.net>
2000-06-19 20:41:00 +0000

    Disallow more characters for nicknames
    tracks internal commit a441ee0bd9149302e4303631db171b164ee5b286

--- themes/slashcode/htdocs/users.pl
+++ themes/slashcode/htdocs/users.pl
@@ -227,7 +227,7 @@ sub newUser {
 
 	$I{F}{newuser} =~ s/\s+/ /g;
 	$I{F}{newuser} = stripByMode($I{F}{newuser}, "nohtml");
-	$I{F}{newuser} =~ s/&//g;
+	$I{F}{newuser} =~ s/[?&%#=]//g;
 	$I{F}{newuser} = substr($I{F}{newuser}, 0, 20);
 
 	(my $matchname = lc $I{F}{newuser}) =~ s/[^a-zA-Z0-9]//g;
@@ -1006,7 +1006,8 @@ EOT1
 EOT2
 
 	print <<EOT;
-	(Note: the characters &amp;, &lt; and &gt; are not allowed in nicknames.)
+	(Note: the characters &amp;&lt;&gt;?%#= are not allowed in nicknames, and
+	will be stripped out.)
 
 	<INPUT TYPE="TEXT" NAME="newuser" SIZE="20" MAXLENGTH="20" VALUE="$I{F}{newuser}">
 	<BR> and a <B>valid email address</B> address to send your registration
