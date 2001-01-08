83d49b4977c46fcbe89435e62b424bb18332dadf
pudge <projects@pudge.net>
2001-01-08 21:01:08 +0000

    Update for bender
    tracks internal commit 3b8da7c0d51e49b59ed163d485142bb82e3a6cb2

--- Bundle/Slash.pm
+++ Bundle/Slash.pm
@@ -1,6 +1,6 @@
 package Bundle::Slash;
 
-$Bundle::Slash::VERSION = '1.0.8';
+$Bundle::Slash::VERSION = '1.0.10';
 
 1;
 
@@ -17,19 +17,21 @@ C<perl -MCPAN -e 'install "Bundle::Slash"'>
 
 =head1 CONTENTS
 
-Bundle::CPAN	- File::Spec,Digest::MD5,Compress::Zlib,libnet,Archive::Tar,Data::Dumper
+Bundle::CPAN		- File::Spec,Digest::MD5,Compress::Zlib,libnet,Archive::Tar,Data::Dumper
 
-Bundle::LWP	- URI,HTML::Parser,MIME::Base64
+Bundle::LWP		- URI,HTML::Parser,MIME::Base64
 
-Bundle::DBI	- Storable
+Bundle::DBI		- Storable
 
 DBI::FAQ
 
-Bundle::DBD::mysql  - Data::ShowTable
+Bundle::DBD::mysql	- Data::ShowTable
 
-Date::Parse         - TimeDate
+Date::Parse		- TimeDate
 
 XML::Parser
+
+XML::RSS
 
 Date::Manip
 
@@ -37,20 +39,15 @@ Mail::Sendmail
 
 Apache::DBI
 
-Apache::DBILogConfig
-
-Apache::DBILogger
-
 Image::Size
+
+Template		- Template Toolkit
 
 
 =head1 DESCRIPTION
 
 mod_perl must be installed by hand, because of the special configuration
 required for it.
-
-IPC::Shareable no longer required for IPC, since we are no longer doing
-IPC.
 
 If CPAN starts downloading "perl-5.6.0" or somesuch, ctrl-C it, exit
 the CPAN shell, and start it again.  The latest CPAN.pm version does not
