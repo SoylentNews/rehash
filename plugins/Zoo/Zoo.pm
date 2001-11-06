a7c063bc8383cf448dbb0d2dc38d221e36f3b9cd
Brian Aker <brian@tangent.org>
2001-11-06 03:42:54 +0000

    First version checkin
    tracks internal commit a998954fc12e901cea92250271672c3541d494cb

---
+++ plugins/Zoo/Zoo.pm
@@ -0,0 +1,153 @@
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2001 by Open Source Development Network. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+package Slash::Zoo;
+
+use strict;
+use DBIx::Password;
+use Slash;
+use Slash::Utility;
+use Slash::DB::Utility;
+
+use vars qw($VERSION @EXPORT);
+use base 'Slash::DB::Utility';
+use base 'Slash::DB::MySQL';
+
+($VERSION) = ' $Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+# "There ain't no justice" -Niven
+# We can try. 	-Brian
+
+sub new {
+	my($class, $user) = @_;
+	my $self = {};
+
+	my $slashdb = getCurrentDB();
+	my $plugins = $slashdb->getDescriptions('plugins');
+	return unless $plugins->{'Zoo'};
+
+	bless($self, $class);
+	$self->{virtual_user} = $user;
+	$self->sqlConnect;
+
+	return $self;
+}
+
+sub getFriends {
+	_get(@_, "friend");
+}
+
+sub getFoes {
+	_get(@_, "foe");
+}
+
+sub getFreaks {
+	my ($self) = @_;
+	_getOpposite(@_, "foe");
+}
+
+sub getFans {
+	_getOpposite(@_, "friend");
+}
+
+sub _get {
+	my($self, $uid, $type) = @_;
+
+	my $people = $self->sqlSelectAll(
+		'people.uid, nickname, journal_last_entry_date',
+		'people, users',
+		"people.uid = $uid AND type =\"$type\" AND person = users.uid"
+	);
+	return $people;
+}
+
+sub _getOpposite {
+	my($self, $uid, $type) = @_;
+
+	my $people = $self->sqlSelectAll(
+		'people.person, nickname, journal_last_entry_date',
+		'people, users',
+		"person = $uid AND type =\"$type\" AND person = users.uid"
+	);
+	return $people;
+}
+
+sub setFriend {
+	_set(@_, 'friend');
+}
+
+sub setFoe {
+	_set(@_, 'foe');
+}
+
+sub _set {
+	my($self, $uid, $person, $type) = @_;
+
+	$self->sqlDo("REPLACE INTO people (uid,person,type) VALUES ($uid, $person, $type)");
+}
+
+sub isFriend {
+	my($self, $uid, $friend) = @_;
+	return 0 unless $uid && $friend;
+	my $cols  = "uid";
+	my $table = "people";
+	my $where = "uid=$uid AND person=$friend AND type = 'friend'";
+
+	my $is_friend = $self->sqlSelect($cols, $table, $where);
+	return $is_friend;
+}
+
+sub isFoe {
+	my($self, $uid, $foe) = @_;
+	return 0 unless $uid && $foe;
+	my $cols  = "uid";
+	my $table = "people";
+	my $where = "uid=$uid AND person=$foe AND type = 'foe'";
+
+	my $is_foe = $self->sqlSelect($cols, $table, $where);
+	return $is_foe;
+}
+
+sub delete {
+	my($self, $uid, $person) = @_;
+	$self->sqlDo("DELETE FROM people WHERE uid=$uid AND person=$person");
+}
+
+
+sub DESTROY {
+	my($self) = @_;
+	$self->{_dbh}->disconnect if !$ENV{GATEWAY_INTERFACE} && $self->{_dbh};
+}
+
+
+1;
+
+__END__
+
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+Slash::Zoo - Zoo system splace
+
+=head1 SYNOPSIS
+
+	use Slash::Zoo;
+
+=head1 DESCRIPTION
+
+This is a port of Tangent's journal system.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+Brian Aker, brian@tangent.org
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
---
+++ plugins/Zoo/zoo.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/perl -w
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2001 by Open Source Development Network. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+use strict;
+use Slash 2.001;	# require Slash 2.1
+use Slash::Display;
+use Slash::Utility;
+use Slash::Zoo;
+use Slash::XML;
+use vars qw($VERSION);
+
+($VERSION) = ' $Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+use constant ALLOWED	=> 0;
+use constant FUNCTION	=> 1;
+
+sub main {
+	my $zoo   = getObject('Slash::Zoo');
+	my $constants = getCurrentStatic();
+	my $slashdb   = getCurrentDB();
+	my $user      = getCurrentUser();
+	my $form      = getCurrentForm();
+
+	# require POST and logged-in user for these ops
+	my $user_ok   = $user->{state}{post} && !$user->{is_anon};
+
+	# possible value of "op" parameter in form
+	my %ops = (
+		confirm		=> [ 1,		\&confirm		], # formkey?
+		add		=> [ $user_ok,		\&add		], # formkey?
+		'delete'		=> [ $user_ok,		\&dekete		], # formkey?
+		friends		=> [ 1,			\&friends		],
+		fans		=> [ 1,			\&fans		],
+		foes		=> [ 0,			\&list		],
+		freaks		=> [ 0,			\&list		],
+		editfriend		=> [ $user_ok,			\&edit		],
+		editfoe		=> [ $user_ok,			\&edit		],
+		default		=> [ 0,			\&list	],
+	);
+
+	my $op = $form->{'op'};
+	print STDERR "OP $op\n";
+	if (!$op || !exists $ops{$op} || !$ops{$op}[ALLOWED]) {
+		redirect($constants->{rootdir});
+		return;
+	}
+
+	# hijack RSS feeds
+	if ($form->{content_type} eq 'rss') {
+		# Do nothing for the moment.
+	} else {
+		$ops{$op}[FUNCTION]->($zoo, $constants, $user, $form, $slashdb);
+		footer();
+	}
+}
+
+sub list {
+	my($zoo, $constants, $user, $form, $slashdb) = @_;
+
+	_printHead("mainhead");
+
+	my $friends = $zoo->getFriends($user->{uid});
+	if (@$friends) {
+		slashDisplay('plainlist', { friends => $friends });
+	} else {
+		print getData('nofriends');
+	}
+}
+
+sub friends {
+	my($zoo, $constants, $user, $form, $slashdb) = @_;
+
+	_printHead("friendshead", { nickname => $form->{nick} });
+	
+	my $uid = $form->{uid} ? $form->{uid} : $slashdb->getUserUID($form->{nick});
+	my $friends = $zoo->getFriends($uid);
+	my $editable = ($uid == $user->{uid} ? 1 : 0);
+	if (@$friends) {
+		slashDisplay('plainlist', { friends => $friends, editable => $editable });
+	} else {
+		print getData('nofriends', { nickname => $form->{nick}});
+	}
+}
+
+sub fans {
+	my($zoo, $constants, $user, $form, $slashdb) = @_;
+
+	_printHead("fanshead",{ nickname => $form->{nick} });
+
+	my $uid = $form->{uid} ? $form->{uid} : $slashdb->getUserUID($form->{nick});
+	my $friends = $zoo->getFans($uid);
+	my $editable = ($uid == $user->{uid} ? 1 : 0);
+	if (@$friends) {
+		slashDisplay('plainlist', { friends => $friends, editable => $editable });
+	} else {
+		print getData('nofans', { nickname => $form->{nick}});
+	}
+}
+
+
+sub addFriend {
+	my($zoo, $constants, $user, $form, $slashdb) = @_;
+
+	$zoo->addFriend($user->{uid}, $form->{uid}) if $form->{uid};
+	displayFriends(@_);
+}
+
+sub deleteFriend {
+	my($zoo, $constants, $user, $form, $slashdb) = @_;
+
+	for my $uid (grep { $_ = /^del_(\d+)$/ ? $1 : 0 } keys %$form) {
+		$zoo->deleteFriend($user->{uid}, $uid);
+	}
+
+	displayFriends(@_);
+}
+
+
+sub _validFormkey {
+	my $error;
+	# this is a hack, think more on it, OK for now -- pudge
+	Slash::Utility::Anchor::getSectionColors();
+	for (qw(max_post_check interval_check formkey_check)) {
+		last if formkeyHandler($_, 0, 0, \$error);
+	}
+
+	if ($error) {
+		_printHead("mainhead");
+		print $error;
+		return 0;
+	} else {
+		# why does anyone care the length?
+		getCurrentDB()->updateFormkey(0, length(getCurrentForm()->{article}));
+		return 1;
+	}
+}
+
+sub _printHead {
+	my($head, $data) = @_;
+	my $title = getData($head, $data);
+	header($title);
+	slashDisplay("zoohead", { title => $title });
+}
+
+createEnvironment();
+main();
+1;
