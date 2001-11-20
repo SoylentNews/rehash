2e01af6a162ce7fe6a8c62e7c443ff5cd8158344
Brian Aker <brian@tangent.org>
2001-11-20 00:58:52 +0000

    Adding stats stuff
    tracks internal commit 01fe09e084aad88b28f7e62f5aec2e9b9d690d9e

---
+++ plugins/Stats/Stats.pm
@@ -0,0 +1,208 @@
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2001 by Open Source Development Network. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+package Slash::Stats;
+
+use strict;
+use DBIx::Password;
+use Slash;
+use Slash::Utility;
+use Slash::DB::Utility;
+
+use vars qw($VERSION);
+use base 'Slash::DB::Utility';
+use base 'Slash::DB::MySQL';
+
+($VERSION) = ' $Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+# On a side note, I am not sure if I liked the way I named the methods either.
+# -Brian
+sub new {
+	my($class, $user) = @_;
+	my $self = {};
+
+	my $slashdb = getCurrentDB();
+	my $plugins = $slashdb->getDescriptions('plugins');
+	return unless $plugins->{'Stats'};
+
+	bless($self, $class);
+	$self->{virtual_user} = $user;
+	$self->sqlConnect;
+
+	return $self;
+}
+
+########################################################
+sub createStatDaily {
+	my($self, $day,  $hash) = @_;
+
+	$self->sqlInsert(
+			day => $day,
+			total => $hash->{key},
+			unique => $hash->{key},
+			users => $hash->{key},
+			comments => $hash->{key},
+			homepage => $hash->{key},
+			journals => $hash->{key},
+	);
+}
+
+########################################################
+sub getPoints {
+	my($self) = @_;
+	return $self->sqlSelect('SUM(points)', 'users_comments');
+}
+
+########################################################
+sub countModeratorLog {
+	my($self, $yesterday) = @_;
+
+	my $used = $self->sqlCount(
+		'moderatorlog', 
+		"ts BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'"
+	);
+}
+
+########################################################
+sub countModeratorLog {
+	my($self, $yesterday) = @_;
+
+	my $modlog_hr = $self->sqlSelectAllHashref(
+		"val",
+		"val, COUNT(*) AS count",
+		"moderatorlog",
+		"ts BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'",
+		"GROUP BY val"
+	);
+	
+	return $modlog_hr;
+}
+
+########################################################
+sub countCommentsDaily {
+	my($self, $yesterday) = @_;
+
+	# Count comments posted yesterday... using a primary key,
+	# if it'll save us a table scan.  On Slashdot this cuts the
+	# query time from about 12 seconds to about 0.8 seconds.
+	my $max_cid = $self->sqlSelect("MAX(cid)", "comments");
+	my $cid_limit_clause = "";
+	if ($max_cid > 300_000) {
+		# No site can get more than 100K comments a day.
+		# It is decided.  :)
+		$cid_limit_clause = "cid > " . ($max_cid-100_000)
+			. " AND ";
+	}
+	my $comments = $self->sqlSelect(
+		"COUNT(*)",
+		"comments",
+		"$cid_limit_clause date BETWEEN '$yesterday 00:00' AND '$yesterday 23:59:59'"
+	);
+
+	return $comments; 
+}
+
+########################################################
+sub updateStamps {
+	my($self) = @_;
+	my $columns = "uid";
+	my $tables = "accesslog";
+	my $where = "to_days(now())-to_days(ts)=1 AND uid > 0";
+	my $other = "GROUP BY uid";
+
+	my $E = $self->sqlSelectAll($columns, $tables, $where, $other);
+
+	for (@{$E}) {
+		my $uid = $_->[0];
+		$self->setUser($uid, {-lastaccess=>'now()'});
+	}
+}
+
+########################################################
+sub countDaily {
+	my($self) = @_;
+	my %returnable;
+
+	my $constants = getCurrentStatic();
+
+	($returnable{'total'}) = $self->sqlSelect("count(*)", "accesslog",
+		"to_days(now()) - to_days(ts)=1");
+
+	my $c = $self->sqlSelectMany("count(*)", "accesslog",
+		"to_days(now()) - to_days(ts)=1 GROUP BY host_addr");
+	$returnable{'unique'} = $c->rows;
+	$c->finish;
+
+	$c = $self->sqlSelectMany("count(*)", "accesslog",
+		"to_days(now()) - to_days(ts)=1 GROUP BY uid");
+	$returnable{'unique_users'} = $c->rows;
+	$c->finish;
+
+	$returnable{'journals'}  = $self->sqlSelect("count(*)", "accesslog",
+		"op='journal' AND to_days(now()) - to_days(ts)=1 GROUP BY op");
+
+	$c = $self->sqlSelectMany("dat,count(*)", "accesslog",
+		"to_days(now()) - to_days(ts)=1 AND
+		(op='index' OR dat='index')
+		GROUP BY dat");
+
+	my(%indexes, %articles, %commentviews);
+
+	while (my($sect, $cnt) = $c->fetchrow) {
+		$indexes{$sect} = $cnt;
+	}
+	$c->finish;
+
+	$c = $self->sqlSelectMany("dat,count(*),op", "accesslog",
+		"to_days(now()) - to_days(ts)=1 AND op='article'",
+		"GROUP BY dat");
+
+	while (my($sid, $cnt) = $c->fetchrow) {
+		$articles{$sid} = $cnt;
+	}
+	$c->finish;
+
+	$returnable{'index'} = \%indexes;
+	$returnable{'articles'} = \%articles;
+
+
+	return \%returnable;
+}
+
+sub DESTROY {
+	my($self) = @_;
+	$self->{_dbh}->disconnect if !$ENV{GATEWAY_INTERFACE} && $self->{_dbh};
+}
+
+
+1;
+
+__END__
+
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+Slash::Stats - Stats system splace
+
+=head1 SYNOPSIS
+
+	use Slash::Stats;
+
+=head1 DESCRIPTION
+
+This is the Slash stats system.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+Brian Aker, brian@tangent.org
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
---
+++ plugins/Stats/adminmail.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use vars qw( %task $me );
+
+# Remember that timespec goes by the database's time, which should be
+# GMT if you installed everything correctly.  So 6:07 AM GMT is a good
+# sort of midnightish time for the Western Hemisphere.  Adjust for
+# your audience and admins.
+$task{$me}{timespec} = '7 6 * * *';
+$task{$me}{timespec_panic_2} = ''; # if major panic, dailyStuff can wait
+$task{$me}{code} = sub {
+	my($virtual_user, $constants, $slashdb, $user) = @_;
+	my($stats, $backupdb);
+	if ($constants->{backup_db_user}) {
+		$stats = getObject('Slash::Stats', $constants->{backup_db_user});
+		$backupdb = getObject('Slash::DB', $constants->{backup_db_user});
+	} else {
+		$stats = getObject('Slash::Stats');
+		$backupdb = $slashdb;
+	}
+
+	unless($stats) {
+		slashdLog('No database to run adminmail against');
+		return;
+	}
+
+	slashdLog('Send Admin Mail Begin');
+	my $count = $stats->countDaily();
+
+	# homepage hits are logged as either '' or 'shtml'
+	$count->{'index'}{'index'} += delete $count->{'index'}{''};
+	$count->{'index'}{'index'} += delete $count->{'index'}{'shtml'};
+	# these are 404s
+	delete $count->{'index.html'};
+
+	my $sdTotalHits = $stats->getVar('totalhits', 'value');
+
+	$sdTotalHits = $sdTotalHits + $count->{'total'};
+	$slashdb->setVar("totalhits", $sdTotalHits);
+
+	$slashdb->updateStamps();
+
+	my $accesslog_rows = $stats->sqlCount('accesslog');
+	my $formkeys_rows = $stats->sqlCount('formkeys');
+	my $modlog_rows = $stats->sqlCount('moderatorlog');
+	my $metamodlog_rows = $stats->sqlCount('metamodlog');
+
+	my $mod_points = $stats->getPoints;
+	my @yesttime = localtime(time-86400);
+	my $yesterday = sprintf "%4d-%02d-%02d", 
+		$yesttime[5] + 1900, $yesttime[4] + 1, $yesttime[3];
+	my $used = $stats->countModeratorLog($yesterday);
+	my $modlog_hr = $stats->countModeratorLogHour($yesterday);
+	my $modlog_total = $modlog_hr->{1}{count} + $modlog_hr->{-1}{count};
+
+	my $comments = $stats->countCommentsDaily($yesterday);
+
+	my @numbers = (
+		$count->{total},
+		$count->{unique},
+		$count->{unique_users},
+		$accesslog_rows,
+		$formkeys_rows,
+		$modlog_rows,
+		$metamodlog_rows,
+			($modlog_rows  ? $metamodlog_rows/$modlog_rows : 0),
+		$mod_points,
+		$modlog_total,
+			($mod_points   ? $modlog_total*100/$mod_points : 0),
+			($comments     ? $modlog_total*100/$comments   : 0),
+		$modlog_hr->{-1}{count},
+			($modlog_total ? $modlog_hr->{-1}{count}*100
+						/$modlog_total         : 0),
+		$modlog_hr->{1}{count},
+			($modlog_total ? $modlog_hr->{1}{count}*100
+						/$modlog_total         : 0),
+		$comments,
+		$sdTotalHits,
+		$count->{index}{index},
+		$count->{journals},
+	);
+	my $email = sprintf(<<"EOT", @numbers);
+$constants->{sitename} Stats for yesterday
+
+     total: %8d
+    unique: %8d
+     users: %8d
+  
+ accesslog: %8d rows total
+  formkeys: %8d rows total
+    modlog: %8d rows total
+metamodlog: %8d rows total (%.1fx modlog)
+mod points: %8d in pool
+used total: %8d yesterday (%.1f%% of pool, %.1f%% of comments)
+   used -1: %8d yesterday (%.1f%%)
+   used +1: %8d yesterday (%.1f%%)
+  comments: %8d posted yesterday
+
+total hits: %8d
+  homepage: %8d
+  journals: %8d
+   indexes
+EOT
+
+	for (sort {lc($a) cmp lc($b)} keys %{$count->{index}}) {
+		$email .= "\t   $_=$count->{index}{$_}\n"
+	}
+
+	$email .= "\n-----------------------\n";
+
+
+	for my $key (sort { $count->{'articles'}{$b} <=> $count->{'articles'}{$a} } keys %{$count->{'articles'}}) {
+		my $value = $count->{'articles'}{$key};
+
+ 		my $story = $backupdb->getStory($key, ['title', 'uid']);
+
+		$email .= sprintf("%6d %-16s %-30s by %s\n",
+			$value, $key, substr($story->{'title'}, 0, 30),
+			($slashdb->getUser($story->{uid}, 'nickname') || $story->{uid})
+		) if $story->{'title'} && $story->{uid} && $value > 100;
+	}
+
+	$email .= "\n-----------------------\n";
+	$email .= `$constants->{slashdir}/bin/tailslash -u $virtual_user -y today`;
+
+	$email .= "\n-----------------------\n";
+
+	# Send a message to the site admin.
+	for (@{$constants->{stats_reports}}) {
+		sendEmail($_, "$constants->{sitename} Stats Report", $email, 'bulk');
+	}
+	slashdLog('Send Admin Mail End');
+
+	return ;
+};
+
+1;
+
