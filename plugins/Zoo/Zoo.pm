879088238e7e2a56a82cae4566528d1606ed6e98
Brian Aker <brian@tangent.org>
2001-11-21 07:09:59 +0000

    Comitted wrong version of Zoo
    tracks internal commit d0a0d74ec35bc2b7a179e614384a04c7cde0c679

--- plugins/Zoo/Zoo.pm
+++ plugins/Zoo/Zoo.pm
@@ -10,7 +10,6 @@ use DBIx::Password;
 use Slash;
 use Slash::Utility;
 use Slash::DB::Utility;
-use Storable qw(thaw freeze);
 
 use vars qw($VERSION @EXPORT);
 use base 'Slash::DB::Utility';
@@ -76,23 +75,15 @@ sub _getOpposite {
 }
 
 sub setFriend {
-	my($self, $uid) = @_;
 	_set(@_, 'friend');
 }
 
 sub setFoe {
-	my($self, $uid) = @_;
 	_set(@_, 'foe');
 }
 
 sub _set {
 	my($self, $uid, $person, $type) = @_;
-
-	# This is the incremental rebuild we do
-	my $slashdb = getCurrentDB();
-	my ($store, $bonus) = thaw($slashdb->getUser($uid, qw[people $type_bonus]));
-	$store->{$person} = $bonus;
-	$slasbdb->setUser($uid, { people => freeze($store) });
 
 	$self->sqlDo("REPLACE INTO people (uid,person,type) VALUES ($uid, $person, $type)");
 }
