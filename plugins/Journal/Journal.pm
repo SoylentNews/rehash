76772401a2b8f9801b7429e9c48b8b99f0bed9a1
Jamie McCarthy <jamie@mccarthy.vg>
2001-12-30 17:32:27 +0000

    Documentation of the bug pudge just fixed.
    tracks internal commit 9361d79d078627502ff973d92b5d8cc4f5075815

--- plugins/Journal/Journal.pm
+++ plugins/Journal/Journal.pm
@@ -107,8 +107,14 @@ sub create {
 sub remove {
 	my($self, $id) = @_;
 	my $uid = $ENV{SLASH_USER};
-	return if $self->sqlDo("DELETE FROM journals WHERE uid=$uid AND id=$id") == 0;
-	$self->sqlDo("DELETE FROM journals_text WHERE id=$id");
+
+	if ($self->sqlDelete("journals", "uid=$uid AND id=$id") == 0) {
+		# Return value 0E0 means "no rows deleted" (i.e. this user owns
+		# no such journal) and undef means "error."  Either way, abort.
+		return;
+	}
+
+	$self->sqlDelete("journals_text", "id=$id");
 
 	my $date = $self->sqlSelect('MAX(date)', 'journals', "uid=$uid");
 	if ($date) {
