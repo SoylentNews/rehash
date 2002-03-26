4afd350fabf094f361bbe8d5590a9cf4b1a4f61b
Jamie McCarthy <jamie@mccarthy.vg>
2002-03-26 23:08:16 +0000

    Only getStoryByTime() for stories more recent than twice the archiving delay (which for Slashdot is 14, thus, the last 28 days).  This prevents long DB hangs when getting the prev/next story for stories posted many thousands of rows in the past.
    tracks internal commit 77d8dbb1553599b27c3de11c1e4f48275b9f0904

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -3010,9 +3010,20 @@ sub getAuthorNames {
 # the database?
 sub getStoryByTime {
 	my($self, $sign, $story, $section, $limit) = @_;
-	my($where);
+	my $where;
+	my $constants = getCurrentStatic();
 	my $user = getCurrentUser();
 	$limit ||= '1';
+
+	# We only do getStoryByTime() for stories that are more recent
+	# than twice the story archiving delay.  If the DB has to scan
+	# back thousands of stories, this can really bog.  We solve
+	# this by having the first clause in the WHERE be an impossible
+	# condition for any stories that are too old (this is more
+	# straightforward than parsing the timestamp in perl).
+	my $time = $story->{time};
+	my $twice_arch_delay = $constants->{archive_delay}*2;
+	$twice_arch_delay = 7 if $twice_arch_delay < 7;
 
 	my $order = $sign eq '<' ? 'DESC' : 'ASC';
 	if ($section->{isolate}) {
@@ -3028,11 +3039,14 @@ sub getStoryByTime {
 	$where .= "   AND section not in ($user->{'exsect'})" if $user->{'exsect'};
 	$where .= "   AND sid != '$story->{'sid'}'";
 
-	my $time = $story->{'time'};
 	my $returnable = $self->sqlSelectHashref(
 			'title, sid, section, tid',
 			'stories',
-			"time $sign '$time' AND writestatus != 'delete' AND time < now() $where",
+			"'$time' > DATE_SUB(NOW(), INTERVAL $twice_arch_delay DAY)
+			 AND time $sign '$time'
+			 AND time < NOW()
+			 AND writestatus != 'delete'
+			 $where",
 			"ORDER BY time $order LIMIT $limit"
 	);
 
