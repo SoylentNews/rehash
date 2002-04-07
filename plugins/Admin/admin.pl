0bb6dd9eb055093a3f7eaccf8b1d475e2a58d0cf
Jamie McCarthy <jamie@mccarthy.vg>
2002-04-07 13:26:44 +0000

    Allow posted submissions to be marked as accepted, not rejected.
    tracks internal commit 7a6d707c05d54a850386124ac5d86d0534b52d71

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -1431,10 +1431,6 @@ sub saveStory {
 	}
 	my $sid = $slashdb->createStory($data);
 
-	if ($form->{subid}) {
-	    $slashdb->deleteSubmission($form->{subid});
-	}
-
 	if ($constants->{feature_story_enabled}) {
 		if ($form->{feature_story}) {
 			$slashdb->setSection($form->{section}, { feature_story => $sid });
