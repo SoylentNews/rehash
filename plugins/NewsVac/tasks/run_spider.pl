1724bdcac8d8cd91d42c700ee8e7f92a389512eb
Cliff Wood <clifton.wood@gmail.com>
2002-06-20 02:15:11 +0000

    This should have been fixed with the one of the last three commits.
    So either it hasn't been or some naughy guy with a name starting with
    'C' has been committing the wrong copy. *sigh*
    
    tracks internal commit 4c0283d19c3e5f7a7262992317f1044c1c989618

--- plugins/NewsVac/tasks/run_spider.pl
+++ plugins/NewsVac/tasks/run_spider.pl
@@ -31,7 +31,7 @@ use vars qw( %task $me );
 
 # We run spiders, periodically depending 
 # on this cron timespec.
-$task{$me}{timespec} = '*/15 * * * *';	# Every 15 mins.
+$task{$me}{timespec} = '0-59/15 * * * *';	# Every 15 mins.
 $task{$me}{timespec_panic_1} = '';	# Don't run at all in a panic.
 
 $task{$me}{'fork'} = 1;			# If allowed, fork this task from 
