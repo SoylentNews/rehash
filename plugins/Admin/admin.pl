49dffb436af4e7d24c739017bb7638c6b4d34517
Jamie McCarthy <jamie@mccarthy.vg>
2002-06-20 14:49:02 +0000

    balanceTags() called for story creation. No more forgotten </I> tags!
    tracks internal commit 805126a0a5d6b9a53a2b73146e085d783ab5eeb1

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -1048,6 +1048,8 @@ sub editStory {
 				$storyref->{$field});
 			$storyref->{$field} = parseSlashizedLinks(
 				$storyref->{$field});
+			$storyref->{$field} = balanceTags(
+				$storyref->{$field});
 		}
 
 		$topic = $slashdb->getTopic($storyref->{tid});
@@ -1455,6 +1457,8 @@ sub updateStory {
 	}
 	$form->{introtext} = slashizeLinks($form->{introtext});
 	$form->{bodytext} =  slashizeLinks($form->{bodytext});
+	$form->{introtext} = balanceTags($form->{introtext});
+	$form->{bodytext} =  balanceTags($form->{bodytext});
 
 	my $data = {
 		uid		=> $form->{uid},
@@ -1533,6 +1537,8 @@ sub saveStory {
 	) . otherLinks($edituser->{nickname}, $form->{tid}, $edituser->{uid});
 	$form->{introtext} = slashizeLinks($form->{introtext});
 	$form->{bodytext} =  slashizeLinks($form->{bodytext});
+	$form->{introtext} = balanceTags($form->{introtext});
+	$form->{bodytext} =  balanceTags($form->{bodytext});
 
 	my $time = ($form->{fastforward})
 		? $slashdb->getTime()
