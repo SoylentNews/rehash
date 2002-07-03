ef2ce0e73ccf8b82dcae293d0883f5f125c7691c
Brian Aker <brian@tangent.org>
2002-07-03 01:17:46 +0000

    Removes double login issue
    tracks internal commit 81ef2383d4c5395bb96948bd31fa72e2faf8b766

--- themes/slashcode/htdocs/index.pl
+++ themes/slashcode/htdocs/index.pl
@@ -168,8 +168,8 @@ sub displayStandardBlocks {
 	} else {
 		@boxes = @{$sectionBoxes->{$getblocks}}
 			if ref $sectionBoxes->{$getblocks};
-		push(@boxes, @{$sectionBoxes->{'all_sections'}})
-			if ref $sectionBoxes->{'all_sections'};
+#		push(@boxes, @{$sectionBoxes->{'all_sections'}})
+#			if ref $sectionBoxes->{'all_sections'};
 		$boxcache = $cache->{slashboxes}{$getblocks}{$user->{light}} ||= {};
 	}
 
