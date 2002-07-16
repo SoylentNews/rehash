3d8308c4cb9a9bd00da460b49ec3694af5c19028
Jamie McCarthy <jamie@mccarthy.vg>
2002-07-16 11:35:12 +0000

    Typo fixes, "comments"->"submissions" where the latter was meant.
    tracks internal commit 10442348b1cc959ee62cc5031730b41a49c8c416

--- Slash/DB/Static/MySQL/MySQL.pm
+++ Slash/DB/Static/MySQL/MySQL.pm
@@ -327,13 +327,13 @@ sub forgetSubmissionIPs {
 	my($self) = @_;
 	my $constants = getCurrentStatic();
 
-	# Forget the source IP information for comments older than a given
-	# time.
+	# Forget the source IP information for submissions older than a
+	# given time.
 	my $hours = $constants->{submit_forgetip_hours} ||
 		$constants->{comments_forgetip_hours} || 720;
 	my $hours1 = $hours-1; $hours1 = 0 if $hours1 < 0;
 
-	# At what cid do we start scanning?
+	# At what subid do we start scanning?
 	my $minsubid = $constants->{submit_forgetip_minsubid};
 	if (!defined($minsubid)) {
 		$self->sqlInsert('vars', {
@@ -343,11 +343,7 @@ sub forgetSubmissionIPs {
 		$minsubid = 0;
 	}
 	# How many rows to do at once?  We don't want to tie up the DB
-	# for too long at one sitting.  Find the first discussion posted
-	# just after the time limit, and then the first comment in that
-	# discussion.  A discussion predates its comments, so this comment
-	# is guaranteed to postdate the time limit, and finding it doesn't
-	# require a table scan of comments, only of discussions.
+	# for too long at one sitting.
 	my $maxrows = $constants->{submit_forgetip_maxrows} ||
 		$constants->{comments_forgetip_maxrows} || 10000;
 	my $maxsubid = $minsubid + $maxrows;
