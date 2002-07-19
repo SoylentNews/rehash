817989a99529471fab3d02f6573c9881ee99bca4
Jamie McCarthy <jamie@mccarthy.vg>
2002-07-19 17:50:45 +0000

    Fix improper exclusion of "rss" op in daily total.
    tracks internal commit de1583d95920023539d6bc9f71f9e83993f88f8b

--- plugins/Stats/Stats.pm
+++ plugins/Stats/Stats.pm
@@ -471,9 +471,12 @@ sub countDaily {
 		"GROUP BY op"
 	);
 	$returnable{total} = 0;
+	my %excl_countdaily = map { $_, 1 } @{$constants->{op_exclude_from_countdaily}};
 	for my $op (keys %$totals_op) {
-		$returnable{total} += $totals_op->{$op}{count}
-			unless grep /^\Q$op\E$/, $constants->{op_exclude_from_countdaily}; # doesn't count in total
+		# If this op is on the list of ops to exclude,
+		# don't add its count into the daily total.
+		next if $excl_countdaily{$op};
+		$returnable{total} += $totals_op->{$op}{count};
 	}
 
 	my $c = $self->sqlSelectMany("COUNT(*)", "accesslog",
