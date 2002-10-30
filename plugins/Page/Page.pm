37c9cc8582265aa36f6dbcc10c5021e43d533d4d
Brian Aker <brian@tangent.org>
2002-10-30 18:35:49 +0000

    Adding portalbox support for Wes
    tracks internal commit 7cbd0b6224eec9e0581e073789abc80f31ebd8c5

--- plugins/Page/Page.pm
+++ plugins/Page/Page.pm
@@ -334,4 +334,153 @@ sub getStoryTitleContent {
 	return($storycontent);
 }
 
+#################################################################
+# Should this method be in the DB library?
+# absolutely.  we should hide the details there.  but this is in a lot of
+# places (modules, index, users); let's come back to it later.  -- pudge
+sub saveUserBoxes {
+	my(@a) = @_;
+	my $slashdb = getCurrentDB();
+	my $user = getCurrentUser();
+	$user->{exboxes} = @a ? sprintf("'%s'", join "','", @a) : '';
+	$slashdb->setUser($user->{uid}, { exboxes => $user->{exboxes} })
+		unless $user->{is_anon};
+}
+
+#################################################################
+sub getUserBoxes {
+	my $boxes = getCurrentUser('exboxes');
+	$boxes =~ s/'//g;
+	return split m/,/, $boxes;
+}
+
+#################################################################
+sub upBid {
+	my($bid) = @_;
+	my @a = getUserBoxes();
+
+	if ($a[0] eq $bid) {
+		($a[0], $a[@a-1]) = ($a[@a-1], $a[0]);
+	} else {
+		for (my $x = 1; $x < @a; $x++) {
+			($a[$x-1], $a[$x]) = ($a[$x], $a[$x-1]) if $a[$x] eq $bid;
+		}
+	}
+	saveUserBoxes(@a);
+}
+
+#################################################################
+sub dnBid {
+	my($bid) = @_;
+	my @a = getUserBoxes();
+	if ($a[@a-1] eq $bid) {
+		($a[0], $a[@a-1]) = ($a[@a-1], $a[0]);
+	} else {
+		for (my $x = @a-1; $x > -1; $x--) {
+			($a[$x], $a[$x+1]) = ($a[$x+1], $a[$x]) if $a[$x] eq $bid;
+		}
+	}
+	saveUserBoxes(@a);
+}
+
+#################################################################
+sub rmBid {
+	my($bid) = @_;
+	my @a = getUserBoxes();
+	for (my $x = @a; $x >= 0; $x--) {
+		splice @a, $x, 1 if $a[$x] eq $bid;
+	}
+	saveUserBoxes(@a);
+}
+
+#################################################################
+sub displayStandardBlocks {
+	my($self, $section_passed, $older_stories_essentials) = @_;
+	my $slashdb = getCurrentDB();
+	my $constants = getCurrentStatic();
+	my $user = getCurrentUser();
+	my $cache = getCurrentCache();
+	
+	my $section = $slashdb->getSection($section_passed);
+
+	return if $user->{noboxes};
+
+	my(@boxes, $return, $boxcache);
+	my($boxBank, $sectionBoxes) = $slashdb->getPortalsCommon();
+	my $getblocks = $section->{section} || 'index';
+
+	# two variants of box cache: one for index with portalmap,
+	# the other for any other section, or without portalmap
+
+	if ($user->{exboxes} && ($getblocks eq 'index' || $constants->{slashbox_sections})) {
+		@boxes = getUserBoxes();
+		$boxcache = $cache->{slashboxes}{index_map}{$user->{light}} ||= {};
+	} else {
+		@boxes = @{$sectionBoxes->{$getblocks}}
+			if ref $sectionBoxes->{$getblocks};
+		$boxcache = $cache->{slashboxes}{$getblocks}{$user->{light}} ||= {};
+	}
+
+	for my $bid (@boxes) {
+		if ($bid eq 'mysite') {
+			$return .= portalbox(
+				$constants->{fancyboxwidth},
+				getData('userboxhead'),
+				$user->{mylinks} || getData('userboxdefault'),
+				$bid
+			);
+
+		} elsif ($bid =~ /_more$/ && $older_stories_essentials) {
+			$return .= portalbox(
+				$constants->{fancyboxwidth},
+				getData('morehead'),
+				getOlderStories($older_stories_essentials, $section),
+				$bid
+			) if @$older_stories_essentials;
+
+		} elsif ($bid eq 'userlogin' && ! $user->{is_anon}) {
+			# do nothing!
+
+		} elsif ($bid eq 'userlogin' && $user->{is_anon}) {
+			$return .= $boxcache->{$bid} ||= portalbox(
+				$constants->{fancyboxwidth},
+				$boxBank->{$bid}{title},
+				slashDisplay('userlogin', 0, { Return => 1, Nocomm => 1 }),
+				$boxBank->{$bid}{bid},
+				$boxBank->{$bid}{url}
+			);
+
+		} elsif ($bid eq 'poll' && !$constants->{poll_cache}) {
+			# this is only executed if poll is to be dynamic
+			$return .= portalbox(
+				$constants->{fancyboxwidth},
+				$boxBank->{$bid}{title},
+				pollbooth('_currentqid', 1),
+				$boxBank->{$bid}{bid},
+				$boxBank->{$bid}{url}
+			);
+
+		# this could grab from the cache in the future, perhaps ... ?
+		} elsif ($bid eq 'rand' || $bid eq 'srandblock') {
+			# don't use cached title/bid/url from getPortalsCommon
+			my $data = $slashdb->getBlock($bid, [qw(title block bid url)]);
+			$return .= portalbox(
+				$constants->{fancyboxwidth},
+				@{$data}{qw(title block bid url)}
+			);
+
+		} else {
+			$return .= $boxcache->{$bid} ||= portalbox(
+				$constants->{fancyboxwidth},
+				$boxBank->{$bid}{title},
+				$slashdb->getBlock($bid, 'block'),
+				$boxBank->{$bid}{bid},
+				$boxBank->{$bid}{url}
+			);
+		}
+	}
+
+	return $return;
+}
+
 1;
