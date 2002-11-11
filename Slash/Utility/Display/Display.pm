e1a07486fffd4f9958fb62b5f5fb38e5ea099bb7
Jon 'CowboyNeal' Pater <cowboyneal@slashdot.org>
2002-11-11 17:14:28 +0000

    fixed nick display issue with score=0
    tracks internal commit ae73e9e553d1d2350a9ffd21c3e657677563ea0c

--- Slash/Utility/Display/Display.pm
+++ Slash/Utility/Display/Display.pm
@@ -1203,7 +1203,8 @@ sub _hard_linkComment {
 	}
 
 	$display .= qq!">$subject</A>!;
-	$display .= qq| by $comment->{nickname} <FONT SIZE="-1">(Score:$comment->{points})</FONT> |
+	$display .= qq| by $comment->{nickname}|;
+	$display .= qq| <FONT SIZE="-1">(Score:$comment->{points})</FONT> |
 		if !$user->{noscores} && $comment->{points};
 	$display .= qq| <FONT SIZE="-1">| . timeCalc($comment->{date}) . qq| </FONT>| if $date;
 	$display .= "\n";
