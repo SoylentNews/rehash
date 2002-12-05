5bc86ac5373a2606f0d9b6f6043cfb19f12b2e3f
Jamie McCarthy <jamie@mccarthy.vg>
2002-12-05 20:36:39 +0000

    More and better-named stats in DB.
    tracks internal commit 3277469793accf99183a8a0858554386efcb8b07

--- plugins/Stats/adminmail.pl
+++ plugins/Stats/adminmail.pl
@@ -268,11 +268,12 @@ EOT
 	$statsSave->createStatDaily("mod_points_lost_spent_minus_1", $modlog_yest_hr->{-1}{spent});
 	$statsSave->createStatDaily("m2_freq", $constants->{m2_freq} || 86400);
 	$statsSave->createStatDaily("m2_consensus", $constants->{m2_consensus} || 0);
+	$statsSave->createStatDaily("m2_mintokens", $self->getVar("m2_mintokens", "value", 1) || 0);
 	$statsSave->createStatDaily("m2_points_lost_spent", $metamodlogs_yest_total);
 	$statsSave->createStatDaily("m2_points_lost_spent_fair", $metamodlogs_yest_fair);
 	$statsSave->createStatDaily("m2_points_lost_spent_unfair", $metamodlogs_yest_unfair);
 	$statsSave->createStatDaily("oldest_unm2d", $oldest_unm2d);
-	$statsSave->createStatDaily("token_conversion_point", $token_conversion_point);
+	$statsSave->createStatDaily("mod_token_conversion_point", $token_conversion_point);
 
 	for my $nickname (keys %$admin_mods) {
 		my $uid = $admin_mods->{$nickname}{uid};
