03200a837bc430cf07f86559a852c1323b39558a
Jon 'CowboyNeal' Pater <cowboyneal@slashdot.org>
2003-01-14 15:22:34 +0000

    updated getAccessListInfo to also get reasons for md5ids where md5id is a subnetid. This is why subnetid ban reasons were not displaying previously.
    tracks internal commit 8edf329ef4c375cfd7691953442f6fd51ea5d0e6

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -3284,7 +3284,7 @@ sub getAccessListInfo {
 		if ($user_check->{uid} =~ /^\d+$/ && !isAnon($user_check->{uid})) {
 			$where = "uid = $user_check->{uid}";
 		} elsif ($user_check->{md5id}) {
-			$where = "ipid = '$user_check->{md5id}'";
+			$where = "(ipid = '$user_check->{md5id}' OR subnetid = '$user_check->{md5id}')";
 		} elsif ($user_check->{ipid}) {
 			$where = "ipid = '$user_check->{ipid}'";
 		} elsif ($user_check->{subnetid}) {
