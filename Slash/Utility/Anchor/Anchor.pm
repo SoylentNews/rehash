947b7a7ebb65852b843f9343eaea797da6c6b68d
Jamie McCarthy <jamie@mccarthy.vg>
2003-02-19 16:36:15 +0000

    The maintable is used in building/tracking the main content table that starts in header;misc;default and continues through index;index;default and similar content templates, concluding in footer;misc;default.  Allows other templates that build the content of the main table to not worry too carefully about placement of <TD> as long as they follow simple rules.
    tracks internal commit c5bbd2545255a99c926120d72ff654b97e9a7031

--- Slash/Utility/Anchor/Anchor.pm
+++ Slash/Utility/Anchor/Anchor.pm
@@ -165,6 +165,8 @@ sub header {
 #print STDERR "header(options->page) defined: '$options->{page}' for title '$data->{title}'\n" if defined($options->{page});
 	$data->{tab_selected} = $options->{tab_selected} if $options->{tab_selected};
 
+	$user->{state}{mt}{curcol} = 0;
+	$user->{state}{mt}{cols} = [ ];
 	if ($options->{admin} && $user->{is_admin}) {
 		$user->{state}{adminheader} = 1;
 		$display = slashDisplay('header-admin', $data, { Return => $options->{Return}, Page => $options->{Page} });
