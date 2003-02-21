7b408573aace15eea00a68ccaa97672d2829b4e6
Brian Aker <brian@tangent.org>
2003-02-21 20:23:20 +0000

    Saving name change
    tracks internal commit 7c09c0b6f32553c38805840476ff677285c744fc

--- plugins/Blob/blob.pl
+++ plugins/Blob/blob.pl
@@ -7,6 +7,7 @@
 use strict;
 use Slash;
 use Slash::Search;
+use Slash::Blob;
 use Slash::Display;
 use Slash::Utility;
 use Slash::XML;
@@ -19,17 +20,19 @@ sub main {
 	my $blob = getObject("Slash::Blob", { db_type => 'reader' });
 	
 	unless ($form->{id}) {
-		redirect("404.pl");
+		redirect("/404.pl");
+		return;
 	}
 	
 	my $data = $blob->get($form->{id});
 	if (!$data || $user->{seclev} < $data->{seclev}) {
-		redirect("404.pl");
+		redirect("/404.pl");
+		return;
 	}
 
 	my $r = Apache->request;
 	$r->header_out('Cache-Control', 'private');
-	$r->content_type($data->{mime_type});
+	$r->content_type($data->{content_type});
 	$r->status(200);
 	$r->send_http_header;
 	$r->rflush;
