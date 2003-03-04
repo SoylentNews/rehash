9d4c06c14b09c568dd393eafcc3f43aee6cf5a90
Jamie McCarthy <jamie@mccarthy.vg>
2003-03-04 18:38:52 +0000

    Recognize __version__ as a field in a template file.
    tracks internal commit 15c58e22fffa207d362609f79ef18faeea334a90

--- Slash/Install/Install.pm
+++ Slash/Install/Install.pm
@@ -108,10 +108,11 @@ sub readTemplateFile {
 	}
 	$val{'tpid'} = undef if $val{'tpid'};
 	{
-		# Make chomp() remove all newlines, not just 1.  These fields
-		# are used in ways sensitive to extraneous whitespace.
+		# Make chomp() remove all newlines, not just 1.  These
+		# fields are used in ways that may be sensitive to
+		# extraneous whitespace.
 		local $/ = "";
-		for (qw| name page section lang seclev |) {
+		for (qw| name page section lang seclev version |) {
 			chomp($val{$_}) if $val{$_};
 		}
 	}
@@ -126,8 +127,8 @@ sub writeTemplateFile {
 	my($self, $filename, $template) = @_;
 	my $fh = gensym;
 	open($fh, "> $filename\0") or die "Can't open $filename to write to: $!";
-	for (qw(section description title page lang name template seclev)) { #(keys %$template) {
-		next if ($_ eq 'tpid');
+	for (qw(section description title page lang name template seclev version)) { #(keys %$template) {
+		next if $_ eq 'tpid';
 		print $fh "__${_}__\n";
 		$template->{$_} =~ s/\015\012/\n/g;
 		print $fh "$template->{$_}\n";
