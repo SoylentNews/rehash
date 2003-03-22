149be016410691f4896418d2f60a07de6d1d4bbd
Jamie McCarthy <jamie@mccarthy.vg>
2003-03-22 01:12:07 +0000

    Use new getObject() format to get the reader DB and log DB.
    tracks internal commit 71b6b92625580454340505f63ab70af45c98cdf9

--- themes/slashcode/tasks/run_moderatord.pl
+++ themes/slashcode/tasks/run_moderatord.pl
@@ -90,33 +90,8 @@ sub give_out_points {
 
 	moderatordLog(getData('moderatord_log_header'));
 
-	my $backup_db = undef;
-	my $log_db = undef;
-
-	# If a backup DB is defined, we use that one.
-	my $backup_user = $constants->{backup_db_user} || '';
-	if ($backup_user) {
-		$backup_db = getObject('Slash::DB', $backup_user);
-		if ($backup_db) {
-			moderatordLog("Using backup database '$backup_user'");
-		} else {
-			moderatordLog("Skipping run, backup DB not avail");
-			return ;
-		}
-	}
-
-	# If an accesslog DB is defined, we use that one;  otherwise we
-	# default to the same as the backup DB.
-	my $log_user = $constants->{log_db_user} || '';
-	if ($log_user) {
-		$log_db = getObject('Slash::DB', $log_user);
-		if ($log_db) {
-			moderatordLog("Using accesslog database '$log_user'");
-		} else {
-			moderatordLog("Skipping run, accesslog DB not avail");
-			return ;
-		}
-	}
+	my $backup_db = getObject('Slash::DB', 'reader');
+	my $log_db = getObject('Slash::DB', 'log_slave');
 
 	my $newcomments = get_num_new_comments($constants, $slashdb);
 	if ($newcomments > 0) {
