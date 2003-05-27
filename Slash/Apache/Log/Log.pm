ac00868bfc8509b370dd7e125f8ca93f5a1746bf
pudge <projects@pudge.net>
2003-05-27 18:02:42 +0000

    Debugging crapola
    tracks internal commit c8031c6a5435fe7540fb3a6bf6e56449a3d66be8

--- Slash/Apache/Log/Log.pm
+++ Slash/Apache/Log/Log.pm
@@ -30,6 +30,19 @@ sub handler {
 	# Added this so that small sites would not have admin logins 
 	# recorded in their stats. -Brian
 
+	my $slashdb = getCurrentDB();
+	my $SECT;
+	$SECT = $slashdb->{"_sections_cache"}{$constants->{section}}
+		if exists($slashdb->{"_sections_cache"})
+		&& exists($slashdb->{"_sections_cache"}{$constants->{section}});
+	$SECT ||= {};
+
+	printf STDERR ("sections : %d : %d : %s : %s : %s : %s : %s : %s\n",
+		$$, scalar(keys %$SECT), $constants->{section}, $ENV{REQUEST_URI},
+		$uri, $dat, $r->status,
+		join('|', %{ getCurrentForm() })
+	);
+
 	# so it will still log it if the admin DOES request
 	# to admin.pl?  i thought you wanted it to NOT log
 	# requests to admin.pl?  should the !~ be =~ ?
