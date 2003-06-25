e9377bfe48264429ce53ab1484c99af9b213376c
pudge <projects@pudge.net>
2003-06-25 21:52:26 +0000

    Move soap_methods to main schema, so plugins can install into it easily enough, even if it is not going to be used, as it might be added later, and produces errors if it is not there.
    tracks internal commit 340f93dbb02a1feaa343a348a891c2cb28b22331

--- sql/mysql/slashschema_create.sql
+++ sql/mysql/slashschema_create.sql
@@ -671,6 +671,23 @@ CREATE TABLE section_topics (
 	PRIMARY KEY (section,type,tid)
 ) TYPE=InnoDB;
 
+
+#
+# Table structure for table 'soap_methods'
+#
+
+DROP TABLE IF EXISTS soap_methods;
+CREATE TABLE soap_methods (
+        id MEDIUMINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+        class VARCHAR(100) NOT NULL,
+        method VARCHAR(100) NOT NULL,
+        seclev MEDIUMINT DEFAULT 1000 NOT NULL,
+        subscriber_only TINYINT DEFAULT 0 NOT NULL,
+        formkeys VARCHAR(255) DEFAULT '' NOT NULL,
+        PRIMARY KEY (id),
+        UNIQUE soap_method(class, method)
+);
+
 #
 # Table structure for table 'subsections'
 #
