bbbefd28b48a68aa60cdfb65a204ebe38a3ed40e
pudge <projects@pudge.net>
2003-07-08 21:23:03 +0000

    whitespace fixes
    tracks internal commit f04e0b687333b33cb200aa8fa9aee282abc7508d

--- themes/slashcode/htdocs/comments.pl
+++ themes/slashcode/htdocs/comments.pl
@@ -749,8 +749,8 @@ sub editComment {
 		$form->{postersubj} = "Re:$form->{postersubj}";
 	}
 
-        my $gotmodwarning;
-        $gotmodwarning=1 if (($error_message eq getError("moderations to be lost")) or $form->{gotmodwarning});
+	my $gotmodwarning;
+	$gotmodwarning = 1 if (($error_message eq getError("moderations to be lost")) || $form->{gotmodwarning});
 	slashDisplay('edit_comment', {
 		error_message 	=> $error_message,
 		label		=> $label,
@@ -758,7 +758,7 @@ sub editComment {
 		indextype	=> $form->{indextype},
 		preview		=> $preview,
 		reply		=> $reply,
-                gotmodwarning   => $gotmodwarning
+		gotmodwarning	=> $gotmodwarning
 	});
 }
 
@@ -951,11 +951,11 @@ sub validateComment {
 			last;
 		}
 	}
-        if($constants->{allow_moderation} && $slashdb->sqlCount("moderatorlog","cuid=$user->{uid} and sid=$form->{sid}") && !$form->{gotmodwarning}){
-		$$error_message=getError("moderations to be lost");
-		$form_success=0;
+	if ($constants->{allow_moderation} && $slashdb->sqlCount("moderatorlog", "cuid=$user->{uid} and sid=$form->{sid}") && !$form->{gotmodwarning}) {
+		$$error_message = getError("moderations to be lost");
+		$form_success = 0;
 		return; 
-        }
+	}
 
 	$$error_message ||= '';
 	# Return false if error condition...
