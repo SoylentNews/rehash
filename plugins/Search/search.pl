f82232876e53010ad189fb9d619e2c8f5086efbd
pudge <projects@pudge.net>
2003-07-22 22:28:58 +0000

    whitespace
    tracks internal commit cde15420ba7c6dab25c53883f1767af50f2735dd

--- plugins/Search/search.pl
+++ plugins/Search/search.pl
@@ -50,10 +50,10 @@ sub main {
 	# is specified. If someone needs to search on all sections, they
 	# shouldn't be in one.				--Pater
 	$form->{section}	||= $user->{currentSection} || '';
-        if($user->{currentSection} eq "poll"){
-		$form->{op} = "poll";
+	if ($user->{currentSection} eq 'poll') {
+		$form->{op} = 'poll';
 		$form->{section} = '';
-        }
+	}
          
 	# This next line could be removed -Brian
 	$form->{section}	= '' if $form->{section} eq 'index';
@@ -146,7 +146,7 @@ sub _sections {
 	my $sections = $reader->getDescriptions('sections');
 	my %newsections = %$sections;
 	$newsections{''} = getData('all_sections');
-        delete $newsections{'poll'};
+	delete $newsections{'poll'};
 	return \%newsections;
 }
 
