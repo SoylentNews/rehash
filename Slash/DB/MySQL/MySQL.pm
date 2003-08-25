e7daf7f1a80b69539b43d6b29f0a6fdb893c1fd3
pudge <projects@pudge.net>
2003-08-25 20:36:44 +0000

    Add caching to getStoryByTime
    tracks internal commit 047ddae838179374e8b6572737eeb6e75afa607c

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -4375,6 +4375,13 @@ sub getStoryByTime {
 	my $topic   = $options->{topic}   || '';
 	my $section = $options->{section} || '';
 	my $where;
+	my $name  = 'story_by_time';
+	_genericCacheRefresh($self, $name, $constants->{story_expire} || 600); # use same cache time as for stories
+	my $cache = $self->{"_${name}_cache"} ||= {};
+
+	$self->{"_${name}_cache_time"} = time() if !keys %$cache;
+# any reason to force a refresh?
+#	%$cache = () if $refresh;
 
 	# We only do getStoryByTime() for stories that are more recent
 	# than twice the story archiving delay (or bytime_delay if defined).
@@ -4387,23 +4394,36 @@ sub getStoryByTime {
 	$bytime_delay = 7 if $bytime_delay < 7;
 
 	my $order = $sign eq '<' ? 'DESC' : 'ASC';
+	my $key = $sign;
 
 	if (!$section && !$topic && $user->{sectioncollapse}) {
 		$where .= ' AND displaystatus>=0';
+		$key .= '|>=';
 	} else {
 		$where .= ' AND displaystatus=0';
+		$key .= '|=';
 	}
 
 	$where .= " AND sid != '$story->{sid}'";
+	$key .= "|$story->{sid}";
+
 	if (!$topic && !$section) {
 		$where .= " AND tid not in ($user->{extid})" if $user->{extid};
 		$where .= " AND uid not in ($user->{exaid})" if $user->{exaid};
 		$where .= " AND section not in ($user->{exsect})" if $user->{exsect};
+		# don't cache if user has own prefs -- pudge
+		$key = $user->{extid} || $user->{exaid} || $user->{exsect} ? '' : $key . '|';
 	} elsif ($section) {
 		$where .= " AND section = '$section'";
+		$key .= "|$section";
 	} elsif ($topic) {
 		$where .= " AND tid = '$topic'";
+		$key .= "|$topic";
 	}
+
+	$key .= "|$time" if $key;
+
+	return $cache->{$key} if $key && defined $cache->{$key};
 
 #	print STDERR "SELECT title, sid, section, tid FROM stories WHERE " .
 #		"'$time' > DATE_SUB(NOW(), INTERVAL $bytime_delay DAY) AND time $sign '$time' AND time < NOW() AND writestatus != 'delete' $where " .
@@ -4421,6 +4441,8 @@ sub getStoryByTime {
 
 		"ORDER BY time $order LIMIT $limit"
 	);
+	# needs to be defined as empty
+	$cache->{$key} = $returnable || '' if $key;
 
 	return $returnable;
 }
