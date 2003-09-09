e7fe3c194895aa99bb87f5dfe71582a6663c31a2
Jamie McCarthy <jamie@mccarthy.vg>
2003-09-09 16:26:16 +0000

    Badpassword bugfixes; more info and different info in message template; sending badpassword msg to users still doesn't seem to work.
    tracks internal commit bcc3cb9dfc84c953f59c84f557fffcf500ee2b16

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -1331,7 +1331,7 @@ sub setContentFilter {
 ########################################################
 # This creates an entry in the accesslog
 sub createAccessLog {
-	my($self, $op, $dat, $status) = @_;	
+	my($self, $op, $dat, $status) = @_;
 	my $constants = getCurrentStatic();
 	my $form = getCurrentForm();
 	my $user = getCurrentUser();
@@ -1622,27 +1622,29 @@ sub createBadPasswordLog {
 
 	my $warn_interval = $constants->{bad_password_warn_user_interval} || 0;
 	my $bp_count = $self->getBadPasswordCountByUID($uid);
-	
-	# we only warn a user every X bad password attempts.  We don't want to
+
+	# We only warn a user at the Xth bad password attempt.  We don't want to
 	# generate a message for every bad attempt over a threshold
-	if($bp_count and $warn_interval and ($bp_count % $warn_interval == 0)){
+	if ($bp_count && $bp_count == $warn_interval) {
 
 		my $messages = getObject("Slash::Messages");
 		return unless $messages;
-		my $users  = $messages->checkMessageCodes(
-                	MSG_CODE_BADPASSWORD, [$uid]
-        	);
-		if(@$users){
-			my $nick = $self->sqlSelect("nickname","users","uid='$uid'");
-			my $bp = $self->getBadPasswordsByUID($uid);
+		my $users = $messages->checkMessageCodes(
+			MSG_CODE_BADPASSWORD, [$uid]
+		);
+		if (@$users) {
+			my $uid_q = $self->sqlQuote($uid);
+			my $nick = $self->sqlSelect("nickname", "users", "uid=$uid_q");
+			my $bp = $self->getBadPasswordIPsByUID($uid);
 			my $data  = {
-				template_name   => 'badpassword_msg',
-				subject         => 'Bad login attempts warning',
-				nickname	=> $nick,
-				uid		=> $uid,
-				bad_passwords	=> $bp
-                	};
-		
+				template_name =>	'badpassword_msg',
+				subject =>		'Bad login attempts warning',
+				nickname =>		$nick,
+				uid =>			$uid,
+				bp_count =>		$bp_count,
+				bp_ips =>		$bp
+			};
+
 			$messages->create($users->[0],
 				MSG_CODE_BADPASSWORD, $data, 0, '', 'now'
 			);
@@ -1651,18 +1653,36 @@ sub createBadPasswordLog {
 }
 
 ########################################################
-sub getBadPasswordsByUID{
-	my ($self, $uid) = @_;
-	my $ar = $self->sqlSelectAllHashrefArray('ip,password,date_format(ts,"%Y-%m-%d %h:%i:%s") as ts',"badpasswords","uid='$uid' and ts > date_sub(now(),interval 1 day)");
+sub getBadPasswordsByUID {
+	my($self, $uid) = @_;
+	my $uid_q = $self->sqlQuote($uid);
+	my $ar = $self->sqlSelectAllHashrefArray(
+		"ip, password, DATE_FORMAT(ts, '%Y-%m-%d %h:%i:%s') AS ts",
+		"badpasswords",
+		"uid=$uid_q AND ts > DATE_SUB(NOW(), INTERVAL 1 DAY)");
 	return $ar;
 }
 
 ########################################################
-sub getBadPasswordCountByUID{
-	my ($self, $uid) = @_;
-	return $self->sqlSelect("count(*)","badpasswords","uid='$uid' and ts > date_sub(now(),interval 1 day)");
+sub getBadPasswordCountByUID {
+	my($self, $uid) = @_;
+	my $uid_q = $self->sqlQuote($uid);
+	return $self->sqlCount("badpasswords",
+		"uid=$uid_q AND ts > DATE_SUB(NOW(), INTERVAL 1 DAY)");
 }
 
+########################################################
+sub getBadPasswordIPsByUID {
+	my($self, $uid) = @_;
+	my $uid_q = $self->sqlQuote($uid);
+	my $ar = $self->sqlSelectAllHashrefArray(
+		"ip, COUNT(*) AS c,
+		 MIN(DATE_FORMAT(ts, '%Y-%m-%d %h:%i:%s')) AS mints,
+		 MAX(DATE_FORMAT(ts, '%Y-%m-%d %h:%i:%s')) AS maxts",
+		"badpasswords",
+		"uid=$uid_q AND ts > DATE_SUB(NOW(), INTERVAL 1 DAY)",
+		"GROUP BY ip ORDER BY c DESC");
+}
 
 ########################################################
 # Make a new password, save it in the DB, and return it.
