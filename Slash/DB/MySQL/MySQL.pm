3b7a6f57bf0d86813d02644de2edeb2860dd6a3b
Jon 'CowboyNeal' Pater <cowboyneal@slashdot.org>
2003-11-17 23:34:55 +0000

    fixed sql typos
    tracks internal commit c0996937d7e47e4c468edce73da5e978772c5148

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -239,7 +239,7 @@ my %descriptions = (
 	},
 
 	'forums'
-		=> sub { $_[0]->sqlSelectMany('subsection.id, subsection.alttext', 'section_subsections, subsections', "section_subsections.subsection=subsections.id AND section_subsections.section='forums'") },
+		=> sub { $_[0]->sqlSelectMany('subsections.id, subsections.title', 'section_subsections, subsections', "section_subsections.subsection=subsections.id AND section_subsections.section='forums'") },
 	
 );
 
