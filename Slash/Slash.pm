220e40f056466668bc2dc294abc472c45882d84e
Jon 'CowboyNeal' Pater <cowboyneal@slashdot.org>
2003-12-12 16:11:02 +0000

    added File::Spec-> to catfile call
    tracks internal commit 4710c553534e92fd0f63378f0353972bcf70d772

--- Slash/Slash.pm
+++ Slash/Slash.pm
@@ -1195,7 +1195,7 @@ EOT
 
 	# stats for clampe
 	if ($constants->{clampe_stats} && $ENV{SCRIPT_NAME}) {
-		my $fname = catfile('clampe', $user->{ipid});
+		my $fname = File::Spec->catfile('clampe', $user->{ipid});
 		my $comlog = "IPID: $user->{ipid} UID: $user->{uid} SID: $comment->{sid} CID: $comment->{cid} Dispmode: $user->{mode} Thresh: $user->{threshold} CIPID: $comment->{ipid} CUID: $comment->{uid}";
 		doLog($fname, $comlog);	
 	}
