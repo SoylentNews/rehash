8176a04239322241db10b4f8bef51573a55483af
Tim Vroom <vroom@blockstackers.com>
2004-01-27 18:29:11 +0000

    Allow preview so they can post after time limit
    tracks internal commit 8c4b3160ef43984f5c4745b06289e9e5f0d2f33a

--- Slash/Utility/Access/Access.pm
+++ Slash/Utility/Access/Access.pm
@@ -238,6 +238,8 @@ sub intervalString {
 sub formkeyHandler {
 	# ok, I know we don't like refs, but I don't wanna rewrite the 
 	# whole damned system
+
+
 	my($formkey_op, $formname, $formkey, $message_ref, $options) = @_;
 	my $form = getCurrentForm();
 	my $user = getCurrentUser();
@@ -287,7 +289,8 @@ sub formkeyHandler {
 		# check interval from this attempt to last successful post
 		if (my $interval = $slashdb->checkPostInterval($formname)) {	
 			$msg = formkeyError('speed', $formname, $interval);
-			$error_flag = 1;
+			# give the user a preview form so they can post after time limit
+			$error_flag = -1;
 		}
 	} elsif ($formkey_op eq 'formkey_check') {
 		# check if form already used
