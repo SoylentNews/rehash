e8bc879dd63392846e3ac69b7f5a494915d5992e
Tim Vroom <vroom@blockstackers.com>
2004-03-16 15:57:06 +0000

    Added code for handling user preference changes when the expand/contract buttons are pushed.
    tracks internal commit 7233fcec4a97cb2c10ccf2f8efdf4dccd5824deb

--- themes/slashcode/htdocs/users.pl
+++ themes/slashcode/htdocs/users.pl
@@ -811,9 +811,16 @@ sub showInfo {
 	my $cid_for_time_period = $reader->getVar("min_cid_for_$time_period\_days",'value', 1) || 0;
 	my $admin_time_period_limit = $constants->{admin_daysback_commentlimit} || 100;
 	my $admin_non_time_limit    = $constants->{admin_comment_subsequent_pagesize} || 24;
-
+	
 	my($points, $nickmatch_flag, $uid, $nick);
 	my($mod_flag, $karma_flag, $n) = (0, 0, 0);
+
+	if($admin_flag and defined $form->{show_m2s} or defined $form->{show_m1s}){
+		my $update_hr = {};
+		$update_hr->{m2_with_mod} = $form->{show_m2s} if defined $form->{show_m2s};
+		$update_hr->{mod_with_comm} = $form->{show_m1s} if defined $form->{show_m1s};
+		$slashdb->setUser($user->{uid}, $update_hr);
+	}
 
 	if (!$id && !$form->{userfield}) {
 		if ($form->{uid} && ! $id) {
