a978af5db329b065d7ce42064e808b11b2eb3861
Tim Vroom <vroom@blockstackers.com>
2004-05-28 15:14:57 +0000

    Updated set_cids to keep track of cid for comments that are one day old.
    tracks internal commit b34c192422f2d0664043ae802cc0300a8557411d

--- themes/slashcode/tasks/set_cids.pl
+++ themes/slashcode/tasks/set_cids.pl
@@ -22,27 +22,32 @@ $task{$me}{fork} = SLASHD_NOWAIT;
 # Handles saving useful cids so we can speed up certain selects later.
 $task{$me}{code} = sub {
 	my($virtual_user, $constants, $slashdb, $user) = @_;
-	my $days = $slashdb->getVar("admin_comment_display_days", "value", 1) || 30;
+	
+	my @days = (1);
+	push @days, ($slashdb->getVar("admin_comment_display_days", "value", 1) || 30);
 
-	# Get the old value for this var, or if it hasn't been
-	# defined, create it at the default 0.
-	my $start_at = $slashdb->getVar("min_cid_last_$days\_days", "value", 1);
-	if (!defined($start_at)){
-		$slashdb->createVar("min_cid_last_$days\_days", 0);
-		$start_at ||= 0;
-	}
+	foreach my $days (@days) {
 
-	# Now get the new value for this var;  if the method
-	# doesn't return a new value, then there were no new
-	# comments posted during that period several days ago,
-	# so we just keep using the old value.
-	my $cid = $slashdb->getCidForDaysBack($days, $start_at) || $start_at;
-	my $success = $slashdb->setVar("min_cid_last_$days\_days", $cid);
+		# Get the old value for this var, or if it hasn't been
+		# defined, create it at the default 0.
+		my $start_at = $slashdb->getVar("min_cid_last_$days\_days", "value", 1);
+		if (!defined($start_at)){
+			$slashdb->createVar("min_cid_last_$days\_days", 0);
+			$start_at ||= 0;
+		}
 
-	if ($success) {
-		return "min_cid_last_$days\_days is $cid";
-	} else {
-		return "could not set min_cid_last_$days\_days to $cid";
+		# Now get the new value for this var;  if the method
+		# doesn't return a new value, then there were no new
+		# comments posted during that period several days ago,
+		# so we just keep using the old value.
+		my $cid = $slashdb->getCidForDaysBack($days, $start_at) || $start_at;
+		my $success = $slashdb->setVar("min_cid_last_$days\_days", $cid);
+
+		if ($success) {
+			slashdLog("min_cid_last_$days\_days is $cid");
+		} else {
+			slashdLog("could not set min_cid_last_$days\_days to $cid");
+		}
 	}
 };
 
