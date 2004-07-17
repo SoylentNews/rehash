db200901f7a41c83889eda6cd4ed76f3c4409d24
Jamie McCarthy <jamie@mccarthy.vg>
2004-07-17 16:43:38 +0000

    Smarter sorting of topics for the topiclist;  try to avoid having nexus tids and non-imaged tids up top.
    tracks internal commit 4ad5db550f810a7262d49ba2dfedcb3668d5a170

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -8585,14 +8585,20 @@ sub getTopiclistFromChosen {
 	}
 
 	my @tids = sort {
-			# Highest priority is whether this topic (at this
-			# weight) is in the preferred skid.
-		   $in_skid{$b} <=> $in_skid{$a}
-			# Second priority is the topic's weight
+			# Highest priority is whether this topic is
+			# NOT a nexus (nexus topics go at the end).
+		   (exists $tree->{$a}{nexus} ? 0 : 1) <=> (exists $tree->{$b}{nexus} ? 0 : 1)
+			# Next highest priority is whether this topic
+			# has an icon.
+		|| ($tree->{$a}{image} ? 1 : 0) <=> ($tree->{$b}{image} ? 1 : 0)
+			# Next highest priority is whether this topic
+			# (at this weight) is in the preferred skid.
+		|| $in_skid{$b} <=> $in_skid{$a}
+			# Next priority is the topic's weight
 		|| $chosen_hr->{$b} <=> $chosen_hr->{$a}
-			# Third priority is alphabetical sort
+			# Next priority is alphabetical sort
 		|| $tree->{$a}{textname} cmp $tree->{$b}{textname}
-			# Last priority is primary key sory
+			# Last priority is primary key sort
 		|| $a <=> $b
 	} keys %$chosen_hr;
 	return \@tids;
