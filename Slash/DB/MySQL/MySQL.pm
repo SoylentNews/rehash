4a5b45ea8b5ba0e0eedfdb4d4e0327b28144820d
Jamie McCarthy <jamie@mccarthy.vg>
2004-07-23 14:53:58 +0000

    When deleting a topic, preventing topics from being their own parents would be good.
    tracks internal commit cdba5870d6906eead2887211245e79d18d1c8f86

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -2808,8 +2808,11 @@ sub deleteAuthor {
 sub deleteTopic {
 	my($self, $tid, $newtid) = @_;
 	my $tid_q = $self->sqlQuote($tid);
+	my $newtid_q = $self->sqlQuote($newtid);
 
 	return 0;  # too dangerous to use right now
+
+	# Abort if $tid == $newtid !
 
 	my @delete_tables = qw(
 		topics topic_nexus topic_nexus_dirty topic_nexus_extras
@@ -2844,10 +2847,12 @@ sub deleteTopic {
 			{ ignore => 1 });
 		$self->sqlDelete('topic_parents', "parent_tid=$tid_q");
 		# Second, update the to-be-deleted topic's parents to instead
-		# be pointed-to by its replacement.
+		# be pointed-to by its replacement.  In case one of the topic's
+		# parents _is_ the replacement, don't make it loop to itself
+		# (and the resulting busted row will be deleted, same as above).
 		$self->sqlUpdate('topic_parents',
 			{ tid => $newtid },
-			"tid=$tid_q",
+			"tid=$tid_q AND parent_tid != $newtid_q",
 			{ ignore => 1 });
 		$self->sqlDelete('topic_parents', "tid=$tid_q");
 
