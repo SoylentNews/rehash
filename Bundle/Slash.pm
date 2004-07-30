a9a6eb193a32ee14aa37ee408b8383fe60ec9285
Jamie McCarthy <jamie@mccarthy.vg>
2004-07-30 17:59:55 +0000

    Clarify comment about optional modules
    tracks internal commit 44fe7bb8c6bbc2a5ed5f32903759b21d9f4f1479

--- Bundle/Slash.pm
+++ Bundle/Slash.pm
@@ -115,7 +115,11 @@ Lingua::Stem
 mod_perl must be installed by hand, because of the special configuration
 required for it.
 
-Optional but recommended modules include:  Cache::Memcached Silly::Werder
-GD GD::Text Apache::SSI Apache::RegistryFilter GraphViz
+Our optional modules, which are not required for a default install
+and thus not installed by default, but which may become required as
+you edit your site configuration, are: Cache::Memcached Silly::Werder
+GD GD::Text GD::Graph Apache::SSI Apache::RegistryFilter GraphViz
+
+(should LWP::Parallel above be LWP::Parallel::UserAgent?)
 
 =cut
