b4daa84d77e1b3d97237dce692b2397df5a5c6cb
Jamie McCarthy <jamie@mccarthy.vg>
2004-10-27 15:36:02 +0000

    Slow down, don't do all the people updates at once
    tracks internal commit d57407bb19a4e40135dc9d8c69de2e7b1029ab09

--- plugins/Zoo/zoo_run_people_log.pl
+++ plugins/Zoo/zoo_run_people_log.pl
@@ -32,6 +32,7 @@ $task{$me}{code} = sub {
 	for my $person (@$people) {
 		my $new_people = $zoo->rebuildUser($person);
 		$slashdb->setUser($person, { people => $new_people, people_status => 'ok'});
+		sleep 2; # don't tax the DB too much, this isn't high-priority
 	}
 	$stats->updateStatDaily("zoo_counts", "value+" . @$people);	
 	slashdLog('Zoo fof/eof End');
@@ -40,3 +41,4 @@ $task{$me}{code} = sub {
 };
 
 1;
+
