2f8e64226de4a1d47060feea109d2100a1875ff7
pudge <projects@pudge.net>
2005-04-08 21:27:12 +0000

    Bad anchor in regex for fixing # in URLs
    tracks internal commit d672b7e98016f92ad8b99f7aaeccd0a9d342d107

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -2068,7 +2068,7 @@ sub fudgeurl {
 		if ($url =~ /#/) {
 			# no # is OK, unless ...
 			$url =~ s/#/%23/g;
-			if ($url =~ m|^https?://$|i) {
+			if ($url =~ m|^https?://|i) {
 				# HTTP, in which case the first # is OK
 				$url =~ s/%23/#/;
 			}
