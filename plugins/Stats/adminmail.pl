885af30e5f61c01b1a2f6de86ec61b5195ec2d53
Jamie McCarthy <jamie@mccarthy.vg>
2005-12-08 19:25:26 +0000

    Remove 'uninitialized' warnings
    tracks internal commit cc1408328322a7ddfb2340dd1e615269b1d4b332

--- plugins/Stats/adminmail.pl
+++ plugins/Stats/adminmail.pl
@@ -387,10 +387,10 @@ EOT
 		my $pages = $logdb->countDailyByPage('', $options);
 		my $bytes = $logdb->countBytesByPage('', $options);
 		my $uids = $logdb->countUsersByPage('', $options);
-		$data{"other_uids"} = sprintf("%8u", $uids);
-		$data{"other_ipids"} = sprintf("%8u", $uniq);
-		$data{"other_bytes"} = sprintf("%0.1f MB",$bytes/(1024*1024));
-		$data{"other_page"} = sprintf("%8u", $pages);
+		$data{"other_uids"} = sprintf("%8u", $uids || 0);
+		$data{"other_ipids"} = sprintf("%8u", $uniq || 0);
+		$data{"other_bytes"} = sprintf("%0.1f MB", $bytes/(1024*1024) || 0);
+		$data{"other_page"} = sprintf("%8u", $pages || 0);
 		# Section is problematic in this definition, going to store
 		# the data in "all" till this is resolved. -Brian
 		$statsSave->createStatDaily("other_uids", $uids);
@@ -702,9 +702,9 @@ EOT
 		push @{$data{avg_hits_per_story}}, sprintf("%12.1f", $avg);
 	}
 
-	$data{total} = sprintf("%8u", $daily_total);
-	$data{total_bytes} = sprintf("%0.1f MB",$total_bytes/(1024*1024));
-	$data{grand_total_bytes} = sprintf("%0.1f MB",$grand_total_bytes/(1024*1024));
+	$data{total} = sprintf("%8u", $daily_total || 0);
+	$data{total_bytes} = sprintf("%0.1f MB", $total_bytes/(1024*1024) || 0);
+	$data{grand_total_bytes} = sprintf("%0.1f MB", $grand_total_bytes/(1024*1024) || 0);
 	$data{total_subscriber} = sprintf("%8u", $total_subscriber);
 	$data{total_secure} = sprintf("%8u", $total_secure);
 	$data{unique} = sprintf("%8u", $unique_ips), 
