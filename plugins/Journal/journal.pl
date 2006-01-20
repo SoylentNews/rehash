89e6480d93cff083961f1b2202fff799199220ec
Tim Vroom <vroom@blockstackers.com>
2006-01-20 16:23:13 +0000

    Updates for journal.pl to fix but that wouldn't allow submit to be set on edit after comments had been enabled
    tracks internal commit 2a9647a088b6eb54912684fca9c85cc4d72badfc

--- plugins/Journal/journal.pl
+++ plugins/Journal/journal.pl
@@ -556,7 +556,7 @@ sub doSaveArticle {
 	}
 
 	return(getData('submit_must_enable_comments'), 1)
-		if $form->{submit} && (
+		if $form->{submit} && !$form->{id} (
 			!$form->{journal_discuss}
 				||
 			$form->{journal_discuss} eq 'disabled'
@@ -575,6 +575,8 @@ sub doSaveArticle {
 	if ($form->{id}) {
 		my %update;
 		my $article = $journal_reader->get($form->{id});
+		return(getData('submit_must_enable_comments'), 1) if ($form->{submit} && !$article->{discussion} &&  (!$form->{journal_discuss} || $form->{journal_discuss} eq 'disabled'));
+									
 
 		# note: comments_on is a special case where we are
 		# only turning on comments, not saving anything else
