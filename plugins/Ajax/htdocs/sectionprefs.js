54ed230879d5ee07b4753c8014301663c8225c22
Tim Vroom <vroom@blockstackers.com>
2006-01-21 18:01:06 +0000

    Updated for shortening the title
    tracks internal commit 81ab2382468d58b1a2eee3f4077aea91f05aff8d

--- plugins/Ajax/htdocs/sectionprefs.js
+++ plugins/Ajax/htdocs/sectionprefs.js
@@ -12,7 +12,7 @@ function configSectionPopup() {
 	div.style.width = "auto";
 	div.style.border = "solid 2px #066";
 	div.padding = "5px";
-	div.innerHTML = "<div id=\"sectionprefs_hdr\"><a href=\"javascript: window.location.reload()\" style=\"color:#fff;\">Sectional&nbsp;Display&nbsp;Preferences</a>&nbsp;<span><a href=\"/faq/UI.shtml#ui500\">?</a></span>&nbsp;<span><a href=\"javascript: window.location.reload()\">X</a></span></div><div id='sectionprefs'><div id=\"sectionprefs_message\">Loading...</div>";
+	div.innerHTML = "<div id=\"sectionprefs_hdr\"><a href=\"javascript: window.location.reload()\" style=\"color:#fff;\">Sectional&nbsp;Display&nbsp;Prefs</a>&nbsp;<span><a href=\"/faq/UI.shtml#ui500\">?</a></span>&nbsp;<span><a href=\"javascript: window.location.reload()\">X</a></span></div><div id='sectionprefs'><div id=\"sectionprefs_message\">Loading...</div>";
 	body.appendChild(div);
 	
 	var url = 'ajax.pl';
