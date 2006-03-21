a2e1ac9dc66a778707923bedfed9571cee53050c
Tim Vroom <vroom@blockstackers.com>
2006-03-21 21:57:29 +0000

    Moved resource sql to ResKey PLUGIN
    tracks internal commit 018552f417b198e4c39da900e46132bbf65950a7

--- plugins/Bookmark/mysql_dump.sql
+++ plugins/Bookmark/mysql_dump.sql
@@ -1,4 +1,3 @@
-INSERT IGNORE INTO reskey_resources VALUES (8, 'bookmark');
 INSERT IGNORE INTO reskey_vars VALUES (8,'duration_creation-use', 2, 'min duration (in seconds) between creation and use');
 INSERT IGNORE INTO reskey_vars VALUES (8,'user_seclev', 2, 'Minimum seclev to use resource');
 INSERT IGNORE INTO reskey_vars VALUES (8, 'duration_uses', 30, 'min duration (in seconds) between uses');
