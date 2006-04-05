8aa36b337c3a02a79c28bd1d73f304a8c8494b5c
Tim Vroom <vroom@blockstackers.com>
2006-04-05 18:09:47 +0000

    Updates for reskey checks
    tracks internal commit bbc6dbd11b16b3c18277014f1d029a939d3e87f2

--- plugins/Bookmark/mysql_dump.sql
+++ plugins/Bookmark/mysql_dump.sql
@@ -3,5 +3,9 @@ INSERT IGNORE INTO reskey_vars VALUES (8,'user_seclev', 2, 'Minimum seclev to us
 INSERT IGNORE INTO reskey_vars VALUES (8, 'duration_uses', 30, 'min duration (in seconds) between uses');
 INSERT IGNORE INTO reskey_vars VALUES (8, 'duration_max-uses', '30', 'how many uses per timeframe');
 
+INSERT INTO `reskey_resource_checks` (`rkrcid`, `rkrid`, `type`, `class`, `ordernum`) VALUES (NULL, 8,'use','Slash::ResKey::Checks::Post',151);
+INSERT INTO `reskey_resource_checks` (`rkrcid`, `rkrid`, `type`, `class`, `ordernum`) VALUES (NULL, 8,'all','Slash::ResKey::Checks::Duration',601);
+INSERT INTO `reskey_resource_checks` (`rkrcid`, `rkrid`, `type`, `class`, `ordernum`) VALUES (NULL ,8,'all','Slash::ResKey::Checks::User',101);
+
 INSERT INTO vars (name, description, value) VALUES ('bookmark_popular_days', "Number of days back to look for popular bookmarks", 1);
 INSERT INTO vars (name, description, value) VALUES ('bookmark_allowed_schemes', "Schemes to allow for bookmarks.  Separate entries with |", "http|https");
