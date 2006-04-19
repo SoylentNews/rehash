d7c3c9a88ad0cd4ec6ef933d36a8e3979db6fdeb
pudge <projects@pudge.net>
2006-04-19 18:16:32 +0000

    Misc. thingys
    tracks internal commit 8f7b7ba76c6fa07b719e978ed6918072183199a4

--- plugins/Admin/Admin.pm
+++ plugins/Admin/Admin.pm
@@ -578,22 +578,22 @@ sub ajax_learnword {
 	my $form = getCurrentForm();
 
 	my $template = $self->getTemplateByName("ispellok", { page => "admin" });
-    if (!$template) {
-        errorLog("Spellcheck: personal dictionary not found.");
-        return;
-    }
+	if (!$template) {
+		errorLog("Spellcheck: personal dictionary not found.");
+		return;
+	}
     
-    my $template_text = $self->sqlSelect("template", "templates", "tpid = " . $template->{tpid});
+	my $template_text = $self->sqlSelect("template", "templates", "tpid = " . $template->{tpid});
 	
-    # Somehow we were called even though the word was found in the personal dictionary. Return.
-    return if ($template_text =~ /\s$form->{'word'}\s/);
+	# Somehow we were called even though the word was found in the personal dictionary. Return.
+	return if ($template_text =~ /\s$form->{'word'}\s/);
     
-    $template_text .= $form->{'word'} . ' ';
+	$template_text .= $form->{'word'} . ' ';
 	my $rows = $self->sqlUpdate("templates", { template => $template_text }, "tpid = " . $template->{tpid});
 
-    if (!$rows) {
-        errorLog("Spellcheck: personal dictionary not updated.");
-    }
+	if (!$rows) {
+		errorLog("Spellcheck: personal dictionary not updated.");
+	}
 }
 
 sub DESTROY {
