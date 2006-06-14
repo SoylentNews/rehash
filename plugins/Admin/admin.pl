40d6fff42fa1b454cb75b09992ec0c5ca3e6c35a
Jamie McCarthy <jamie@mccarthy.vg>
2006-06-14 03:04:53 +0000

    Whitespace
    tracks internal commit 1046d7e8d67b8c051d2d5b5997c9bd80bd753ca1

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -1727,8 +1727,8 @@ sub get_ispell_comments {
 	my $tmptext = write_to_temp_file($text);
 	my $tmpok = "";
 	$tmpok = write_to_temp_file($ok) if $ok;
-        rename($tmpok, lc($tmpok));
-        $tmpok = lc($tmpok);
+	rename($tmpok, lc($tmpok));
+	$tmpok = lc($tmpok);
 	my $tmpok_flag = "";
 	$tmpok_flag = " -p $tmpok" if $tmpok;
 	
@@ -1752,7 +1752,7 @@ sub get_ispell_comments {
 
 	my %misspelled_words = ();
 	foreach my $mWord (keys %misspelled_suggestion) {
-                # Inititally set reference empty in case there are no suggestions.
+		# Inititally set reference empty in case there are no suggestions.
 		$misspelled_words{$mWord} = [];
 		foreach my $suggestion (split(/,\s?/, $misspelled_suggestion{$mWord})) {
 			push(@{$misspelled_words{$mWord}}, $suggestion);
