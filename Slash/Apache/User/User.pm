dfc333691e69a9521610da346b1d2319fac96aa6
Jamie McCarthy <jamie@mccarthy.vg>
2006-07-06 21:53:21 +0000

    more quotes!
    tracks internal commit c29408f4f59637f6e89e205c2590d8b90e94281e

--- Slash/Apache/User/User.pm
+++ Slash/Apache/User/User.pm
@@ -927,6 +927,7 @@ Bender:That's not my gold-plated 25-pin connector.
 Bender:I only know enough binary to ask where the bathroom is.
 Bender:Why would God think in binary?
 Bender:You can't count on God for jack! He pretty much told me so himself.
+Bender:Stop doing the right thing, you jerk!
 Fry:There's a lot about my face you don't know.
 Fry:These new hands are great. I'm gonna break them in tonight.
 Fry:I refuse to testify on the grounds that my organs will be chopped up into a patty.
@@ -970,6 +971,7 @@ Fry:My hands! My horrible, human hands!
 Fry:How about me? I'm human, and I've always wanted to see the future!
 Fry:The less fortunate get all the breaks!
 Fry:Please, Mr. Nixon! We're appealing to your sense of decency!
+Fry:I have more important things to do today than laugh and clap my hands.
 Fry:I'll be whatever I wanna do.
 Fry:There's a political debate on. Quick, change the channel!
 Leela:There's a political debate on. Quick, change the channel!
