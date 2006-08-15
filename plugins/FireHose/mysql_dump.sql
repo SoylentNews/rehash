55f83c73e48760bcbfaadddad1f31de4341158a1
Tim Vroom <vroom@blockstackers.com>
2006-08-15 17:53:41 +0000

    Fixed a couple typos
    tracks internal commit ed8eaea5b935fd9e14cb5e2821d3c7b1a0a7bf5b

--- plugins/FireHose/mysql_dump.sql
+++ plugins/FireHose/mysql_dump.sql
@@ -4,9 +4,9 @@
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_fetch_text', 'Slash::FireHoses, 'fetchItemText, 'ajax_base', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'tags_create_for_firehose', 'Slash::FireHose', 'tags_write', 'ajax_base', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_reject', 'Slash::FireHose', 'rejectItem', 'ajax_admin', 'createuse');
-INSERT INTO ajax_ops VALUES (NULL, 'tags_get_user_firehose', 'Slash::Firehose', 'ajaxGetUserFirehose', 'ajax_tags_write', 'createuse');
+INSERT INTO ajax_ops VALUES (NULL, 'tags_get_user_firehose', 'Slash::FireHose', 'ajaxGetUserFirehose', 'ajax_tags_write', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'tags_create_for_firehose', 'Slash::FireHose', 'ajaxCreateForUrl', 'ajax_tags_write', 'use');
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_up_down', 'Slash::FireHose', 'ajaxUpDownFirehose', 'ajax_tags_write', 'createuse');
-INSERT INTO ajax_ops VALUES (NULL, 'tags_get_admin_firehose', 'Slash::Firehose', 'ajaxGetAdminFirehose', 'ajax_admin', 'createuse');
+INSERT INTO ajax_ops VALUES (NULL, 'tags_get_admin_firehose', 'Slash::FireHose', 'ajaxGetAdminFirehose', 'ajax_admin', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_save_note', 'Slash::FireHose', 'ajaxSaveNoteFirehose', 'ajax_admin', 'createuse');
 INSERT INTO css (rel, type, media, file, title, skin, page, admin, theme, ctid, ordernum, ie_cond) VALUES ('stylesheet','text/css','screen, projection','firehose.css','','','firehose','no','',2,0, '');
