10d401ee701f3f16a12bb05a1bb55111c11f25c9
Jamie McCarthy <jamie@mccarthy.vg>
2006-08-15 18:34:43 +0000

    Typofixes, I hope
    tracks internal commit 524fa029dc2962196f93b8d15fb76d106d47e62e

--- plugins/FireHose/mysql_dump.sql
+++ plugins/FireHose/mysql_dump.sql
@@ -1,11 +1,10 @@
 #
 # $Id$
 #
-INSERT INTO ajax_ops VALUES (NULL, 'firehose_fetch_text', 'Slash::FireHoses, 'fetchItemText, 'ajax_base', 'createuse');
-INSERT INTO ajax_ops VALUES (NULL, 'tags_create_for_firehose', 'Slash::FireHose', 'tags_write', 'ajax_base', 'createuse');
+INSERT INTO ajax_ops VALUES (NULL, 'firehose_fetch_text', 'Slash::FireHose', 'fetchItemText, 'ajax_base', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_reject', 'Slash::FireHose', 'rejectItem', 'ajax_admin', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'tags_get_user_firehose', 'Slash::FireHose', 'ajaxGetUserFirehose', 'ajax_tags_write', 'createuse');
-INSERT INTO ajax_ops VALUES (NULL, 'tags_create_for_firehose', 'Slash::FireHose', 'ajaxCreateForUrl', 'ajax_tags_write', 'use');
+INSERT INTO ajax_ops VALUES (NULL, 'tags_create_for_firehose', 'Slash::FireHose', 'ajaxCreateForFirehose', 'ajax_tags_write', 'use');
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_up_down', 'Slash::FireHose', 'ajaxUpDownFirehose', 'ajax_tags_write', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'tags_get_admin_firehose', 'Slash::FireHose', 'ajaxGetAdminFirehose', 'ajax_admin', 'createuse');
 INSERT INTO ajax_ops VALUES (NULL, 'firehose_save_note', 'Slash::FireHose', 'ajaxSaveNoteFirehose', 'ajax_admin', 'createuse');
