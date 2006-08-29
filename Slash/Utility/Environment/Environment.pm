2caca8f79f2e456840526aaab7853ae44161a63f
Jamie McCarthy <jamie@mccarthy.vg>
2006-08-29 19:04:23 +0000

    Add a questioning note about whether the debugHash code is still valuable.
    tracks internal commit ec16f95837ba8bc30f2ef7053bdc5556afc1bd67

--- Slash/Utility/Environment/Environment.pm
+++ Slash/Utility/Environment/Environment.pm
@@ -3336,6 +3336,10 @@ sub getCurrentCache {
 #
 # pass in the regex that contains what a key SHOULD look like (e.g., '^\d+$'),
 # and optionally the original data as a hashref; assign result to variable
+#
+# XXX I am not aware that this has ever been a problem, and I don't
+# know of any sites that use the debughash* vars.  Can we delete
+# this code? - Jamie 2006-08-27
 
 sub debugHash {
 	my($regex, $hash) = @_;
