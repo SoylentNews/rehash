80373f3724b984cf542e2c84e3903ee43d6b2f69
Jamie McCarthy <jamie@mccarthy.vg>
2006-09-27 02:07:59 +0000

    Two new tagboxes to measure activity and popularity of firehose items and update the firehose table with those values.
    tracks internal commit 0afda15570c0fd53bb819a8e7d41447589b2b6bd

---
+++ tagboxes/FHPopularity/FHPopularity.pm
@@ -0,0 +1,172 @@
+#!/usr/bin/perl -w
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2005 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+package Slash::Tagbox::FHPopularity;
+
+=head1 NAME
+
+Slash::Tagbox::FHPopularity - keep track of popularity of firehose entries
+
+=head1 SYNOPSIS
+
+	my $tagbox_tcu = getObject("Slash::Tagbox::FHPopularity");
+	my $feederlog_ar = $tagbox_tcu->feed_newtags($users_ar);
+	$tagbox_tcu->run($affected_globjid);
+
+=cut
+
+use strict;
+
+use Slash;
+use Slash::DB;
+use Slash::Utility::Environment;
+use Slash::Tagbox;
+
+use Data::Dumper;
+
+use vars qw( $VERSION );
+$VERSION = ' $Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+use base 'Slash::DB::Utility';	# first for object init stuff, but really
+				# needs to be second!  figure it out. -- pudge
+use base 'Slash::DB::MySQL';
+
+sub new {
+	my($class, $user) = @_;
+
+	my $plugin = getCurrentStatic('plugin');
+	return undef if !$plugin->{Tags} || !$plugin->{FireHose};
+	my($tagbox_name) = $class =~ /(\w+)$/;
+	my $tagbox = getCurrentStatic('tagbox');
+	return undef if !$tagbox->{$tagbox_name};
+
+	# Note that getTagboxes() would call back to this new() function
+	# if the tagbox objects have not yet been created -- but the
+	# no_objects option prevents that.  See getTagboxes() for details.
+	my %self_hash = %{ getObject('Slash::Tagbox')->getTagboxes($tagbox_name, undef, { no_objects => 1 }) };
+	my $self = \%self_hash;
+	return undef if !$self || !keys %$self;
+
+	bless($self, $class);
+	$self->{virtual_user} = $user;
+	$self->sqlConnect();
+
+	return $self;
+}
+
+sub feed_newtags {
+	my($self, $tags_ar) = @_;
+	my $constants = getCurrentStatic();
+if (scalar(@$tags_ar) < 9) {
+print STDERR "Slash::Tagbox::FHPopularity->feed_newtags called for tags '" . join(' ', map { $_->{tagid} } @$tags_ar) . "'\n";
+} else {
+print STDERR "Slash::Tagbox::FHPopularity->feed_newtags called for " . scalar(@$tags_ar) . " tags " . $tags_ar->[0]{tagid} . " ... " . $tags_ar->[-1]{tagid} . "\n";
+}
+	my $tagsdb = getObject('Slash::Tags');
+
+	# The algorithm of the importance of tags to this tagbox is simple.
+	# 'nod' and 'nix' are important.  Other tags are not.
+	my $upvoteid   = $tagsdb->getTagnameidCreate($constants->{tags_upvote_tagname}   || 'nod');
+	my $downvoteid = $tagsdb->getTagnameidCreate($constants->{tags_downvote_tagname} || 'nix');
+
+	my $ret_ar = [ ];
+	for my $tag_hr (@$tags_ar) {
+		next unless $tag_hr->{tagnameid} == $upvoteid || $tag_hr->{tagnameid} == $downvoteid;
+		my $ret_hr = {
+			affected_id =>	$tag_hr->{globjid},
+			importance =>	1,
+		};
+		# We identify this little chunk of importance by either
+		# tagid or tdid depending on whether the source data had
+		# the tdid field (which tells us whether feed_newtags was
+		# "really" called via feed_deactivatedtags).
+		if ($tag_hr->{tdid})	{ $ret_hr->{tdid}  = $tag_hr->{tdid}  }
+		else			{ $ret_hr->{tagid} = $tag_hr->{tagid} }
+		push @$ret_ar, $ret_hr;
+	}
+	return [ ] if !@$ret_ar;
+
+	# Tags applied to globjs that have a firehose entry associated
+	# are important.  Other tags are not.
+	my %globjs = ( map { $_->{affected_id}, 1 } @$ret_ar );
+	my $globjs_str = join(', ', sort keys %globjs);
+	my $fh_globjs_ar = $self->sqlSelectColArrayref(
+		'globjid',
+		'firehose',
+		"globjid IN ($globjs_str)");
+	return [ ] if !@$fh_globjs_ar; # if no affected globjs have firehose entries, short-circuit out
+	my %fh_globjs = ( map { $_, 1 } @$fh_globjs_ar );
+	$ret_ar = [ grep { $fh_globjs{ $_->{affected_id} } } @$ret_ar ];
+
+print STDERR "Slash::Tagbox::FHPopularity->feed_newtags returning " . scalar(@$ret_ar) . "\n";
+	return $ret_ar;
+}
+
+sub feed_deactivatedtags {
+	my($self, $tags_ar) = @_;
+print STDERR "Slash::Tagbox::FHPopularity->feed_deactivatedtags called: tags_ar='" . join(' ', map { $_->{tagid} } @$tags_ar) .  "'\n";
+	my $ret_ar = $self->feed_newtags($tags_ar);
+print STDERR "Slash::Tagbox::FHPopularity->feed_deactivatedtags returning " . scalar(@$ret_ar) . "\n";
+	return $ret_ar;
+}
+
+sub feed_userchanges {
+	my($self, $users_ar) = @_;
+	my $constants = getCurrentStatic();
+print STDERR "Slash::Tagbox::FHPopularity->feed_userchanges called: users_ar='" . join(' ', map { $_->{tuid} } @$users_ar) .  "'\n";
+
+	# XXX need to fill this in, and check FirstMover feed_userchanges too
+
+	return [ ];
+}
+
+sub run {
+	my($self, $affected_id) = @_;
+	my $constants = getCurrentStatic();
+	my $tagsdb = getObject('Slash::Tags');
+	my $tagboxdb = getObject('Slash::Tagbox');
+
+	# All firehose entries start out with popularity 1.
+	my $popularity = 1;
+
+	# Some target types gain popularity.
+	my($type, $target_id) = $tagsdb->getGlobjTarget($affected_id);
+	my $target_id_q = $self->sqlQuote($target_id);
+	if ($type eq 'journals') {
+		# One user journaled this which basically counts as good as
+		# a bookmark, so that gets it an extra point.
+		$popularity++;
+	} elsif ($type eq 'urls') {
+		# One or more users bookmarked this.  Find out how many and
+		# give it that many extra points.
+		my $count = $self->sqlCount('bookmarks', "url_id=$target_id_q");
+		$popularity += $count;
+	}
+	# There's also 'feed' and 'submission' which don't get additions.
+
+	# Add up nods and nixes.
+	my $upvoteid   = $tagsdb->getTagnameidCreate($constants->{tags_upvote_tagname}   || 'nod');
+	my $downvoteid = $tagsdb->getTagnameidCreate($constants->{tags_downvote_tagname} || 'nix');
+	my $tags_ar = $tagboxdb->getTagboxTags($self->{tbid}, $affected_id, 0);
+	$tagsdb->addCloutsToTagArrayref($tags_ar);
+	for my $tag_hr (@$tags_ar) {
+		my $sign = 0;
+		$sign =  1 if $tag_hr->{tagnameid} == $upvoteid;
+		$sign = -1 if $tag_hr->{tagnameid} == $downvoteid;
+		next unless $sign;
+		$popularity += $tag_hr->{total_clout} * $sign;
+	}
+
+	# Set the corresponding firehose row to have this popularity.
+	my $affected_id_q = $self->sqlQuote($affected_id);
+print STDERR "Slash::Tagbox::FHPopularity->run setting $affected_id to $popularity\n";
+	$self->sqlUpdate('firehose',
+		{ popularity => $popularity },
+		"globjid = $affected_id_q");
+}
+
+1;
+
