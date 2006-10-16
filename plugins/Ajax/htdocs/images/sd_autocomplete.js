af516b86b425110a64cb215207178fe81a990f52
Scott Collins <scc@ScottCollins.net>
2006-10-16 17:56:39 +0000

    now that yui/autocomplete.js handles it, remove special 'POST' handling
    tracks internal commit 7f92ceff442fb1770dfae5384279d58e424de845

--- plugins/Ajax/htdocs/images/sd_autocomplete.js
+++ plugins/Ajax/htdocs/images/sd_autocomplete.js
@@ -1,3 +1,5 @@
+/* _*_ Mode: JavaScript; tab-width: 8; indent-tabs-mode: true _*_ */
+
 YAHOO.namespace("slashdot");
 
 YAHOO.slashdot.gCompleterWidget = null;
@@ -207,79 +209,7 @@ YAHOO.slashdot.topicTags = ["keyword",
     tagsDS.responseType = tagsDS.TYPE_FLAT;
     tagsDS.scriptQueryParam = "prefix";
     tagsDS.scriptQueryAppend = "op=tags_list_tagnames";
-
-tagsDS.doQuery = function(oCallbackFn, sQuery, oParent) {
-    var isXML = (this.responseType == this.TYPE_XML);
-    
-    var sPostData = /* this.scriptURI+"?"+ */ this.scriptQueryParam+"="+sQuery;
-    if(this.scriptQueryAppend.length > 0) {
-        sPostData += "&" + this.scriptQueryAppend;
-    }
-
-    var oResponse = null;
-    
-    var oSelf = this;
-    /**
-     * Sets up ajax request callback
-     *
-     * @param {object} oReq          HTTPXMLRequest object
-     * @private
-     */
-    var responseSuccess = function(oResp) {
-        // Response ID does not match last made request ID.
-        if(!oSelf._oConn || (oResp.tId != oSelf._oConn.tId)) {
-            oSelf.dataErrorEvent.fire(oSelf, oParent, sQuery, oSelf.ERROR_DATANULL);
-            return;
-        }
-//DEBUG
-for(var foo in oResp) {
-}
-        if(!isXML) {
-            oResp = oResp.responseText;
-        }
-        else { 
-            oResp = oResp.responseXML;
-        }
-        if(oResp === null) {
-            oSelf.dataErrorEvent.fire(oSelf, oParent, sQuery, oSelf.ERROR_DATANULL);
-            return;
-        }
-
-        var aResults = oSelf.parseResponse(sQuery, oResp, oParent);
-        var resultObj = {};
-        resultObj.query = decodeURIComponent(sQuery);
-        resultObj.results = aResults;
-        if(aResults === null) {
-            oSelf.dataErrorEvent.fire(oSelf, oParent, sQuery, oSelf.ERROR_DATAPARSE);
-            return;
-        }
-        else {
-            oSelf.getResultsEvent.fire(oSelf, oParent, sQuery, aResults);
-            oSelf._addCacheElem(resultObj);
-            oCallbackFn(sQuery, aResults, oParent);
-        }
-    };
-
-    var responseFailure = function(oResp) {
-        oSelf.dataErrorEvent.fire(oSelf, oParent, sQuery, oSelf.ERROR_DATAXHR);
-        return;
-    };
-    
-    var oCallback = {
-        success:responseSuccess,
-        failure:responseFailure
-    };
-    
-    if(!isNaN(this.connTimeout) && this.connTimeout > 0) {
-        oCallback.timeout = this.connTimeout;
-    }
-    
-    if(this._oConn) {
-        YAHOO.util.Connect.abort(this._oConn);
-    }
-    
-    oSelf._oConn = YAHOO.util.Connect.asyncRequest('POST', this.scriptURI, oCallback, sPostData);
-};
+    tagsDS.queryMethod = "POST";
 
 YAHOO.slashdot.dataSources = [tagsDS, actionsDS, sectionsDS, topicsDS];
 
