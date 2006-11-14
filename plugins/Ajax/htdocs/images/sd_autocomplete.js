10c739cb70609afcde6517139ece89a6afbdb86a
Scott Collins <scc@ScottCollins.net>
2006-11-14 16:21:02 +0000

    move before showing and focusing should eliminate page jumping
    tracks internal commit 7ce3484f47b465cfbaec7b6d1aaca10c7c8a357c

--- plugins/Ajax/htdocs/images/sd_autocomplete.js
+++ plugins/Ajax/htdocs/images/sd_autocomplete.js
@@ -279,8 +279,12 @@ YAHOO.slashdot.AutoCompleteWidget.prototype._show = function( obj, callbackParam
         this._callbackParams._tagDomain = tagDomain;
         this._completer = this._newCompleter(tagDomain);
 
-        YAHOO.util.Dom.removeClass(this._widget, "hidden");
+        var pos = YAHOO.util.Dom.getXY(this._sourceEl);
+        pos[1] += this._sourceEl.offsetHeight;
+        YAHOO.util.Dom.setXY(this._widget, pos);
+
         YAHOO.util.Dom.addClass(this._sourceEl, "ac-source");
+        YAHOO.util.Dom.removeClass(this._widget, "hidden");
 
         if ( this._needsSpareInput() )
           {
@@ -295,10 +299,6 @@ YAHOO.slashdot.AutoCompleteWidget.prototype._show = function( obj, callbackParam
           YAHOO.util.Dom.addClass(this._spareInput, "hidden");
 
         YAHOO.util.Event.addListener(this._textField(), "keyup", this._onTextboxKeyUp, this, true);
-
-        var pos = YAHOO.util.Dom.getXY(this._sourceEl);
-        pos[1] += this._sourceEl.offsetHeight;
-        YAHOO.util.Dom.setXY(this._widget, pos);
       }
   }
 
