22f2011ef4284cf019b4c45b0195e060627b13b1
Tim Vroom <vroom@blockstackers.com>
2006-11-14 20:27:52 +0000

    mysql_dump.sql
    tracks internal commit ee8006c563c0615cfc742aaea78139b50751da14

---
+++ plugins/Console/mysql_dump.sql
@@ -0,0 +1,2 @@
+INSERT INTO ajax_ops VALUES (NULL, 'console_update', 'Slash::Console', 'ajaxConsoleUpdate', 'ajax_admin', 'createuse');
+INSERT INTO css (rel, type, media, file, title, skin, page, admin, theme, ctid, ordernum, ie_cond) VALUES ('stylesheet','text/css','screen, projection','firehose.css','','','console','no','',2,0, '');
