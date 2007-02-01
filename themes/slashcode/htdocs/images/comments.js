0f0c338c390f7ff052c100e3de0d321f90c4fe68
pudge <projects@pudge.net>
2007-02-01 22:21:39 +0000

    remove spurious newlines
    tracks internal commit 81aa7c98ad782b08e7dd78823d5dc07e5e7046d4

--- themes/slashcode/htdocs/images/comments.js
+++ themes/slashcode/htdocs/images/comments.js
@@ -716,7 +716,7 @@ function quoteReply(pid) {
 	this_reply = this_reply.replace(/^\n+/g, "");
 	this_reply = this_reply.replace(/<\/quote>\n*/g, "</quote>\n\n");
 
-	$('postercomment').value = this_reply + "\n\n" + $('postercomment').value;
+	$('postercomment').value = this_reply + $('postercomment').value;
 }
 
 /* utility functions */
