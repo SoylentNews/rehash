f1105ccc4da0598fd3984885b9b90b2dd86b96c4
pudge <projects@pudge.net>
2007-03-29 02:59:02 +0000

    revert to 102 (basically)
    tracks internal commit 9262af463bd452bd2890b2edeb3f357b11333c41

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -279,10 +279,6 @@ sub getFireHoseEssentials {
 	$options ||= {};
 	$options->{limit} ||= 50;
 
-	my $pop;
-	$pop = $self->getMinPopularityForColorLevel($colors->{$options->{color}})
-		if $options->{colors} && $colors->{$options->{color}};
-
 	my($items, $results, $doublecheck) = ([], {}, 0);
 	# for now, only bother to try searchtoo if there is a qfilter value to search on
 	if (!$options->{no_search} && $constants->{firehose_searchtoo} && $options->{qfilter}) {
@@ -438,21 +434,26 @@ sub getFireHoseEssentials {
 		push @where, "createtime >= DATE_SUB(NOW(), INTERVAL $dur_q DAY)";
 	}
 
-	if ($pop) {
-		my $pop_q = $self->sqlQuote($pop);
-		if ($user->{is_admin} && !$user->{firehose_usermode}) {
-			push @where, "editorpop >= $pop_q";
-		} else {
-			push @where, "popularity >= $pop_q";
+	if ($options->{color}) {
+		if ($colors->{$options->{color}}) {
+			my $pop = $self->getMinPopularityForColorLevel($colors->{$options->{color}});
+			my $pop_q = $self->sqlQuote($pop);
+			if ($user->{is_admin} && !$user->{firehose_usermode}) {
+				push @where, "editorpop >= $pop_q";
+			} else {
+				push @where, "popularity >= $pop_q";
+			}
 		}
 	}
 	if ($user->{is_admin}) {
 		my $signoff_label = "sign" . $user->{uid} . "ed";
 
+		if ($options->{unsigned}) {
+			push @where, "signoffs NOT LIKE \"\%$signoff_label\%\"";
+		}
+
 		if ($options->{signed}) {
 			push @where, "signoffs LIKE \"\%$signoff_label\%\"";
-		} elsif ($options->{unsigned}) {
-			push @where, "signoffs NOT LIKE \"\%$signoff_label\%\"";
 		}
 	}
 
