23670e05854b7e81c953ef702a037d28b4953a83
Christopher Brown <cbrown@corp.sourceforge.com>
2007-04-24 19:59:04 +0000

    Added Remarks forwarding.
    tracks internal commit 8513061633ac9e2e87305d76aa0d1d2fdc060c8a

--- themes/slashcode/tasks/im_messages.pl
+++ themes/slashcode/tasks/im_messages.pl
@@ -12,97 +12,111 @@ use Slash::Utility;
 use Net::OSCAR ':standard';
 use Digest::MD5;
 
-use vars qw(%task $me $task_exit_flag);
-
-# Not sure why this would be needed.
-$! = 0;
+use vars qw(%task $me $task_exit_flag $password);
 
 $task{$me}{timespec} = '* * * * *';
 $task{$me}{fork} = SLASHD_NOWAIT;
 $task{$me}{code} = sub {
 
 	my ($virtual_user, $constants, $slashdb, $user) = @_;
-	my $online = 0;
-
-	my $screenname = $constants->{im_screenname} || '';
 	
+	my $screenname = $constants->{im_screenname} || '';
 	if (!length($screenname)) {
-		# No screen name is defined so there's no point to
-		# trying to connect.  But don't quit, since the task
-		# will just restart.  Sleep until parent slashd quits.
 		slashdLog("No im_screenname, so this task would not be useful -- sleeping permanently");
 		sleep 5 while !$task_exit_flag;
-		return ;
+		return;
 	}
 
+	$password = $constants->{im_password} || '';
+	if (!length($password)) {
+		slashdLog("No im_password, so this task would not be useful -- sleeping permanently");
+		sleep 5 while !$task_exit_flag;
+		return;
+	}
+
+	
 	my $code_in_str = join ',', map { "'$_'" } getMessageCodesByType('IM');
-
-	my $sysmessage_code = getMessageCodeByName("System Messages");
-
+	
 	my $im_mode = getMessageDeliveryByName("IM");
 	
+	# Pull out admins for system messages
+	my $sysmessage_code = getMessageCodeByName("System Messages");
+	my $admin_users =
+                $slashdb->sqlSelectColArrayref("uid", "users", "seclev >= '" . $sysmessage_code->{"seclev"} . "'");
+
+	my $online = 0;	
 	my $oscar = Net::OSCAR->new();
 	$oscar->set_callback_auth_challenge(\&auth_challenge);
 	$oscar->set_callback_signon_done(sub { ++$online; });
+	$oscar->set_callback_error(\&error);
 	$oscar->signon(screenname => $screenname, password => undef);
-	$oscar->timeout(30);
-
+	$oscar->timeout(35);
+	
 	my $retry_counter = 0;
+	my $max_remark_id = 0;
+	my $start_time = time();
 	until ($task_exit_flag) {
 		$oscar->do_one_loop();
-		
+
 		if (!$online) {
 			# Wait until we're fully online (signon_done() is called).
-			# Try to connect once per second for 20 iterations. It's
-			# probably safe to assume there's a network problem if we
-			# connect by then, so exit until we're restarted.
-			if ($retry_counter++ == 20) {
-				slashdLog("Exceeded 20 connect retries. Exiting.");
-				last;
+			# Try to connect once per second for 15 iterations.
+			if ($retry_counter++ == 15) {
+				slashdLog("Exceeded 15 connect retries. -- exiting.");
+				return;
 			}
 			sleep(1);
 			next;
 		}
-	
-		# Pull out all IM compatible messages	
-		my $messages =
-			$slashdb->sqlSelectAllHashref("id", "id, user, code, message", "message_drop",
-				"code IN ($code_in_str)");
 
-		foreach my $id (keys %$messages) {
-			my $im_names;
-			# If it's System Message, pull out admins. Exclusive to forwarding Jabber messages.
-			if ($messages->{$id}->{"code"} eq $sysmessage_code->{"code"}) {
-				$im_names =
-					$slashdb->sqlSelectColArrayref("uid", "users", "seclev >= '" . $sysmessage_code->{"seclev"} . "'");
-			}
-			else {
-				# Single user
-				push(@$im_names, $messages->{$id}->{"user"});
-			}
+		my %messages;
+		# Pull out all IM compatible messages < 10 minutes old.
+		$messages{'message_drop'} = $slashdb->sqlSelectAllHashref(
+			"id", "id, user, code, message", "message_drop",
+			"(code IN ($code_in_str)) and
+			(UNIX_TIMESTAMP(date) > (UNIX_TIMESTAMP(now()) - 600))");
+		
+		# Pull out remarks and record the last remark seen.
+		$messages{'remarks'} = $slashdb->sqlSelectAllHashref(
+			"rid", "rid, remark", "remarks",
+			"(type = 'system') and
+			(UNIX_TIMESTAMP(time) >= $start_time) and
+			(rid > $max_remark_id)");
 
-			foreach my $name (@$im_names) {
-				# Skip this person if they don't want IM messages for this code.
-				next if ($im_mode != getUserMessageDeliveryPref($name, $messages->{$id}->{"code"}));
-				
-				# Look up IM nick for this uid
-				my $im_name =
-					$slashdb->sqlSelect("value", "users_param", "uid = '" . $name . "' and name = 'aim'");
-				push(@{$messages->{$id}->{"im_name"}}, $im_name) if $im_name;
-			}
-			
-			foreach my $name (@{$messages->{$id}->{"im_name"}}) {
-				#$oscar->send_im($name, $messages->{$id}->{"message"});
-				sleep(1);
-			}
-			
-			#$slashdb->sqlDelete("message_drop", "id = " . $messages->{$id}->{"id"}) if @{$messages->{$id}->{"im_name"}};
+		$max_remark_id = $slashdb->sqlSelect("max(rid)", "remarks");
+		
+		foreach my $message_type (keys %messages) {
+			foreach my $id (sort keys %{$messages{$message_type}}) {
+				if ($message_type eq "remarks") {
+					foreach my $admin_user (@$admin_users) {
+						next if(getUserMessageDeliveryPref($admin_user, $sysmessage_code->{'code'}) != $im_mode);
 
-			delete $messages->{$id};
+						my $nick = getImNick($admin_user);
+						next if !$nick;
+						$oscar->send_im($nick, $messages{$message_type}->{$id}->{'remark'});
+						sleep(2);
+						
+					}
+				}
+
+				if ($message_type eq "message_drop") {
+					next if(getUserMessageDeliveryPref(
+						$messages{$message_type}->{$id}->{'user'},
+						$messages{$message_type}->{$id}->{'code'}) != $im_mode);
+
+					my $nick = getImNick($messages{$message_type}->{$id}->{'user'});
+					next if !$nick;
+
+					#$oscar->send_im($nick, $messages{$message_type}->{$id}->{'message'});
+					#sleep(2);
+					
+					#$slashdb->sqlDelete("message_drop", "id = " . $messages{$message_type}->{$id}->{'id'});
+				}
+			}
 		}
 	}
 
-	$oscar->signoff() if $online;
+	$oscar->signoff();
 	
 	return;
 
@@ -111,7 +125,7 @@ $task{$me}{code} = sub {
 
 sub getMessageCodesByType {
 
-my ($type) = @_;
+my $type = shift;
 my @message_codes = ();
 
 	my $slashdb = getCurrentDB();
@@ -130,7 +144,7 @@ my @message_codes = ();
 
 sub getMessageCodeByName {
 
-my ($name) = @_;
+my $name = shift;
 
 	my $slashdb = getCurrentDB();
 	my $code = $slashdb->sqlSelectHashref("code, seclev", "message_codes", "type = '$name'");
@@ -142,7 +156,7 @@ my ($name) = @_;
 
 sub getMessageDeliveryByName {
 
-my ($name) = @_;
+my $name = shift;
 
 	my $slashdb = getCurrentDB();
 	my $code = $slashdb->sqlSelect("code", "message_deliverymodes", "name = '$name'");
@@ -163,17 +177,37 @@ my ($uid, $code) = @_;
 
 }
 
+sub getImNick {
+
+my $uid = shift;
+
+	my $slashdb = getCurrentDB();
+	my $nick = $slashdb->sqlSelect("value", "users_param", "uid = '" . $uid . "' and name = 'aim'");
+
+	return($nick);
+
+}
+
 
 sub auth_challenge {
 
 my ($oscar, $challenge, $hash) = @_;
 
-	my $password = getCurrentStatic('im_password');
 	my $md5 = Digest::MD5->new;
 	$md5->add($challenge);
 	$md5->add($password);
 	$md5->add($hash);
 	$oscar->auth_response($md5->digest, 1);
+
+}
+
+
+sub error {
+
+my($oscar, $connection, $errno, $error, $fatal) = @_;
+
+	slashdLog("Received error '$error'");
+	$task_exit_flag = 1 if $fatal;
 
 }
 
