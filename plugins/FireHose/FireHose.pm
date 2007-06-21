8184f601e9c04bee57b0e68a91429c7d3e387be5
pudge <projects@pudge.net>
2007-06-21 17:51:58 +0000

    pagination
    tracks internal commit e92868a2cdddc8b5e4e8f25db894be0d7d7ab1d1

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1106,14 +1106,31 @@ sub ajaxFireHoseGetUpdates {
 	}
 
 
-	my $html2split = slashDisplay("paginate", { contentsonly => 1, day => $last_day , page => $form->{page}, options => $opts, ulid => "fh-paginate", divid => "fh-pag-div", num_items => $num_items, fh_page => $base_page, split_refresh => 1 }, { Return => 1, Page => "firehose"});
+	my $html2split = slashDisplay("paginate", {
+		contentsonly	=> 1,
+		day		=> $last_day,
+		page		=> $form->{page},
+		options		=> $opts,
+		ulid		=> "fh-paginate",
+		divid		=> "fh-pag-div",
+		num_items	=> $num_items,
+		fh_page		=> $base_page,
+		split_refresh	=> 1
+	}, { Return => 1, Page => "firehose" });
 
 	my ($beforewidget, $afterwidget) = split('<!-- split -->', $html2split);
 
 	$html->{beforewidget} = $beforewidget;
 	$html->{afterwidget} = $afterwidget;
 	
-	$html->{firehose_pages} = slashDisplay("firehose_pages", { page => $form->{page}, num_items => $num_items, fh_page => $base_page, options => $opts, contentsonly => 1}, { Return => 1});
+	$html->{firehose_pages} = slashDisplay("firehose_pages", {
+		page		=> $form->{page},
+		num_items	=> $num_items,
+		fh_page		=> $base_page,
+		options		=> $opts,
+		contentsonly	=> 1,
+		search_results	=> $results
+	}, { Return => 1 });
 
 	$html->{local_last_update_time} = timeCalc($slashdb->getTime(), "%H:%M");
 	$html->{gmt_update_time} = " (".timeCalc($slashdb->getTime(), "%H:%M", 0)." GMT) " if $user->{is_admin};
@@ -1964,8 +1981,9 @@ sub listView {
 		tabs		=> $options->{tabs},
 		slashboxes	=> $Slashboxes,
 		last_day	=> $last_day,
-		fh_page		=> $base_page
-	}, { Page => "firehose", Return => 1});
+		fh_page		=> $base_page,
+		search_results	=> $results
+	}, { Page => "firehose", Return => 1 });
 }
 
 sub setFireHoseSession {
