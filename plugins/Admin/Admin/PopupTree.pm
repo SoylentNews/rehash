4bf74c00fb6c1664f6aaf2b24a02277538241d73
Jamie McCarthy <jamie@mccarthy.vg>
2007-07-10 00:11:18 +0000

    Construct popup tree correctly, with negative min_weight connection links between topics not adding links in this tree.
    tracks internal commit 5b0d17b946af5612236bd436d99973216ef7400a

--- plugins/Admin/Admin/PopupTree.pm
+++ plugins/Admin/Admin/PopupTree.pm
@@ -67,7 +67,19 @@ sub getPopupTree {
 	my($stid, $options, $param) = @_;
 	my $reader	= getObject('Slash::DB', { db_type => 'reader' });
 	my $constants	= getCurrentStatic();
-	my $tree	= $reader->getTopicTree;
+	my $tree_withpg	= $reader->getTopicTree;
+
+	my $tree = { };
+	for my $tid (keys %$tree_withpg) {
+		$tree->{$tid} = $tree_withpg->{$tid}
+			if $tid # just in case someone added a bad tid
+			&& (
+				# filter out product guide topics
+				$tid < ($constants->{product_guide_tid_lower_limit} || 10_000)
+				||
+				$tid > ($constants->{product_guide_tid_upper_limit} || 20_000)
+			);
+	}
 
 	$param   ||= {};
 	$options ||= {};
@@ -75,30 +87,79 @@ sub getPopupTree {
 	$constants->{topic_popup_open} = 1 unless defined $constants->{topic_popup_open};
 
 	my $data;
+	my %topics_added;
+	my @tids =	map  { $_->[0] }
+			sort { $a->[1] cmp $b->[1] }
+			map  { [ $_, lc $tree->{$_}{textname} ] }
+			keys %$tree;
+
+	# Set up %topics except for parents and children
 	my %topics;
-	for my $tid (map  { $_->[0] }
-	             sort { $a->[1] cmp $b->[1] }
-	             map  { [ $_, lc $tree->{$_}{textname} ] } keys %$tree) {
-		next unless $tid && (	# just in case someone added a bad tid
-			# filter out product guide topics
-			$tid < ($constants->{product_guide_tid_lower_limit} || 10_000)
-				||
-			$tid > ($constants->{product_guide_tid_upper_limit} || 20_000)
-		);
-		my $top = $tree->{$tid};
+	for my $tid (@tids) {
+		my $topic = $tree->{$tid};
 		@{$topics{$tid}}{qw(value label height width image open)} = (
-			$tid, @{$top}{qw(textname height width image)},
+			$tid, @{$topic}{qw(textname height width image)},
 			$constants->{topic_popup_open} ? 1 : 0
 		);
+	}
 
-		if (scalar keys %{$top->{parent}}) {
-			$topics{$tid}{parent} = $top->{parent};
-			for my $pid (keys %{$top->{parent}}) {
-				push @{$topics{$pid}{children}}, $topics{$tid};
+	while (1) {
+#print STDERR "getPopupTree added=" . scalar(keys %topics_added) . " tree=" . scalar(@tids) . "\n";
+		last if scalar(keys %topics_added) == scalar(keys %$tree);
+		my $topic;
+		for my $tid (@tids) {
+			next if $topics_added{$tid}; # skip if already added
+			$topic = $tree->{$tid};
+
+			my $linked_in = 0;
+			if (scalar keys %{$topic->{parent}}) {
+				$topics{$tid}{parent} = $topic->{parent};
+				for my $pid (keys %{$topic->{parent}}) {
+					# Don't include topics whose connection to their
+					# parent is negative, i.e. which forbid their
+					# parent topic.
+#print STDERR "topic tid='$topic->{tid}' parent='$pid' conn='$topic->{parent}{$pid}'\n";
+					next if $topic->{parent}{$pid} < 0;
+					push @{$topics{$pid}{children}}, $topics{$tid};
+					$linked_in = 1;
+				}
+			} else {
+#print STDERR "topic tid='$topic->{tid}' ROOT\n";
+				push @$data, $topics{$tid};
+				$linked_in = 1;
 			}
-		} else {
-			push @$data, $topics{$tid};
+			$topics_added{$tid} = 1 if $linked_in;
+#print STDERR "topic tid='$topic->{tid}' NOT_LINKED_IN\n" if !$linked_in;
 		}
+		# Some topics may not have been added.  Start a new subtree
+		# at the bottom and continue.
+		# To find the root of the new subtree, start by taking the
+		# first numerical tid that hasn't been added.
+		my @not_added = (
+			sort { $a <=> $b }
+			grep { !$topics_added{$_} }
+			@tids
+		);
+#print STDERR "not_added: '@not_added'\n";
+		next unless @not_added;
+		my $tid = $not_added[0];
+		$topic = $tree->{$tid};
+		# Then walk up the tree until we get to the top of a subtree
+		# that hasn't been added.
+		while (keys %{$topic->{parent}}) {
+			my $pid = (
+				sort { $a <=> $b }
+				grep { !$topics_added{$_} }
+				keys %{$topic->{parent}}
+			)[0];
+			last unless $pid;
+			$tid = $pid;
+			$topic = $tree->{$tid};
+		}
+#print STDERR "top-not-added: '$tid'\n";
+		# Then add it and continue.
+		push @$data, $topics{$tid};
+		$topics_added{$tid} = 1;
 	}
 
 	# children
