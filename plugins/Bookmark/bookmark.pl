bc17b4f75f9d2bda3d5ab0a22458c39f74a36c56
Tim Vroom <vroom@blockstackers.com>
2007-07-12 14:18:05 +0000

    Fixed error in error-handling
    tracks internal commit a3b192dd3656883e08ef895e82a8f6f28a968a6b

--- plugins/Bookmark/bookmark.pl
+++ plugins/Bookmark/bookmark.pl
@@ -81,7 +81,7 @@ sub saveBookmark {
 	my $rkey = $reskey->key('bookmark');
 	my $bookmarkoptions;
 
-	$bookmarkoptions->{errors}{invalidurl}    = !validUrl($form->{url});
+	$bookmarkoptions->{errors}{invalidurl}    = 1 if !validUrl($form->{url});
 	$bookmarkoptions->{errors}{missingfields} = 1 if !$form->{url} || !$form->{title} || !$form->{tags};
 	
 	if (!$bookmarkoptions->{errors}) {
