533f21c78ca5db11874aea0765715e18dc61c604
Scott Collins <scc@ScottCollins.net>
2007-07-17 22:11:42 +0000

    activating nod/nix menus
    tracks internal commit ca185a35361f346f41056f8c4abd7a153c2032c9

--- plugins/Ajax/htdocs/images/common.js
+++ plugins/Ajax/htdocs/images/common.js
@@ -659,10 +659,18 @@ function get_nix_menu() {
 }
 
 
+
 var g_elem_for_pending_showmenu = null;
 var g_menu_for_pending_showmenu = null;
+var g_id_for_pending_showmenu = null;
 var g_pending_showmenu = null;
 var g_pending_hidemenu = null;
+
+var g_nodnix_item_id = null;
+
+function nodnix_tag( tag ) {
+  createTag(tag, g_nodnix_item_id, "firehose");
+}
 
 function hide_nod_menu() {
 	get_nod_menu().style.display = 'none';
@@ -677,6 +685,8 @@ function hide_nodnix_menu( delay ) {
 		hide_nod_menu();
 		hide_nix_menu();
 	} else {
+	  if ( g_pending_hidemenu )
+	    clearTimeout(g_pending_hidemenu);
 		g_pending_hidemenu = setTimeout("hide_nodnix_menu()", delay);
 	}
 }
@@ -688,16 +698,24 @@ function dont_hide_nodnix_menu() {
 
 
 
-function show_nodnix_menu(elem, menu, delay) {
-	if ( delay == undefined || !delay ) {
+function show_nodnix_menu(elem, id, menu, show_delay, hide_delay) {
+	if ( show_delay == undefined || !show_delay ) {
 		var pos = YAHOO.util.Dom.getXY(elem);
 		menu.style.display = 'block';
 		YAHOO.util.Dom.setXY(menu, pos);
+		g_nodnix_item_id = id;
 	} else {
 		g_elem_for_pending_showmenu = elem;
 		g_menu_for_pending_showmenu = menu;
-		g_pending_showmenu = setTimeout("show_nodnix_menu(g_elem_for_pending_showmenu, g_menu_for_pending_showmenu)", delay);
+		g_id_for_pending_showmenu = id;
+		if ( g_pending_showmenu )
+		  clearTimeout(g_pending_showmenu);
+		g_pending_showmenu = setTimeout("show_nodnix_menu(g_elem_for_pending_showmenu, g_id_for_pending_showmenu, g_menu_for_pending_showmenu)", show_delay);
 	}
+
+  if ( hide_delay != undefined && hide_delay != 0 ) {
+    hide_nodnix_menu(hide_delay);
+  }
 }
 
 function dont_show_nodnix_menu() {
@@ -705,14 +723,14 @@ function dont_show_nodnix_menu() {
 	g_pending_showmenu = null;
 }
 
-function show_nod_menu(elem, delay) {
+function show_nod_menu(elem, id, show_delay, hide_delay) {
 	hide_nix_menu();
-	show_nodnix_menu(elem, get_nod_menu(), delay);
+	show_nodnix_menu(elem, id, get_nod_menu(), show_delay, hide_delay);
 }
 
-function show_nix_menu(elem, delay) {
+function show_nix_menu(elem, id, show_delay, hide_delay) {
 	hide_nod_menu();
-	show_nodnix_menu(elem, get_nix_menu(), delay);
+	show_nodnix_menu(elem, id, get_nix_menu(), show_delay, hide_delay);
 }
 
 
