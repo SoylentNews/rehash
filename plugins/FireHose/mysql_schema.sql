b17f41b135a64cf9be92cb41874395a157812ec1
Jamie McCarthy <jamie@mccarthy.vg>
2007-08-19 17:46:22 +0000

    Should have committed this a long time ago
    tracks internal commit 0ec6244c43a98971661fa46ca9ffb1fa653fff8b

--- plugins/FireHose/mysql_schema.sql
+++ plugins/FireHose/mysql_schema.sql
@@ -40,7 +40,14 @@ CREATE TABLE firehose (
 	KEY createtime (createtime),
 	KEY popularity (popularity),
 	KEY popularity2 (popularity2)
-) TYPE=InnoDB; 
+) TYPE=InnoDB;
+
+# The table giving an Object's Generally Accepted Story Publication Time
+CREATE TABLE firehose_ogaspt (
+	globjid		int(10) unsigned NOT NULL default '0',
+	pubtime		datetime NOT NULL default '0000-00-00 00:00:00',
+	PRIMARY KEY	(globjid)
+) TYPE=InnoDB;
 
 DROP TABLE IF EXISTS firehose_text;
 CREATE TABLE firehose_text(
