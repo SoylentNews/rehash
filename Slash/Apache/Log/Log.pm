0a77c90983e7b5cba732ad616f2f2c5da0737970
Jamie McCarthy <jamie@mccarthy.vg>
2007-08-30 21:28:48 +0000

    Comment regarding use of Apache::DBI
    tracks internal commit 78ea739d91befae29170ee6d686fc4c7875a39d1

--- Slash/Apache/Log/Log.pm
+++ Slash/Apache/Log/Log.pm
@@ -15,6 +15,17 @@ use vars qw($VERSION);
 # AMY: Leela's gonna kill me.
 # BENDER: Naw, she'll probably have me do it.
 
+# Note regarding Apache::DBI:
+# Slash does not use Apache::DBI (it's just an extra, unnecessary,
+# layer of dbh caching).  However, some sites may use it, perhaps for
+# other database-backed projects on the same web server.  Apache::DBI
+# pushes a PerlCleanupHandler with every created dbh, to roll back
+# any open transactions.  The docs for Apache.pm say a handler added
+# with push_handlers will be called "before any configured handlers,"
+# i.e. before Slash's PerlCleanupHandler, i.e. before the functions below.
+# Since createLog and setUser don't leave any transactions open, this
+# should not be a problem.
+
 #
 # This is the first PerlCleanupHandler in each site's .conf file
 # (as written by install-slashsite).  This handler writes to the
