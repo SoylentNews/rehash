77fe2bd27e2cfe508ded4645a2492e6f8e3a02e2
Scott Collins <scc@ScottCollins.net>
2008-01-16 18:38:30 +0000

    _still_ dangling keyup handler; temporary fix until I can dig deeper
    tracks internal commit 9599ec8fc6ff1c4d658340f966626bf2d463435b

--- plugins/Ajax/htdocs/images/sd_autocomplete.js
+++ plugins/Ajax/htdocs/images/sd_autocomplete.js
@@ -508,7 +508,11 @@ YAHOO.slashdot.AutoCompleteWidget.prototype._onTextboxKeyUp = function( e, me )
           me._hide();
           break;
         case 13:
-          me._completer.unmatchedItemSelectEvent.fire(me._completer, me, me._completer._sCurQuery);
+        		// I'm sorry to say we have to test first, something somehow somewhere can still leave
+        		//	leave this listener dangling; want to look deeper into this, as this would _still_
+        		//	leave the listener dangling
+        	if ( me._completer )
+          	me._completer.unmatchedItemSelectEvent.fire(me._completer, me, me._completer._sCurQuery);
           break;
         default:
           if ( me._pending_hide )
