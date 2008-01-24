e510ea50313a8cee6ed403171a91733b2009eacd
Jamie McCarthy <jamie@mccarthy.vg>
2008-01-24 18:27:54 +0000

    More logging
    tracks internal commit d32c6a82b36f930150fd655af33794de4e5a2ee8

--- plugins/Tags/tagbox.pl
+++ plugins/Tags/tagbox.pl
@@ -171,6 +171,8 @@ sub update_feederlog {
 	my $clout_types = $tagsdb->getCloutTypes();
 	for my $tagbox (@$tagboxes) {
 
+main::tagboxLog("update_feederlog name=$tagbox->{name} tbid=$tagbox->{tbid} objtbid=$tagbox->{object}{tbid} last=$tagbox->{last_tagid_logged}");
+
 		my @tags_copy = @$tags_ar;
 		my $clout_type = $clout_types->{ $tagbox->{clid} };
 		$tagsdb->addCloutsToTagArrayref(\@tags_copy, $clout_type);
@@ -193,6 +195,7 @@ sub update_feederlog {
 				$feeder_ar = undef;
 				$feeder_ar = $tagbox->{object}->feed_newtags($tags_this_tagbox_ar);
 				# XXX optimize by consolidating here: sum importances, max tagids
+main::tagboxLog("update_feederlog name=$tagbox->{name} inserting " . ($feeder_ar ? scalar(@$feeder_ar) : 'none'));
 				insert_feederlog($tagbox, $feeder_ar) if $feeder_ar;
 				# XXX The previous insert and this update should be wrapped
 				# in a transaction.
@@ -279,7 +282,7 @@ sub update_feederlog {
 sub insert_feederlog {
 	my($tagbox, $feeder_ar) = @_;
 	for my $feeder_hr (@$feeder_ar) {
-#print STDERR "addFeederInfo: tbid=$tagbox->{tbid} tagid=$feeder_hr->{tagid} affected_id=$feeder_hr->{affected_id} imp=$feeder_hr->{importance}\n";
+main::tagboxLog("addFeederInfo: tbid=$tagbox->{tbid} tagid=$feeder_hr->{tagid} affected_id=$feeder_hr->{affected_id} imp=$feeder_hr->{importance}");
 		$tagboxdb->addFeederInfo($tagbox->{tbid}, $feeder_hr);
 	}
 }
