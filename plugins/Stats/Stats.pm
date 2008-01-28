215fdfc34cbb3cb59ea8cf416b47622b58504e07
Jamie McCarthy <jamie@mccarthy.vg>
2008-01-28 14:35:50 +0000

    Bugfix for getOpCombinationStats, and minor fix for getTopBadgeURLs
    tracks internal commit ae9f2a8d25cef0bc7b75249107c968641a72dc20

--- plugins/Stats/Stats.pm
+++ plugins/Stats/Stats.pm
@@ -1122,10 +1122,11 @@ sub getOpCombinationStats {
 		push @where, "a$tn.op=" . $self->sqlQuote($hit);
 	}
 	if (@$nohit_ar) {
+		my $tnprev = $tn;
 		++$tn;
 		my $notlist = join ',', map { $self->sqlQuote($_) } @$nohit_ar;
 		$tables[-1] .= " LEFT JOIN accesslog_temp_uidip AS a$tn
-			ON (a$tn.uidip=a1.uidip AND a$tn.op IN ($notlist))";
+			ON (a$tn.uidip=a$tnprev.uidip AND a$tn.op IN ($notlist))";
 		push @where, "a$tn.op IS NULL";
 	}
 	my $tables = join ', ', @tables;
@@ -1750,7 +1751,7 @@ sub getTopBadgeURLs {
 		$qs =~ s/^.*url=//;
 		$qs =~ s/\%[a-fA-F0-9]?$//;
 		$qs =~ s/%([a-fA-F0-9]{2})/pack('C', hex($1))/ge;
-		$duple = [$qs, $c];
+		$duple = [fudgeurl($qs), $c];
 	}
 	return $top_ar;
 }
@@ -1764,14 +1765,18 @@ sub getTopBadgeHosts {
 	for my $duple (@$top_ar) {
 		my($uri, $c) = @$duple;
 		my $uri_obj = URI->new($uri);
-		my $host = $uri_obj && $uri_obj->can('host') ? $uri_obj->host() : $uri;
+		my $host = $uri_obj && $uri_obj->can('host') ? lc($uri_obj->host()) : $uri;
+		$host =~ s/^www\.//;
 		$count{$host} += $c;
 	}
-	my @top_hosts = (sort { $count{$b} <=> $count{$a} || $a <=> $b } keys %count);
+	my @top_hosts = (sort { $count{$b} <=> $count{$a} || $a cmp $b } keys %count);
 	$#top_hosts = $count-1 if scalar @top_hosts > $count;
 	my @top = ( map { [ $_, $count{$_} ] } @top_hosts );
 	return \@top;
 }
+
+# Not much point in anon_only option, at least not right now, since
+# bookmarks don't get created anonymously at the moment.
 
 sub getNumBookmarks {
 	my($self, $options) = @_;
