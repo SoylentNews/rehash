4af1cd58ec877aaa7eeed5575a6f620345d4bdda
pudge <projects@pudge.net>
2008-04-11 22:29:16 +0000

    enable ProxyScan and HumanConf checks for submit
    tracks internal commit 970dc72596d13f249a4c783ef9df15c6a8c457b2

---
+++ plugins/ResKey/ResKey/Checks/ProxyScan.pm
@@ -0,0 +1,47 @@
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2005 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+package Slash::ResKey::Checks::ProxyScan;
+
+use warnings;
+use strict;
+
+use Slash::Utility;
+use Slash::Constants ':reskey';
+
+use base 'Slash::ResKey::Key';
+
+our($VERSION) = ' $Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+sub doCheck {
+	my($self) = @_;
+
+	if (!$ENV{GATEWAY_INTERFACE}) {
+		return RESKEY_SUCCESS;
+	}
+
+	my $reader = getObject('Slash::DB', { db_type => 'reader' });
+	my $user = getCurrentUser();
+	my $check_vars = $self->getCheckVars;
+
+	if ($check_vars->{adminbypass} && $user->{is_admin}) {
+		return RESKEY_SUCCESS;
+	}
+
+	unless ($reader->checkAL2($user->{srcids}, 'trusted')) {
+		my $is_proxy = $reader->checkForOpenProxy($user->{srcids}{ip});
+		if ($is_proxy) {
+			return(RESKEY_DEATH, ['open proxy', {
+				unencoded_ip	=> $ENV{REMOTE_ADDR},
+				port		=> $is_proxy,
+			}]);
+		}
+	}
+
+	return RESKEY_SUCCESS;
+}
+
+
+1;
