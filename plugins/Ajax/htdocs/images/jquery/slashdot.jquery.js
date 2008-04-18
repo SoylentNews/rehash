634876b3bae684e9e68890b1054d54e5079e4544
Scott Collins <scc@ScottCollins.net>
2008-04-18 13:56:09 -0400

    Adding jquery-related utils for converting between dictionaries, arrays, and strings
    tracks internal commit 64afb8fe84cc07198d3967e4cc2ed93fc4e60c6c

--- plugins/Ajax/htdocs/images/jquery/slashdot.jquery.js
+++ plugins/Ajax/htdocs/images/jquery/slashdot.jquery.js
@@ -37,3 +37,62 @@ jQuery.fn.extend({
 	}
 
 });
+
+
+function split_if_string( list, sep ){
+	return typeof list === 'string' ? list.split(sep || /\s+/) : list
+}
+
+
+function join_wrap( a, elem_prefix, elem_suffix, list_prefix, list_suffix ) {
+	var result = '';
+	if ( a.length ) {
+		var ep = elem_prefix || '';
+		var es = elem_suffix || '';
+							// Example:
+		result = (list_prefix || '') + ep	// '<ul><li>'
+			+ a.join(es+ep)			// .join('</li><li>')
+			+ es + (list_suffix || '');	// '</li></ul>
+	}
+	return result;
+}
+
+
+function map_list_to_set( list, map_fn ){
+	if ( !list || !list.length )
+		return;
+
+	list = split_if_string(list);
+
+	if ( map_fn === undefined )
+		map_fn = function(x){return x};
+
+	var set = {};
+	$.each(list, function(){
+		var k = map_fn(this);
+		if ( k !== undefined && k !== null )
+			set[k] = true;
+	});
+	return set;
+}
+
+
+function map_set_to_list( set, map_fn ){
+	var list = [];
+	$.each(set, function(k, v){
+		var x = map_fn(k, v);
+		if ( x !== undefined && x !== null )
+			list.push(x)
+	});
+	return list;
+}
+
+
+function keys( dict ){
+	return map_set_to_list(dict, function(k, v){return k})
+}
+
+
+function values( dict ){
+	return map_set_to_list(dict, function(k, v){return v})
+}
