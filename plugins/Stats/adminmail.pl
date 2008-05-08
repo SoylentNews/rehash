c815e0357919bdd345dbcf3a1308c746752ad834
pudge <projects@pudge.net>
2008-05-07 22:59:55 -0700

    d2asp updates
    Make d2asp accept a URL, use a new JavaScript function call, and "work" for
    non-existent URLs.
    
    tracks internal commit f9a8a582854d816fab217584cddae9fcff384cb6

--- plugins/Stats/adminmail.pl
+++ plugins/Stats/adminmail.pl
@@ -2,7 +2,6 @@
 # This code is a part of Slash, and is released under the GPL.
 # Copyright 1997-2005 by Open Source Technology Group. See README
 # and COPYING for more information, or see http://slashcode.com/.
-# $Id$
 
 use strict;
 use Slash::Constants qw( :messages :slashd );
