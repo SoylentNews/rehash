a153a6b01501bfaf050127851a1b07a4bda6ef36
Jamie McCarthy <jamie@mccarthy.vg>
2008-06-17 10:13:01 -0400

    Neediness comment scoring
    tagboxes/CommentScoreReason now scores comments for "neediness" --
    how much a moderated comment needs to have its moderations reviewed,
    by admins or high-clout users.
    
    tracks internal commit 3b0d2151a1b479635437558e92953e0070ac0d2a

---
+++ tagboxes/CommentScoreReason/mysql_dump.sql
@@ -0,0 +1,6 @@
+# $Id$
+INSERT INTO tagboxes (tbid, name, affected_type, clid, weight, last_run_completed, last_tagid_logged, last_tdid_logged, last_tuid_logged) VALUES (NULL, 'CommentScoreReason', 'globj', 3, 1, '2000-01-01 00:00:00', 0, 0, 0);
+#INSERT INTO tagbox_userkeyregexes VALUES ('CommentScoreReason', '^tag_clout$');
+
+INSERT INTO vars (name, value, description) VALUES ('tagbox_csr_baseneediness', '60', 'Base neediness score for comments that have been moderated');
+
