e85dfced0e127c502264a75543b702e3ffd0b5b9
Scott Collins <scc@ScottCollins.net>
2008-07-04 13:47:51 -0400

    Teach split_if_string and map_list_to_set to handle edge-cases better
    split_if_string:
    	the `big picture' is that we are building a real array corresponding to
    	a _logical_ list represented in a string; for any other input we are the
    	identity function.  So...
    
    	+ explain both the big picture and the 'identity function' goal
    	+ callers can no longer supply a separator pattern
    	+ return the empty list for a string that is all whitespace
    	+ fix the extra empty-string entries in the result for inputs that have
    		leading/trailing whitespace
    	+ fix the non-identity result when list && !list.length
    	+ dropped try/catch; it wasn't solving the non-identity result problem
    
    map_list_to_set:
    	+ for empty input, return an empty set instead of undefined
    
    map_set_to_list:
    	+ make map_fn optional
    
    keys:
    	+ use map_set_to_list's new default map_fn
    
    throughout the file:
    	+ much better commenting
    	+ adopt my new convention for ';' use
    
    tracks internal commit 8decd272140bc702f5e4941b4f700be7d655c09f

--- plugins/Ajax/htdocs/images/jquery/jquery.slashdot.js
+++ plugins/Ajax/htdocs/images/jquery/jquery.slashdot.js
@@ -39,17 +39,49 @@ jQuery.fn.extend({
 });
 
 
-function split_if_string( list, sep ){
-	try {
-		return list.length ? list.split(sep || /\s+/) : []
-	} catch ( e ) {
-		// if it's not actually splitable, I don't have to do anything
-		return list || []
+/*
+We push around a lot of strings, often using them to represent a lists, e.g.,
+
+	'tag1 tag2 tag3'
+
+is a string representation for the logical list
+
+	['tag1', 'tag2', 'tag3']
+
+split_if_string comes in when you need your "logical list" to really be a
+concrete array of individual strings; building you that _actual_ list.  It is
+also designed to "nest": if the data came to you already in the form of an array
+of strings, split_if_string just hands it right back to you. That is, for any
+input that is _not_ split-able, split_if_string's goal is to be the identity
+function.
+
+Because we're thinking of the logical list implied by your string, there are a
+couple of special cases: the empty string, '', or a string containing only
+whitespace both imply an empty list [], as does any expression that evaluates to
+false. Leading or trailing whitespace in the input list must be ignored.
+JavaScript's split won't give us the answer we want for any of these cases (if
+it did, we'd just call it directly!).
+*/
+function split_if_string( list ){
+	if ( list ) {
+
+		// trim leading/trailing whitespace if we can
+		if ( list.replace  )
+			list = list.replace(/^\s*(.*?)\s*$/, '$1');
+
+		// Were list=='', we would have missed the outer 'if', but
+		// trimming may have _made_ list empty.  That's why we did it
+		// _before_ calling split.
+
+		return list.length && list.split ? list.split(/\s+/) : list || []
 	}
+
+	return []
 }
 
 
 function join_wrap( a, elem_prefix, elem_suffix, list_prefix, list_suffix ) {
+	// always returns a string, even if it's the empty string, ''
 	var result = '';
 	a = split_if_string(a);
 	if ( a && a.length ) {
@@ -60,41 +92,61 @@ function join_wrap( a, elem_prefix, elem_suffix, list_prefix, list_suffix ) {
 			+ a.join(es+ep)			// .join('</li><li>')
 			+ es + (list_suffix || '');	// '</li></ul>
 	}
-	return result;
+	return result
 }
 
 
 function map_list_to_set( list, map_fn ){
-	list = split_if_string(list);
-	if ( !list || !list.length )
-		return;
+	// map_list_to_set is most useful when you need to repeatedly ask "Is 'x'
+	//	in this list?  Is 'y' in this list?", or do set like operations
+	//	on lists, e.g., union, intersection, difference.
 
+	// list (required) can be an array or a string
+	list = split_if_string(list);
+
+	// map_fn (optional) allows you to filter the list
 	if ( map_fn === undefined )
 		map_fn = function(x){return x};
 
+	// always returns a set, even if it's the empty set, {}
 	var set = {};
-	$.each(list, function(){
-		var k = map_fn(this);
-		if ( k !== undefined && k !== null )
-			set[k] = true;
-	});
-	return set;
+	if ( list && list.length ) {
+		$.each(list, function(){
+			var k = map_fn(this);
+			if ( k !== undefined && k !== null )
+				set[k] = true;
+		})
+	}
+	return set
 }
 
 
 function map_set_to_list( set, map_fn ){
+	// map_set_to_list is most useful for getting back to a list after you've
+	//	used sets (made with map_list_to_set) to do a bunch of math.  But
+	//	see also keys() and values(), below.
+
+	// set (required) must be iterable
+
+	// map_fn (optional) allows you to filter the set, and specify what
+	//	actually ends up in the resulting list.  The default is the 'key',
+	//	thus "un-doing" the map_list_to_set operation.
+	if ( map_fn === undefined )
+		map_fn = function(k, v){return k};
+
+	// always returns a list, even if it's the empty list, []
 	var list = [];
 	$.each(set, function(k, v){
 		var x = map_fn(k, v);
 		if ( x !== undefined && x !== null )
 			list.push(x)
 	});
-	return list;
+	return list
 }
 
 
 function keys( dict ){
-	return map_set_to_list(dict, function(k, v){return k})
+	return map_set_to_list(dict)
 }
 
 
