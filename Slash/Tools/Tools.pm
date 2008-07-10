cf76a21d356e0826aa3ffb2d000bf6aa9740b564
pudge <projects@pudge.net>
2008-07-10 16:08:39 -0700

    Allow callers to set BBEdit window type
    tracks internal commit 51cd8c1ee3fade30beba4b816bd7804e23b5f437

--- Slash/Tools/Tools.pm
+++ Slash/Tools/Tools.pm
@@ -21,7 +21,12 @@ our @EXPORT = qw(
 	pmpath pathpm pmpathsrc counterpart srcfile installfile basefile
 	basefile basename dirname
 	syntax_check %CONFIG
+	@BIN_EXT $BIN_EXT $BIN_RE
 );
+
+our @BIN_EXT = qw(gz tgz bz2 gif jpg png ico);
+our $BIN_EXT = join '|', @BIN_EXT;
+our $BIN_RE  = qr/\.(?:$BIN_EXT)$/;
 
 my(%cache);
 # if cache gets stale, you can use force => 0, or heck, just
@@ -292,6 +297,8 @@ sub do {
 	my $output = `$cmd \Q$file\E`;
 	if ($output) {
 		$self->output($output, { title => "$cmd $basename" });
+		$self->front->prop('source_language')->set(to => $opt->{type})
+			if $opt->{type};
 		$self->front->prop('source_language')->set(to => '(none)')
 			if $opt->{notype};
 	}
@@ -323,6 +330,31 @@ sub diff {
 		carp "$src_file or $install_file does not exist";
 	}
 }
+
+
+package Slash::Tools::Mac;
+
+sub new {
+	require MacPerl;
+	shift;
+	my $self = bless { @_ }, __PACKAGE__;
+	$self->{creator} ||= 'R*ch';
+	$self;
+}
+
+sub set_type {
+	my $self = shift;
+	return unless $self->{creator};
+
+	my($file) = @_;
+	return if $file =~ $BIN_RE;
+
+	my($creator, $type) = MacPerl::GetFileInfo($file);
+	return if $creator && $type && $creator eq $self->{creator} && $type eq 'TEXT';
+
+	MacPerl::SetFileInfo($self->{creator}, 'TEXT', $file);
+}
+
 
 1;
 
