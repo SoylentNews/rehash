7bbd153c05d43e27d26e15a839124993514c4f64
Scott Collins <scc@ScottCollins.net>
2008-07-31 09:31:58 -0400

    The nod/nix reasons widget _doesn't_ have to be a special case
    Tag displays now notice when they become empty/non-empty, and mark themselves
    with the CSS class 'no-tags', accordingly.  CSS rules can now hide them.  When
    the user nods/nixs, the context is set and the reasons appear.  When the user
    clicks a reason, the context is set again, but this time to something that
    empties the nod/nix reasons display... and because it's empty, it immediately
    gets marked as such, and the CSS hides it.
    
    Tried to pick some more sensible names for some things.
    
    tracks internal commit c238c77b60b5fc0d97c857c0e27e986a781f723b

--- plugins/Ajax/htdocs/images/tagui.js
+++ plugins/Ajax/htdocs/images/tagui.js
@@ -20,7 +20,7 @@ var tag_server_fns = {
 					//	console.log('broadcast --- "ready", but no handler: ', this);
 				})
 			}
-			style_tags_globally(this, $listeners);
+			recompute_css_classes(this, $listeners);
 		}
 		return this
 	},
@@ -36,11 +36,11 @@ var tag_server_fns = {
 				return this;
 
 
-			// 'harden' the new tags into the user tag-display, but styled 'local-only'
-			// tags in the response from the server will wipe-out local-only
+			// 'harden' the new tags into the user tag-display, but styled 'not-saved'
+			// tags in the response from the server will wipe-out 'not-saved'
 			var command_feedback = normalize_tag_commands(tag_cmds, /*excluding:*/nodnix_commands);
 			$('.tag-display.ready[listen*=user]', this).each(function(){
-				this.update_tags(command_feedback, 'prepend', 'local-only')
+				this.update_tags(command_feedback, 'prepend', 'not-saved')
 			});
 		}
 
@@ -150,7 +150,7 @@ function click_tag( event ) {
 
 		if ( event.shiftKey ) {
 			// if the shift key is down, append the tag to the edit field
-			$server.find('.tag-entry:text:visible').each(function(){
+			$server.find('.tag-entry:text:visible:first').each(function(){
 				if ( this.value ) {
 					var last_char = this.value[ this.value.length-1 ];
 					if ( '-#!)_ '.indexOf(last_char) == -1 )
@@ -175,6 +175,8 @@ var tag_display_fns = {
 
 	// return a dictionary mapping bare tags to the corresponding *.tag DOM element
 	map_tags: function( how ){
+		// map_tags() does not add, remove, or alter any tags
+
 		// we may limit the result, if the caller says how
 		var map_fn;
 		if ( !how )
@@ -206,6 +208,9 @@ var tag_display_fns = {
 	//  optional string, how, tells where to add new tags { 'append', 'prepend' }
 	//  optional string, annotate, tells a css class to add to all touched tags
 	update_tags: function( tags, how, annotate ){
+		// invariant: before.count_tags() <= after.count_tags()
+		// no other call adds tags (except by calling _me_)
+
 		// the intersection of the requested vs. existing tags are the ones I can update in-place
 		var update_map = this.map_tags(tags = list_as_array(tags));
 
@@ -235,22 +240,28 @@ var tag_display_fns = {
 
 			// add in a list of the actual .tag elements we created from scratch
 			$changed_tags = $changed_tags.add( $new_elems.find('.tag') );
+
+			this.mark_if_empty(false);
 		}
 
-		// for every .tag element we touched/created, fix the style to match the kind of tag and add annotate if supplied
-		//   Use case for annotate: the tag was modified locally, we mark it with "local-only" until the server
-		//   comes back with a complete list in response that will wipe out the "local-only" style, essentially
+		// for every .tag we added/changed, fix parent <li>'s css class(es)
+		//   Use case for annotate: the tag was modified locally, we mark it with "not-saved" until the server
+		//   comes back with a complete list in response that will wipe out the "not-saved" class, essentially
 		//   confirming the user's change has been recorded
-		var base_classes = 'tag' + (annotate ? ' '+annotate : '');
 		$changed_tags.each(function(){
-			var style = local_style_for($(this).text());
-			this.className = style ? base_classes + ' ' + style : base_classes
+			var $tag = $(this);
+			$tag.parent()
+				.removeClass()
+				.addClass(static_css_classes_for($tag.text()) + ' ' + (annotate||''));
 		});
 		return this
 	},
 
 
 	remove_tags: function( tags ){
+		// invariant: before.count_tags() >= after.count_tags()
+		// no other call removes tags (except by calling _me_)
+
 		// when called without an argument, removes all tags, otherwise
 		//   tags to remove may be specified by string, an array, or the result of a previous call to map_tags
 		if ( !tags || tags.length )
@@ -259,7 +270,7 @@ var tag_display_fns = {
 		$.each(tags, function(bt, entry){
 			$(entry).parents('li:first').remove()
 		});
-		return this
+		return this.mark_if_empty()
 	},
 
 
@@ -273,11 +284,65 @@ var tag_display_fns = {
 	},
 
 
+	mark_if_empty: function( if_empty ){
+		var $this = $(this);
+		if ( if_empty === undefined )
+			if_empty = ! $this.is(':has(span.tag)');
+		return $this.toggleClassTo('no-tags', if_empty)
+	},
+
+
+	mark_dirty: function( if_dirty ){
+		return $(this).toggleClassTo('dirty', if_dirty)
+	},
+
+
 	receive_broadcast: function( tags ){
-		this.set_tags(tags)
+		return this.set_tags(tags)
 	}
 
 }; // tag_display_fns
+
+function $init_tag_displays( selector, options ){
+	options = options || {};
+
+	var $tag_displays = $(selector);
+
+	// <div listen="user" label="My Tags">tag1 tag2 tag3</div>
+	$tag_displays
+		.each(function(){
+			var $this = $(this);
+
+			var menu_template = join_wrap(
+				$this.attr('menu') || $init_tag_displays.menu_templates[$this.attr('listen')] || '',
+				'<li>', '</li>',
+				'<ul class="tmenu">', '</ul>'
+			);
+			$this.removeAttr('menu');
+
+			var t, legend = (t=$this.attr('label')) ? '<h1 class="legend">' + t + '</h1>' : '';
+			$this.removeAttr('label');
+
+			var tags = $this.text();
+			$this.html(legend+'<ul></ul>');
+
+			$.extend(
+				this,
+				tag_display_fns,
+				{ tag_display_data: {
+					menu_template:	menu_template,
+					$list_el:	$this.find('ul')
+				} },
+				options
+			);
+			$this.addClass('tag-display ready no-tags dirty').removeClass('stub');
+
+			if ( tags ) this.set_tags(tags);
+		})
+		.click(click_tag) // one click-handler per display
+
+	return $tag_displays
+}
 
 
 function cached_user_tags( selector ){
@@ -432,19 +497,18 @@ var tag_widget_fns = {
 		$('.ready[listen=context]', this)
 			.each(function(){
 				this.set_tags(suggestions)
-			})[context && suggestions ? 'show' : 'hide']()
+			})
 	},
 
 
 	open: function(){
 		// $(this).nearest_parent('[tag-server]').addClass('tagging');
-		$(this)
-			.filter(':hidden')
-			.slideDown(100)
-			.find(':text:visible:first')
-				.each(function(){
-					this.focus()
-				});
+		$(this)	.filter(':hidden')
+				.slideDown(100)
+				.find(':text:visible:first')
+					.each(function(){
+						this.focus()
+					});
 		return this
 	},
 
@@ -475,50 +539,6 @@ function open_tag_widget( selector, fetch ){
 
 function close_tag_widget( selector ){
 	return $(selector).filter(':visible').each(function(){this.close()})
-}
-
-
-
-function $init_tag_displays( selector, options ){
-	options = options || {};
-
-	var $tag_displays = $(selector);
-
-	// <div listen="user" label="My Tags">tag1 tag2 tag3</div>
-	$tag_displays
-		.each(function(){
-			var $this = $(this);
-
-			var menu_template = join_wrap(
-				$this.attr('menu') || $init_tag_displays.menu_templates[$this.attr('listen')] || '',
-				'<li>', '</li>',
-				'<ul class="tmenu">', '</ul>'
-			);
-
-			var t, legend = (t=$this.attr('label')) ? '<h1 class="legend">' + t + '</h1>' : '';
-
-			var tags = $this.text();
-			$this.html(legend+'<ul></ul>');
-
-			$.extend(
-				this,
-				tag_display_fns,
-				{ tag_display_data: {
-					menu_template:	menu_template,
-					$list_el:	$this.find('ul')
-				} },
-				options
-			);
-
-			if ( tags ) this.set_tags(tags);
-		})
-		.click(click_tag) // one click-handler per display
-		.addClass('tag-display ready')
-		.removeClass('stub')
-		.removeAttr('menu')
-		.removeAttr('label');
-
-	return $tag_displays
 }
 
 function $init_tag_widgets( selector, options ){
@@ -553,39 +573,14 @@ function add_firehose_nodnix_glue( parent ){
 
 	// XXX do this in a template, and change this to an 'init' type function
 	var $parent = $(parent)
-		.append('<div class="tag-widget nod-nix-reasons stub" style="display:none">' +
+		.append('<div class="tag-widget nod-nix-reasons stub">' +
 				'<div class="tag-display stub" listen="context" />' +
-				'<div class="firehose-listener" listen="vote" style="display:none" />' +
+				'<div class="firehose-listener" listen="vote" />' +
 			'</div>');
 
 	// add a special widget to show the nod/nix suggestions (right in the title bar)
 	var $widgets = $parent.find('.nod-nix-reasons');
-	$init_tag_widgets(
-		$widgets,
-
-		// override tag_widget_fns.set_context so this widget can hide/show based on context
-		{
-			set_context: function( context ){
-				var widget = this;
-				if ( context == 'nod' || context == 'nix' ) {
-					// for nod or nix, show the associated suggestions
-					var suggestions = suggestions_for_context[context];
-					$('.ready[listen=context]', this)
-						.each(function(){
-							this.set_tags(suggestions)
-						})
-						// and depart that context with the very first selection
-						.one("click", function(){
-							widget.set_context(/*no context*/)
-						});
-					widget.open()
-				} else {
-					// for any other context (including no context), be hidden
-					widget.close()
-				}
-			}
-		}
-	);
+	open_tag_widget($init_tag_widgets($widgets));
 
 	// add a 'listener' to fix the up/down vote
 	$widgets.find('.firehose-listener[listen=vote]')
@@ -631,24 +626,24 @@ var context_triggers = map_list_to_set(['nod', 'nix', 'submission','journal','bo
 var well_known_tags = {};
 
 $(function(){
-	update_style_map(well_known_tags, 's1', YAHOO.slashdot.sectionTags);
-	update_style_map(well_known_tags, 't2', YAHOO.slashdot.topicTags);
-	update_style_map(well_known_tags, 'f', YAHOO.slashdot.feedbackTags);
-	update_style_map(well_known_tags, 'e', YAHOO.slashdot.actionTags);
-	update_style_map(well_known_tags, 'e', YAHOO.slashdot.fhitemOpts);
-	update_style_map(well_known_tags, 'e', YAHOO.slashdot.storyOpts);
-	update_style_map(well_known_tags, 'y p', ['nod']);
-	update_style_map(well_known_tags, 'x p', ['nix']);
+	update_class_map(well_known_tags, 's1', YAHOO.slashdot.sectionTags);
+	update_class_map(well_known_tags, 't2', YAHOO.slashdot.topicTags);
+	update_class_map(well_known_tags, 'f', YAHOO.slashdot.feedbackTags);
+	update_class_map(well_known_tags, 'e', YAHOO.slashdot.actionTags);
+	update_class_map(well_known_tags, 'e', YAHOO.slashdot.fhitemOpts);
+	update_class_map(well_known_tags, 'e', YAHOO.slashdot.storyOpts);
+	update_class_map(well_known_tags, 'y p', ['nod']);
+	update_class_map(well_known_tags, 'x p', ['nix']);
 })
 
-function update_style_map( style_map, style, tags ){
-	var sp_style = ' ' + style;
+function update_class_map( css_class_map, css_class, tags ){
+	var sp_css_class = ' ' + css_class;
 
 	function update( tag ){
-		if ( tag in style_map )
-			style_map[tag] += sp_style
+		if ( tag in css_class_map )
+			css_class_map[tag] += sp_css_class
 		else
-			style_map[tag] = style
+			css_class_map[tag] = css_class
 	}
 
 	function update_from_set( key, value ){ update(key) }
@@ -657,97 +652,111 @@ function update_style_map( style_map, style, tags ){
 	$.each(tags, (tags.length === undefined) ? update_from_set : update_from_list);
 }
 
-var tag_prefix_styles = {
+var css_classes_for_prefix = {
 	'!': 'bang',
 	'#': 'pound',
 	')': 'descriptive',
 	'_': 'ignore'
 };
 
-function local_style_for( tag ){
+function static_css_classes_for( tag ){
 
-	var style = '';
+	var css_class = '';
 	var sep = '';
 
 	function include( expr ){
 		if ( expr ){
-			style += sep + expr;
+			css_class += sep + expr;
 			sep = ' ';
 		}
 	}
 
 	include(well_known_tags[bare_tag(tag)]);
-	include(tag_prefix_styles[ tag[0] ]);
+	include(css_classes_for_prefix[ tag[0] ]);
 
-	return style;
+	return css_class
 }
 
-var style_for_bar = { user: 'u', top: 't', system: 's' };
+var css_class_for_listen = { user: 'u', top: 't', system: 's' };
 
-function style_tags_globally( widget ){
-	var done = {};
-	var style_map = {};
+function recompute_css_classes( root ){
+	var already = {};
+	var computed_css_classes_for = {};
 
-	// Step 1: build one big dictionary mapping tag names to 'global' styles
-	// that is, styles we deduce from where a tag appears.  If a tag appears
+	var $displays = $('.tag-display', root);
+
+	// Step 1: build one big dictionary mapping tag names to 'computed' css classes
+	// that is, classes we deduce from where a tag appears.  If a tag appears
 	// in the user tag-display, then every occurance of that tag will be styled
 	// to indicate that.
 
 	// So, for each of the big three (user, top, system) tag-displays; extract
-	// their tags, and update our style map with a class for that display
-	$('.tag-display.ready[listen]', widget).each(function(){
-		var display = $(this).attr('listen');
-		var style = style_for_bar[display];
+	// their tags, and update our css class map for that display
+	$displays
+		.filter('.ready[listen]:not(.no-tags)')
+			.each(function(){
+				var display = $(this).attr('listen');
+				var css_class = css_class_for_listen[display];
 
-		// style true for a display that exclusively gets one of the big three
-		// so: if it's one of the big three that we haven't yet seen...
-		if ( style && !done[display] ){
-			update_style_map(
-				style_map,
-				style,
+				// css_class true for a display that exclusively gets one of the big three
+				// so: if it's one of the big three that we haven't yet seen...
+				if ( css_class && !already[display] ){
+					update_class_map(
+						computed_css_classes_for,
+						css_class,
 
-				// build an array of all the tag names in this display
-				$('span.tag', this).map(function(){
-					return $(this).text()
-				})
-			);
-			done[display] = true;
-		}
-	});
+						// build an array of all the tag names in this display
+						$('span.tag', this).map(function(){
+							return $(this).text()
+						})
+					);
+					already[display] = true;
+				}
+			});
 
-	// style_map now contains every tag in the user, top, and system displays
+	// computed_css_classes_for now contains every tag in the user, top, and system displays
 	// (i.e., all tags that globally influence each other) and maps those
-	// tag names to strings containing a style class for each display in which
+	// tag names to strings containing a css class for each display in which
 	// the tag appeared, e.g., if 'hello' is in both the user and top tag
-	// displays, then style_map['hello'] == 'u t' (mod order)
+	// displays, then computed_css_classes_for['hello'] == 'u t' (mod order)
 
-	// Step 2: for tags that are sections, topics, etc., add corresponding styles
-	$.each(style_map, function(k, v){
-		var local_styles = local_style_for(k);
-		if ( local_styles )
-			style_map[k] += ' ' + local_styles;
+	// Step 2: for tags that are sections, topics, etc., add corresponding classes
+	$.each(computed_css_classes_for, function(k, v){
+		var static_css_classes = static_css_classes_for(k);
+		if ( static_css_classes )
+			computed_css_classes_for[k] += ' ' + static_css_classes;
 	});
 
-	// Step 3: find every tag span and apply the styles we've calculated
-        $('.tag-display span.tag', widget).each(function(){
-		var $this = $(this);
-		var tag = $this.text();
+	// Step 3: find every tag span and apply the css classes we've calculated
+	$displays
+		.find('span.tag')
+			.each(function(){ // for each tag
+				var $tag = $(this);
+				var tag = $tag.text();
 
-		var class_list = '';
-		if ( tag in style_map )
-			// we saw this tag, and know all the styles
-			class_list = style_map[tag];
-		else {
-			// didn't see this tag on the global phase, so it has
-			// no global styles, but it _might_ still have local
-			// which we'll cache in case we see this tag again
-			var local_styles = style_map[tag] = local_style_for(tag);
-			if ( local_styles ) {
-				class_list = local_styles;
-			}
-		}
+				var class_list = '';
+				if ( tag in computed_css_classes_for )
+					// we saw this tag, and know all the classes
+					class_list = computed_css_classes_for[tag];
+				else {
+					// didn't see this tag on the global phase, so it has
+					// no 'computed' classes, but it _might_ still have static classes
+					// which we'll cache in case we see this tag again
+					var static_css_classes = computed_css_classes_for[tag] = static_css_classes_for(tag);
+					if ( static_css_classes ) {
+						class_list = static_css_classes;
+					}
+				}
 
-		$this.parent().setClass(class_list);
-		this.className = class_list ? 'tag '+class_list : 'tag';
-        })
+				$tag.parent().setClass(class_list);
+			})
+		.end()
+		.filter('[listen=user]')
+			.each(function(){ // for each display of user tags
+				var $this = $(this);
+				$this.toggleClassTo(
+					'no-visible-tags',
+					! $this.is(':has(li.u:not(.t,.s,.p))')
+				)
+			})
 }
