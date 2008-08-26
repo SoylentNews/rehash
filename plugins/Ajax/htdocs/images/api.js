988fa176140e9ce276b20f7f4d7f316d619c4664
Scott Collins <scc@ScottCollins.net>
2008-08-26 13:33:13 -0400

    Document API's API in a big comment; update unit-tests for 'this' handling
    tracks internal commit 7d39ebf4809a648f75ffa5952a15ae5eaf6c200b

--- plugins/Ajax/htdocs/images/api.js
+++ plugins/Ajax/htdocs/images/api.js
@@ -1,7 +1,75 @@
+/* API
+
+Usage:
+	var api = new API(api_definition);
+
+api_definition is an object with the following properties, of which 'name' is
+required and all others are optional (of course the whole thing is pointless if
+you don't supply at least one of api, or element_api).
+
+	name:			non-empty string
+	api:			object with function and data members
+	element_api:		object with function members whose first argument is an element
+	element_constructor:	boolean or function
+	extend_jquery:		boolean or string
+	extend_jquery_wrapper:	boolean or function
+
+Example:
+
+	var greeter = new API({
+		name: 'greeter',
+		element_api: {
+			say_hello: function( element, person ){
+				$element.text("Hello, " + (person || "world") + "!");
+			}
+		},
+		extend_jquery_wrapper: true
+	});
+
+	var elem = $('#world-greeting')[0];
+
+	// you can now use the functions from your 'namespace'
+	greeter.say_hello(elem);
+
+	// or you can attach your api to an element...
+	greeter(elem);
+
+	// ...and then call your functions directly on the element
+	elem.greeter.say_hello('Bob');
+
+	// Because we asked to extend the jQuery wrapper, we can use our
+	// functions from a selection even without attaching the api to the
+	// selected elements
+	$('[id*=greeting]').greeter__say_hello();
+
+	// or we can use jQuery to attach the api to the selected elements
+	$('[id*=greeting]').greeter();
+
+When your element functions are called _from_ elements, 'this' will always be
+set to the element.  If your function is called in the form where the elem is
+supplied as a parameter at the call-site, 'this' is set to your global api
+object.
+
+	elem.greeter.say_hello('Bob')	// inside say_hello, 'this' is elem
+	greeter.say_hello(elem, 'Bob')	// inside say_hello, 'this' is greeter
+
+If you also supply an 'api' entry in the definition, those functions (and data
+members) will be available on greeter, above, but _not_ on individual elements
+no from a jQuery selection.
+
+If you also elect to extend_jquery itself, then jQuery will have an member named
+as your api name, or else as the string you supplied:
+
+	$.greeter
+
+...which has all the same powers as our greeter object, above.
+ */
+
 (function($){
 
 /*global window jQuery */
 
+// XXX move to Algorithm
 var clone = ($.clone = function( o ){
 	if ( !o || typeof o !== 'object' ) {
 		return o;
