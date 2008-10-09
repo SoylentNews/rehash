78d3e68ce89478b5d8d74bd0efcd51f7c9caa7d8
pudge <projects@pudge.net>
2008-10-09 12:10:51 -0700

    Fix linkComment stuff srsly
    tracks internal commit 650f0389c92e7fe34a4df28e0ef078d740d78c0d

--- Slash/Utility/Display/Display.pm
+++ Slash/Utility/Display/Display.pm
@@ -1323,8 +1323,8 @@ sub _hard_linkComment {
 	my $subject = $linkdata->{subject};
 
 	my $display = qq|<a |;
-	$display .= qq|id="$linkdata->{id}" |    if $linkdata->{id};
-	$display .= qq|class="$linkdata->{class}" | if $linkdata->{class};
+	$display .= qq|id="$linkdata->{a_id}" |    if $linkdata->{a_id};
+	$display .= qq|class="$linkdata->{a_class}" | if $linkdata->{a_class};
 	$display .= qq|href="$gSkin->{rootdir}/comments.pl?sid=$linkdata->{sid}|;
 	$display .= "&amp;op=$linkdata->{op}" if defined($linkdata->{op});
 	$display .= "&amp;threshold=$linkdata->{threshold}" if defined($linkdata->{threshold});
