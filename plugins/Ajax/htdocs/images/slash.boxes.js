047fd792735da945f07dbf3824ef5e0fb79d66b6
Scott Collins <scc@ScottCollins.net>
2008-10-23 12:34:40 -0400

    Slashboxes now get closeboxes in js rather than template; no more client-callable code
    tracks internal commit e0f0fd05e0c420fd4fe3d09b199273f96a9a5583

--- plugins/Ajax/htdocs/images/slash.boxes.js
+++ plugins/Ajax/htdocs/images/slash.boxes.js
@@ -1,39 +1,38 @@
 ;(function($){
+// No client-callable functions.  It's all automatic.
 
-function save_slashbox_positions(){
+function save_slashboxes(){
+	// tell the server our current list of slashboxes
 	ajax_update({
 		op:	'page_save_user_boxes',
 		reskey:	reskey_static,
-		bids:	$('#slashboxes div.title').map(function(){
-				return this.id.slice(0,-6);
-			}).get().join(',')
+		bids:	$('#slashboxes div.title').
+				map(function(){
+					return this.id.slice(0,-6);
+				}).
+				get().
+				join(',')
 	});
 }
 
-function remove_slashbox_by_id( id ){
-	if ( $('#slashboxes > #'+id).remove().size() ) {
-		save_slashbox_positions();
-	}
-}
+$(function(){ // on document ready:
 
-function make_slashboxes_sortable(){
-	$('#slashboxes').sortable({
+$('#slashboxes').
+	sortable({			// make slashboxes sortable...
 		axis: 'y',
 		containment: 'parent',
 		handle: '.title',
 		opacity: 0.8,
-		update: save_slashbox_positions
-	});
-}
+		update: save_slashboxes	// ...and save their new order
+	}).
+	find('> div.block > div.title > h4').	// add closeboxes...
+		append('<span class="closebox">x</span>').
+		find('span.closebox').
+			click(function(){	// ...that close; save new state
+				$(this).nearest_parent('div.block').remove();
+				save_slashboxes();
+			});
 
-Slash.Util.Package({ named: 'Slash.Boxes',
-	api: {
-		remove: remove_slashbox_by_id
-	}
-});
-
-$(function(){
-	make_slashboxes_sortable();
 });
 
 })(Slash.jQuery);
