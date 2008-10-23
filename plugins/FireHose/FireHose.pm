d7dda2719b4c166e7551d4b98046126f902246b4
Weston Moran <wmoran@corp.sourceforge.com>
2008-10-23 10:54:33 -0400

    simplfying note
    tracks internal commit 471c53a4c8b8bc3b908c97d7ac1dfce248766efe

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1313,7 +1313,7 @@ sub ajaxSaveNoteFirehose {
 		my $firehose = getObject("Slash::FireHose");
 		$firehose->setFireHose($id, { note => $note });
 	}
-	return $note || "Note";
+	return $note || "<img src='//images.slashdot.org/sic_notes.png' alt='Note'>";
 }
 
 sub ajaxSaveFirehoseTab {
