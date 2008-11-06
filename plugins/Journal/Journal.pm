7180c03bdbcd9b3d4394009960c5dd40531e4ad3
pudge <projects@pudge.net>
2008-11-05 16:46:13 -0800

    Add "unsbumittable" journals into the hose as "post" type Journal entries that do not pass the submit reskey check can still get into the hose but only as "post" type so they do not show up by default.
    tracks internal commit e6de7982ce7c71ec0796f5bb2dec444772033f14

--- plugins/Journal/Journal.pm
+++ plugins/Journal/Journal.pm
@@ -41,15 +41,30 @@ sub set {
 
 	$self->sqlUpdate('journals', \%j1, "id=$id") if keys %j1;
 	$self->sqlUpdate('journals_text', \%j2, "id=$id") if $j2{article};
-	if ($constants->{plugin}{FireHose}) {
-		# XXX: this will not work with HumanConf!
+
+	$self->insertFireHose($id, $j1{promotetype});
+}
+
+sub insertFireHose {
+	my($self, $id, $promotetype) = @_;
+
+	return unless getCurrentStatic()->{plugin}{FireHose};
+
+	if ($promotetype ne 'post') {
 		my $reskey = getObject('Slash::ResKey');
 		my $rkey = $reskey->key('submit', { nostate => 1 });
-		if ($rkey && $rkey->createuse) {
-			my $firehose = getObject("Slash::FireHose");
-			$firehose->createUpdateItemFromJournal($id);
+		if (!$rkey || !$rkey->createuse) {
+			# user is not able to submit, so we make it
+			# a "non-submitted" FH entry
+			$self->sqlUpdate('journals',
+				{ promotetype => 'post' },
+				"id=" . $self->sqlQuote($id)
+			);
 		}
 	}
+
+	my $firehose = getObject("Slash::FireHose");
+	$firehose->createUpdateItemFromJournal($id);
 }
 
 sub getsByUid {
@@ -196,16 +211,8 @@ sub create {
 	my($date) = $self->sqlSelect('date', 'journals', "id=$id");
 	my $slashdb = getCurrentDB();
 	$slashdb->setUser($user->{uid}, { journal_last_entry_date => $date });
-	if ($constants->{plugin}{FireHose}) {
-		# XXX: this will not work with HumanConf!
-		my $reskey = getObject('Slash::ResKey');
-		my $rkey = $reskey->key('submit', { nostate => 1 });
-		if ($rkey && $rkey->createuse) {
-			my $firehose = getObject("Slash::FireHose");
-			$firehose->createItemFromJournal($id);
-		}
-	}
 
+	$self->insertFireHose($id, $promotetype);
 
 	return $id;
 }
