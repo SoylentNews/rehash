4430874cfe683c5dbed556d815acec1ce82083c0
Jamie McCarthy <jamie@mccarthy.vg>
2009-01-16 10:28:40 -0500

    Better debug print
    tracks internal commit c7d3273a2aafb9744bebe54d2353e753e3a509ab

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -1041,7 +1041,7 @@ sub getFireHoseByTypeSrcid {
 sub getFireHose {
 	my($self, $id, $options) = @_;
 	if ($id !~ /^\d+$/) {
-		print STDERR scalar(gmtime) . " getFireHose($id) caller=" . join(':', caller(1)[1,2]) . "\n"
+		print STDERR scalar(gmtime) . " getFireHose($id) caller=" . join(':', caller(0)[1,2]) . "\n"
 		return undef;
 	}
 	my $hr = $self->getFireHoseMulti([$id], $options);
@@ -1605,9 +1605,6 @@ sub ajaxFireHoseGetUpdates {
 		}
 		my $item = {};
 		if (!$_->{day}) {
-			if ($_->{id} !~ /^\d+$/) {
-				print STDERR scalar(gmtime) . " day='$_->{day}' id='$_->{id}'\n";
-			}
 			$item = $firehose_reader->getFireHose($_->{id});
 			$last_day = timeCalc($item->{createtime}, "%Y%m%d");
 		}
