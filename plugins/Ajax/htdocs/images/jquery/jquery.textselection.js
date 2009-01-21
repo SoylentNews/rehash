e47d4fc3cfc1d2493a2fe1711a2481f8a7869f45
Wolf <wolf@zv.cx>
2009-01-21 14:26:32 -0500

    Bug: sometimes can't change Firehose section; Fix: handle exceptions in $.TextSelection
    Our theory: on changing sections in the Firehose when a protected edit field has focus, Firehose would
    	- enter its refresh loop where it would, eventually
    	- save that state in a $.TextSelection object
    	- delete the edit field referenced by the TextSelection
    	- ask the TextSelection object to restore the selection in (and focus on) the "late" DOM element
    	- which would throw an exception
    	- aborting the refresh and sometimes even sending the browser to a fixed URL
    
    Either way, TextSelection must handle orphaned fields---a case I failed to consider previously.  So:
    	- added internal exception class $.TextSelection.Error
    	- within .get/.set:
    		- rethrow DOM exceptions in terms of $.TextSelection.Error
    		- throw $.TextSelection.Error for parameter errors
    	- within spull/spush:
    		- contain all exceptions (use Firebug when you need to see them yourself)
    		- discard referenced text element if I can't get/set ranges in it
    	- within .focus: don't fail when ._el is null
    	- better comments
    
    While a TextSelection object references null or other unsatisfying DOM element, many operations will
    internally throw and then catch the exception.  It really is an exceptional state so this is not an
    efficiency consideration.  Forcing the throw allows you to find and fix; while trapping the exceptions
    prevents this state from bothering readers.
    
    tracks internal commit 4ef12da4780e0eb253ce18a7f98923ae9cd02a19

--- plugins/Ajax/htdocs/images/jquery/jquery.textselection.js
+++ plugins/Ajax/htdocs/images/jquery/jquery.textselection.js
@@ -2,13 +2,22 @@
 
 // constructor
 $.TextSelection = function( el, r ){
-	spull(this, el);
+	spull(this, el); // initializes:
+	// this._el	to: text field DOM element, iff $.TextSelection.get() understands it
+	// this._r	to: browser-specific object representing a selection range
+
 	if ( r ) { spush(this.range(r)); }
+};
+
+$.TextSelection.Error = function( description, obj ){
+	this._description = description;
+	this._obj = obj;
 };
 
 // public 'class' methods
 $.TextSelection.get = function( el ){
-	if ( el ) {
+	// expected to throw an exception if for any reason el doesn't satisfy
+	try {
 		if ( el.selectionStart !== undefined ) {
 			return {
 				selectionStart:	el.selectionStart,
@@ -28,11 +37,20 @@ $.TextSelection.get = function( el ){
 				selectionEnd:	bound(END)
 			};
 		}
+	} catch ( unused_error ) {
+		// fall through...
 	}
+
+	throw $.TextSelection.Error('$.TextSelection.get(el): no range operations available on el', el);
 };
 
 $.TextSelection.set = function( el, r ){
-	if ( el && r ) {
+	// expected to throw an exception if for any reason el doesn't satisfy
+	if ( ! r ) {
+		throw $.TextSelection.Error('$.TextSelection.set(el, r): range required', el);
+	}
+
+	try {
 		if ( el.createTextRange ) {
 			var tr = el.createTextRange();
 			tr.collapse(true);
@@ -45,18 +63,35 @@ $.TextSelection.set = function( el, r ){
 			el.selectionStart =	r.selectionStart;
 			el.selectionEnd =	r.selectionEnd;
 		}
+
+		return;
+	} catch ( unused_error ) {
+		// fall through...
 	}
+
+	throw $.TextSelection.Error('$.TextSelection.set(el, r): no range operations available on el', el);
 };
 
 
 // private
+// spull/spush trap all exceptions and try to be non-destructive; use Firebug (or equiv) if you need to see them
 function spull( ts, el ){
-	ts._r = $.TextSelection.get(ts._el=(el||ts._el));
+	// initialize or update a TextSelection, fetching the currently selected range
+	try {
+		ts._r = $.TextSelection.get(ts._el=(el||ts._el));
+	} catch ( unused_error ) {
+		ts._el = null;
+	}
 	return ts;
 }
 
 function spush( ts, el ){
-	$.TextSelection.set(el || ts._el, ts._r);
+	// apply the range described by a TextSelection to a text field
+	try {
+		$.TextSelection.set(el || ts._el, ts._r);
+	} catch ( unused_error ) {
+		// non-fatal, ignore
+	}
 	return ts;
 }
 
@@ -65,9 +100,11 @@ function spush( ts, el ){
 // public instance methods
 $.TextSelection.prototype = {
 	field: function( el ){
+		// set or get the underlying text field DOM element
 		return el ? spull(this, el) : this._el;
 	},
 	range: function( r, dont_select ){
+		// set or get the selection range as cached in the TextSelection
 		if ( r ) {
 			this._r = r;
 			return dont_select ? this : spush(this);
@@ -76,7 +113,7 @@ $.TextSelection.prototype = {
 		}
 	},
 	focus: function(){
-		$(this._el).focus();
+		$(this._el || []).focus();
 	},
 	save: function( el ){
 		return spull(this, el);
