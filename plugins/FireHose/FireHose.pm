752a53c4f6ef9a4040f231fe9b82ded1aafd7cca
Jamie McCarthy <jamie@mccarthy.vg>
2009-02-13 11:44:58 -0500

    Enable sphinx for 5% of non-admins
    tracks internal commit 5a3e84744a6ea200be49adc7c71ab9ea90a1af5e

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -830,11 +830,11 @@ sub getFireHoseEssentials {
 
 	my($sphinx, $sphinxdb, @sphinx_opts, @sphinx_terms, @sphinx_where, $sphinx_other) = (0);
 	my @sphinx_tables = ('sphinx_search');
-	$sphinxdb = getObject('Slash::Sphinx', { db_type => 'sphinx' });
+	$sphinxdb = getObject('Slash::Sphinx', { db_type => 'sphinx', timeout => 2 });
 	$sphinx = 1 if $sphinxdb;
 	$sphinx = 2 if $sphinx && $options->{firehose_sphinx} && $user->{is_admin};
 	# admins turn it on or off manually
-	#if ($sphinx == 1 && !$user->{is_admin}) { $sphinx == 2 if rand(20) <= 1 }  # 5 percent
+	if ($sphinx == 1 && !$user->{is_admin}) { $sphinx = 2 if rand(20) <= 1 }  # 5 percent
 
 	my $no_mcd = $user->{is_admin} && !$options->{usermode} ? 1 : 0;
 
