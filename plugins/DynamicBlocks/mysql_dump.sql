c41959c5ec4c908c8428a6cbbb8064734b67aaa0
Christopher Brown <cbrown@corp.sourceforge.com>
2009-03-12 12:48:22 -0400

    Added the DynamicBlocks tables and default values (to plugin).
    tracks internal commit 64f97349a02c2c90da1dfabed3eb073dda95f832

---
+++ plugins/DynamicBlocks/mysql_dump.sql
@@ -0,0 +1,11 @@
+INSERT INTO dynamic_blocks VALUES (1, 'portal', 'no');
+INSERT INTO dynamic_blocks VALUES (2, 'portal', 'yes');
+INSERT INTO dynamic_blocks VALUES (3, 'admin', 'no');
+INSERT INTO dynamic_blocks VALUES (4, 'admin', 'yes');
+INSERT INTO dynamic_blocks VALUES (5, 'user', 'no');
+INSERT INTO dynamic_blocks VALUES (6, 'user', 'yes');
+INSERT INTO dynamic_user_blocks (bid, type_id, uid, name, title, description, seclev, created, last_update) VALUES (300, 3, 0, 'performancebox', 'Performance', 'Performance Stats', 10000, NOW(), NOW());
+INSERT INTO dynamic_user_blocks (bid, type_id, uid, name, title, description, seclev, created, last_update) VALUES (301, 3, 0, 'authoractivity', 'Authors', 'Author Activity', 10000, NOW(), NOW());
+INSERT INTO dynamic_user_blocks (bid, type_id, uid, name, title, description, seclev, created, last_update) VALUES (302, 3, 0, 'admintodo', 'Admin Todo', 'Admin Todo Items', 10000, NOW(), NOW());
+INSERT INTO dynamic_user_blocks (bid, type_id, uid, name, title, description, seclev, created, last_update) VALUES (303, 3, 0, 'recenttagnames', 'Recent Tagnames', 'Recent Tagnames', 10000, NOW(), NOW());
+INSERT INTO dynamic_user_blocks (bid, type_id, uid, name, title, description, seclev, created, last_update) VALUES (304, 3, 0, 'firehoseusage', 'Firehose Usage', 'Firehose Usage', 10000, NOW(), NOW());
---
+++ plugins/DynamicBlocks/mysql_schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS dynamic_blocks;
+CREATE TABLE dynamic_blocks (
+  type_id tinyint(1) unsigned NOT NULL default '0',
+  type enum('portal','admin','user') NOT NULL default 'user',
+  private enum('yes','no') NOT NULL default 'no',
+  PRIMARY KEY  (type_id)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS dynamic_user_blocks;
+CREATE TABLE dynamic_user_blocks (
+  id mediumint(8) unsigned NOT NULL auto_increment,
+  bid mediumint(8) unsigned NOT NULL default '0',
+  type_id tinyint(1) unsigned NOT NULL default '0',
+  uid mediumint(8) unsigned NOT NULL default '0',
+  title varchar(30) NOT NULL default '',
+  url varchar(128) NOT NULL default '',
+  name varchar(30) NOT NULL default '',
+  description varchar(64) NOT NULL default '',
+  block text,
+  seclev mediumint(8) unsigned NOT NULL default '0',
+  created datetime NOT NULL default '0000-00-00 00:00:00',
+  last_update datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (id),
+  UNIQUE KEY block (bid,uid)
+) ENGINE=InnoDB;
