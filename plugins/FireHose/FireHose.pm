71d9a23d571550fc837b86ab652ba4c5e562bb70
Tim Vroom <vroom@blockstackers.com>
2009-03-20 16:12:03 -0400

    Fix syntax error
    tracks internal commit 378b84d6f5e2889bafce0d4d23948296a38f16ab

--- plugins/FireHose/FireHose.pm
+++ plugins/FireHose/FireHose.pm
@@ -2542,7 +2542,7 @@ sub ajaxFireHoseGetUpdates {
                 $dynamic_blocks = $dynamic_blocks_reader->getBlocksEligibleForUpdate($form->{dynamic_blocks}, { min_time => $update_time, is_admin => $user->{is_admin} });
         }
 
-	my $color_js = "\$('.currcolor').removeClass('red orange yellow green blue violet indigo black').addClass('$opts->{color}');"
+	my $color_js = "\$('.currcolor').removeClass('red orange yellow green blue violet indigo black').addClass('$opts->{color}');";
 	my $eval_last = "$color_js $title_js";
 
 	my $data_dump =  Data::JavaScript::Anon->anon_dump({
