eac81fd936a6b5723c37a874b0f46a894f7aa3bb
Wolf <wolf@zv.cx>
2009-05-08 23:16:53 -0400

    Teach TypeOf to identify 'NaN' and 'event'; simplify overall
    Type identification is no longer a "two-stage" process.  The special cases are
    all handled up-front.  Map only from the '[...]' forms; no longer _also_ from the
    (builtin) typeof results.
    
    tracks internal commit 8beda3985f15821a62b59250979b2447e17e4ed2

--- plugins/Ajax/htdocs/images/typeof.js
+++ plugins/Ajax/htdocs/images/typeof.js
@@ -1,137 +1,203 @@
-var  TypeOf;
+/* TypeOf(o), ...
+
+Stand-alone.  Does not need jQuery (or anything else, for that matter).
+
+Fixes some failures in the builtin typeof operator:
+
+	TypeOf(null) => 'null'		typeof(null) => 'object'	Object.prototype.toString.call(null) => '[object Window]'
+	TypeOf([]) => 'array'		typeof([]) => 'object'
+	TypeOf(0/0) => 'NaN'		typeof(0/0) => 'number'
+
+	Never uses instanceof, which fails across contexts.
+
+
+Recognizes lists:
+
+	TypeOf(arguments) => 'list'
+	TypeOf(document.childNodes) => 'list'
+	TypeOf(jQuery('span')) => 'list'
+	TypeOf([]) => 'array'
+
+
+Qualified calls, e.g., TypeOf.list(o), return a typename just as TypeOf(o) would, if o qualifies
+(in this case, if o is a list) or else a result!=true.
+
+	TypeOf.list("Hello, World") => 'string'
+	TypeOf.list(7) => false
+	TypeOf.list([]) => 'array'
+	TypeOf.list(document.childNodes) => 'list'
+
+	TypeOf.scalar("Hello, World!") => 'string'
+	TypeOf.scalar([]) => false
+	TypeOf.scalar(false) => 'boolean'
+	TypeOf.scalar(0/0) => 'NaN'
+
+	TypeOf.number("Hello, World!") => false
+	TypeOf.number(Infinity) => 'Infinity'
+	TypeOf.number(0/0) => 'NaN'
+	TypeOf.number(1729) => 'number'
+
+		TypeOf(o)==='number'		=> exclude NaN (i.e., typeof(o)==='number' && !isNaN(o))
+		TypeOf.number(o)==='number'	=> exclude NaN and +/- Infinity (i.e., typeof(o)==='number' && isFinite(o))
+		TypeOf.number(o)			=> include NaN, +/- Infinity (i.e., typeof(o)==='number')
+
+
+Recognizes nodes:
+
+	TypeOf(o) can return 'element', 'document', or 'node'
+	TypeOf.element(o) will return the actual element kind, e.g., 'h1'
+	TypeOf.node(o), similarly
+
+
+Recognizes events:
+
+	TypeOf(o) returns 'event'
+	TypeOf.event(o) returns the actual event kind, e.g., 'click'
+
+		You can write a function that can be used directly or as a click-handler:
+
+		function close_widget( what ){
+			switch ( TypeOf(what) ) {
+				case 'event':	what=event.target;
+				case 'element':	what=$(what).closest('.widget');
+								what.hide();
+			}
+		}
+
+Recognizes some "almost builtin" types:
+
+	'date', 'regexp', 'error', 'window'
+
+
+Does not, itself, modify any system objects... but if you want to, you can hang a __typeOf function
+(returning a string) almost anywhere you like.  TypeOf considers this an "intrusive" type.
+
+*/
+
+var TypeOf;
 (function(){
 var U=void(0), N=null, W=window,
-	OBJECT, FUNCTION, NUMBER,
-	TYPEOF={}, TYPEOF_NODE=[], TYPEOF_SCALAR={}, TYPEOF_LIST={};
+	NUM_T='number', LIST_T='list', EL_T='element', DOC_T='document', EVENT_T='event',
+	KNOWN_TYPE={}, SCALAR_TYPE={}, LIST_TYPE={}, NODE_TYPE=[];
 
-function otype( o, compare ){
-	var ot;
-	return o!==U
-		&& o!==N
-		&& (ot=Object.prototype.toString.call(o))
-		&& (!compare || compare===ot)
-		&& ot
 
-		|| U;
+function TK( o ){
+	var tk;
+	return o===U && '[type undefined]'
+		|| o===N && '[type null]'
+		|| (tk=Object.prototype.toString.call(o))==='[object Number]' && isNaN(o) && '[type NaN]'
+		|| tk;
 }
 
-function typeof_fn( o ){
-	return otype(o, FUNCTION);
+function is_fn( o ){
+	return TK(o)==='[object Function]';
 }
 
-function typeof_node( o ){
-	try {
-		if ( o && o.nodeName ) {
-			return TYPEOF_NODE[o.nodeType];
-		}
-	} catch ( e ) {
-	}
+
+function scalar_type( o ){
+	return SCALAR_TYPE[ TK(o) ];
+}
+function qualify_number( o ){
+	var t=typeof(o);
+	return t===NUM_T && (isFinite(o) ? t : o.toString());
 }
 
-function qualify_element( o ){
-	var t = typeof_node(o);
-	if ( t !== 'node' ) {
-		return t==='element' && o.nodeName.toLowerCase()
-			|| t;
-	}
+
+function maybe_list( o ){
+	try { return !!o && qualify_number(n=o.length)===NUM_T && (!n || n-1 in o) && LIST_T; } catch ( e ) {}
+}
+function list_type( o ){
+	var tk=TK(o);
+	return tk in LIST_TYPE ? LIST_TYPE[ tk ] : maybe_list(o);
 }
 
+
+function maybe_node( o ){
+	try { return !!o && o.nodeName && NODE_TYPE[ o.nodeType ]; } catch ( e ) {}
+}
 function qualify_node( o ){
-	return qualify_element(o)
-		|| typeof_node(o);
+	var t;
+	return (t=maybe_node(o))===DOC_T && t
+		|| t===EL_T && o.nodeName.toLowerCase()
+		|| t && o.nodeName
+		|| t;
+}
+function qualify_element( o ){
+	return maybe_node(o)===EL_T && qualify_node(o);
 }
 
-function intrusive_typeof( o ){
-	// UNSAFE!! |o| must be defined and non-null
-	return typeof_fn(o.__typeOf) && o.__typeOf();
+
+function maybe_event( o ){
+	return !!o && is_fn(o.preventDefault) && is_fn(o.stopPropagation) && EVENT_T;
+}
+function qualify_event( o ){
+	return maybe_event(o) && o.type;
 }
 
-function typeof_list( o ){
-	var len;
-	try {
-		if ( o && !isNaN(len=o.length) && (!len || len-1 in o) ) {
-			return 'list';
-		}
-	} catch ( e ) {
-	}
-}
 
 TypeOf = function( o ){
-	return TYPEOF[ typeof(o) ]
-		|| o===N && 'null'
-		|| TYPEOF[ otype(o) ]
-		|| intrusive_typeof(o)
-		|| typeof_node(o)
+	return KNOWN_TYPE[ TK(o) ]
+		|| is_fn(o.__typeOf) && o.__typeOf()
 		|| o===W && 'window'
-		|| typeof_list(o)
+		|| maybe_node(o)
+		|| maybe_event(o)
+		|| maybe_list(o)
 		|| 'object';
-};
-TypeOf.fn		= typeof_fn;
-TypeOf.element	= qualify_element;
-TypeOf.node		= qualify_node;
-TypeOf.list		= function( o ){ return TYPEOF_LIST[ TypeOf(o) ]; };
-
-TypeOf.number = function( o, all ){
-	if ( otype(o, NUMBER) ) {
-		return isFinite(o) && 'number'
-			|| all && o.toString()
-			|| U;
-	}
-};
-
-TypeOf.scalar = function( o ){
-	return TYPEOF_SCALAR[ typeof(o) ]
-		|| o===N && 'null'
-		|| U;
 }
-
-TypeOf.debug	= function(){ return TYPEOF; };
-
+TypeOf.scalar	= scalar_type;
+TypeOf.number	= qualify_number;
+TypeOf.fn		= function( o ){ return is_fn(o) && 'function'; };
+TypeOf.list		= list_type;
+TypeOf.node		= qualify_node;
+TypeOf.element	= qualify_element;
+TypeOf.event	= qualify_event;
 
 (function(){
-	OBJECT		= otype({});
-	FUNCTION	= otype(function(){});
-	NUMBER		= otype(1);
+	var i;
 
-	var ELEMENT_NODE=1, DOCUMENT_NODE=9, BROKEN=[ 'object', OBJECT ];
-	for ( var i=0; i<=12; ++i ){
-		TYPEOF_NODE[i] = 'node';
+	for ( i=document.ELEMENT_NODE; i<=document.NOTATION_NODE; ++i ) {
+		NODE_TYPE[ i ] = 'node';
 	}
-	TYPEOF_NODE[ELEMENT_NODE] = 'element';
-	TYPEOF_NODE[DOCUMENT_NODE] = 'document';
+	NODE_TYPE[ document.ELEMENT_NODE ] = EL_T;
+	NODE_TYPE[ document.DOCUMENT_NODE ] = DOC_T;
 
-	var INIT = [
-		{ o:undefined,												scalar:true,	list:false },
-		{ o:false,													scalar:true,	list:false },
-		{ o:0,														scalar:true,	list:false },
-		{ o:'',														scalar:true,	list:true },
-		{ o:function(){},															list:false },
-		{ o:{} },
-		{ o:[],									want:'array',						list:true },
-		{ o:/./,			expect:'object',	want:'regexp',						list:false },
-		{ o:new Date(),		expect:'object',	want:'date',						list:false },
-		{ o:new Error(),	expect:'object',	want:'error',						list:false },
-		{ o:document,							want:'document',					list:false },
-		{ o:W,									want:'window',						list:false }
-	];
 
-	while ( INIT.length ){
-		var	entry			= INIT.pop(),
-			actual			= typeof(entry.o),
-			actual_otype	= otype(entry.o),
-			want			= entry.want||entry.expect||actual;
+	function define( o, scalar, list, tn ){
+		var tk=TK(o), unique=(tk!==TK({}));
+		if ( unique ) {
+			tn || (tn = tk.replace(/^\[.+ |\]$/g, '').toLowerCase());
 
-		entry.expect && actual!==entry.expect && BROKEN.push(actual);
-		actual!=='object'		&& (TYPEOF[ actual ] = want);
-		actual_otype!==OBJECT	&& (TYPEOF[ actual_otype ] = want);
-		entry.scalar!==U		&& (TYPEOF_SCALAR[want] = want);
-		entry.list!==U			&& (TYPEOF_LIST[want] = entry.list && want || U);
+			KNOWN_TYPE[ tk ] = tn;
+			scalar!==U	&& (SCALAR_TYPE[ tk ] = tn);
+			list!==U	&& (LIST_TYPE[ tk ] = list && tn);
+		}
+		return unique;
 	}
-	TYPEOF_LIST.list = 'list';
 
-	while ( BROKEN.length ){
-		delete TYPEOF[ BROKEN.pop() ];
-	}
+	define(void(0),			true, false);
+	define(null,			true, false);
+	define(false,			true, false);
+	define(0,				true, false);
+	define(0/0,				true, false, 'NaN');
+	define('',				true, true);
+
+	define(function(){},	false, false);
+	define([],				false, true);
+	define(/./,				false, false);
+	define(new Date(),		false, false);
+	define(new Error(),		false, false);
+
+	define(document,		false, false, DOC_T);
+	define(window,			false, false, 'window');
+
+	define(document.childNodes, false, true, LIST_T);
+	define(arguments,		false, true, LIST_T);
+
+	define(document.createEvent('UIEvents'),		false, false, EVENT_T);
+	define(document.createEvent('MouseEvents'),		false, false, EVENT_T);
+	define(document.createEvent('MutationEvents'),	false, false, EVENT_T);
+	define(document.createEvent('HTMLEvents'),		false, false, EVENT_T);
 })();
 
 })();
+
