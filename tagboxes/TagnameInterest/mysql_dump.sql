cf329b1838df5ae1e720197892e19510780f80fe
Jamie McCarthy <jamie@mccarthy.vg>
2009-07-17 14:46:40 -0400

    FHEditorPop: handle scoring tagname items
    tracks internal commit 2c5bc365103ace0c52c3e53c91c5a9899d14a1fe

--- tagboxes/TagnameInterest/mysql_dump.sql
+++ tagboxes/TagnameInterest/mysql_dump.sql
@@ -1,5 +1,6 @@
 INSERT INTO tagboxes (tbid, name, weight, last_run_completed, last_tagid_logged, last_tdid_logged, last_tuid_logged) VALUES (NULL, 'TagnameInterest', 1, '2000-01-01 00:00:00', 0, 0, 0);
 
+INSERT INTO vars (name, value, description) VALUES ('tagbox_tni_initial_color', '5', 'Initial editor firehose color level for tagnames');
 INSERT INTO vars (name, value, description) VALUES ('tagbox_tni_min_globjid', '2', 'Min num globjs necessary to put a tagname into the hose');
 INSERT INTO vars (name, value, description) VALUES ('tagbox_tni_min_pop', '', 'Min firehose popularity to count about tagnames on (blank means use tags_active_mincare, default minpop for 5 aka blue)');
 INSERT INTO vars (name, value, description) VALUES ('tagbox_tni_min_time', '14400', 'Min num seconds of timespan necessary to put a tagname into the hose');
