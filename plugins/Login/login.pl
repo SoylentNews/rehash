e4e9ed81b5c99159b8100083f990eca652279c45
Christopher Brown <cbrown@corp.sourceforge.com>
2009-07-18 15:06:17 -0400

    Remove Daily Headlines as new user default.
    tracks internal commit 46802829d79c3639f950813d60c9cd6a32913fea

--- plugins/Login/login.pl
+++ plugins/Login/login.pl
@@ -171,7 +171,6 @@ sub newUser {
                         }
 
 			$params{MSG_CODE_NEWSLETTER()} = MSG_MODE_EMAIL();
-			$params{MSG_CODE_HEADLINES()} = MSG_MODE_EMAIL();
 
 			$messages->setPrefs($uid, \%params);
 
