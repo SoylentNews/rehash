d4ac69f4faa3ea1855b41b2cec4b9310dd5dd416
Tim Vroom <vroom@blockstackers.com>
2009-08-03 14:42:27 -0400

    Add no wrap in appropriate places
    tracks internal commit da035a68bac7bc2a21215caa67a3f5eb3cabc705

--- plugins/Edit/Edit.pm
+++ plugins/Edit/Edit.pm
@@ -824,7 +824,7 @@ sub ajaxEditorAfter {
 	my $edit = getObject("Slash::Edit");
 	$edit->initEditor();
 	my $html_add_after = {};
-	$html_add_after->{$form->{after_id}} = $edit->showEditor();
+	$html_add_after->{$form->{after_id}} = $edit->showEditor({ nowrap => 1});
 
 	return Data::JavaScript::Anon->anon_dump({ html_add_after => $html_add_after });
 }
