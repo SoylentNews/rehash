cd9be1a21ae82af177bed13fe5da58243c8ec686
Christopher Brown <cbrown@corp.sourceforge.com>
2009-08-13 16:53:20 -0400

    delete_stale_previews task. Deletes previews greater than 24 hours old, the associated firehose entry, and inactivates all associated tags.
    tracks internal commit f9245192a1465a9bb897623da1cb829269ab0ce2

---
+++ plugins/Edit/delete_stale_previews.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2009 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+
+use strict;
+
+use Slash;
+use Slash::Constants ':slashd';
+use Slash::Utility;
+
+use vars qw(%task $me $task_exit_flag);
+
+$task{$me}{timespec} = '0 3 * * *';
+$task{$me}{fork} = SLASHD_NOWAIT;
+$task{$me}{code} = sub {
+        my($virtual_user, $constants, $slashdb, $user) = @_;
+
+        my $tagsdb = getObject("Slash::Tags");
+
+        my $previews = $slashdb->sqlSelectAllHashrefArray(
+                'preview_id, preview_fhid',
+                'preview',
+                'createtime < DATE_SUB(NOW(), INTERVAL 24 HOUR)'
+        );
+
+        foreach my $preview (@$previews) {
+                my ($fh_id, $globjid) = $slashdb->sqlSelect(
+                        'id, globjid',
+                        'firehose',
+                        'id = ' . $preview->{preview_fhid} . " AND preview = 'yes'"
+                );
+
+                if ($fh_id && $globjid) {
+                        $slashdb->sqlDo('START TRANSACTION');
+
+			# preview
+			$slashdb->sqlDelete('preview', "preview_id = " . $preview->{preview_id});
+
+			# preview_param
+			$slashdb->sqlDelete('preview_param', "preview_id = " . $preview->{preview_id});
+
+			# firehose
+			$slashdb->sqlDelete('firehose', "id = " . $fh_id . " and preview = 'yes'");
+
+			# firehose_text
+			$slashdb->sqlDelete('firehose_text', "id = " . $fh_id);
+
+			$slashdb->sqlDo('COMMIT');
+
+			# tags
+			$tagsdb->deactivateAllTagsByGlobjid($globjid);
+		}
+	}
+};
+
+1;
