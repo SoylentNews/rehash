ca179878990c707c63d64abe682e7aa1f6886b48
Wolf <wolf@zv.cx>
2009-08-26 16:07:18 -0400

    Trigger events when the server returns new vote, datatype, or topics
    The event is triggered directly on the fhitem to which the returned tags apply.
    Because the event "bubbles up" you need only bind once, as I've done here
    to fix the nod/nix capsule when the user's vote changes.  event.target will be
    the fhitem in question.  The data parameter will contain ---
    
      binding to:             gives you (in data):
      ---------------------------------------------------------------------------
      'vote-assigned'         'nod', 'metanod', 'nix', 'metanix'
      'datatype-assigned'     'story', 'submission', etc.
      'skin-assigned'         'apple', etc.
      'topics-assigned'       space-separated list of topic names in server-order
    
    tracks internal commit c9ef68a0bfddf2cf20ee5f7f23c0f95b9d6c1b3f

--- plugins/Ajax/htdocs/images/tag-ui.js
+++ plugins/Ajax/htdocs/images/tag-ui.js
@@ -1,7 +1,70 @@
 ; // tag-ui.js
 
-var T2={}, context_triggers, well_known_tags, tag_admin=false;
+var Tags={}, tag_admin=false, gFocusedText, $previous_context_trigger = $([]);
+(function(){
+var WS=/\s+/, NODNIX=/\b(?:meta)?(?:nod|nix)\b/i;
 
+function topics( $tags, sort ){
+	var tags = $tags.filter('a.topic').map(function(){ return $(this).text(); }).get();
+	sort && tags.sort();
+	return tags.join(' ');
+}
+
+function inspect( $tagbar ){
+	var $tags = $tagbar.children();
+	return {
+		skin:		$tags.filter('a.main:first').text(),
+		vote:		$tags.filter('a.my[href$=/nod],a.my[href$=/nix],a.my[href$=/metanod],a.my[href$=/metanix]').text(),
+		topics:		topics($tags, 'sorted'),
+		datatype:	$tags.filter('a.datatype')
+	};
+}
+
+function preprocess( fhitem, tags ){ // catch and handle tag-like commands to be processed locally
+	var nodnix = NODNIX.test(tags) && ($(fhitem).is('.fhitem-comment') ? firehose_handle_comment_nodnix : firehose_handle_nodnix);
+	tags = tags.split(WS);
+	firehose_handle_admin_commands && (tags=firehose_handle_admin_commands.call(fhitem, tags));
+	nodnix && nodnix.call(fhitem, tags);
+	return tags.join(' ');
+}
+
+Tags.submit = function( fhitem, tags ){
+	var $fhitem=$(fhitem), key=fhitem_key(fhitem), $spinner=$('span.tag-server-busy', fhitem).show();
+	tags && (tags=preprocess(fhitem, tags));
+
+	$.ajax({
+		type:'POST',
+		dataType:'text',
+		data:{
+			op:'tags_setget_display',
+			key:key.key,
+			key_type:key.key_type,
+			reskey:reskey_static,
+			tags:tags||'',
+			limit_fetch:'',
+			include_topic_images:sign($fhitem.is('.fhitem-editor'))
+		},
+		success: function( next_markup ){
+			var $tagbar=$fhitem.find('span.tag-bar'), prev=inspect($tagbar), next=inspect($tagbar.html(next_markup));
+
+			// Trigger events directly on $fhitem, but bubble up to document (so you only need to .bind() once).
+			// In your handler: event.target is the fhitem.
+			function notice_changes( k, data ){ next[k]!==prev[k] && $fhitem.trigger(k+'-assigned', data||next[k]); }
+
+			// postprocess
+			notice_changes('datatype');
+			notice_changes('topics', topics($tagbar.children())); // compare sorted, but notify in server-supplied order
+			notice_changes('skin');
+			notice_changes('vote');
+		},
+		complete: function(){
+			$spinner.hide();
+		}
+	});
+};
+
+Tags.fetch = function( fhitem ){ Tags.submit(fhitem); };
+})();
 
 (function(){
 var	CLASS={ 'true':'expand', 'false':'collapse' },
