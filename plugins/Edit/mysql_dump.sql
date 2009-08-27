d3e54c1991f4c5206e8e097309b82ce5b87a7a00
Christopher Brown <cbrown@corp.sourceforge.com>
2009-08-27 13:03:57 -0400

    Added imgupload_preview template. Added ajax ops for preview and thumbnail selection.
    tracks internal commit daab761e986429df026eef74f21e22ff3676c141

--- plugins/Edit/mysql_dump.sql
+++ plugins/Edit/mysql_dump.sql
@@ -1,1 +1,3 @@
 INSERT INTO ajax_ops VALUES (NULL, 'edit_submit_box_after', 'Slash::Edit', 'ajaxEditorAfter', 'ajax_user_static', 'createuse');
+INSERT INTO ajax_ops VALUES (NULL, 'imgupload_show_preview', 'Slash::Edit', 'ajaxUploadShowPreview', 'ajax_user_static', 'createuse');
+INSERT INTO ajax_ops VALUES (NULL, 'imgupload_set_thumbnail', 'Slash::Edit', 'ajaxUploadSetThumb', 'ajax_user_static', 'createuse');
