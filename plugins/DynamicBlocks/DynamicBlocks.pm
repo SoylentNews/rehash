df3cbf1ac9c5a0c4eb4d39feae3e70fe75681bfc
Jonathan T. Beard <jbeard@corp.sourceforge.com>
2009-09-14 14:26:16 -0400

    Fixes to game dynamic slashbox
    tracks internal commit c3c02376f8621d69a1f941c8a4a4a89f47bf85bc

--- plugins/DynamicBlocks/DynamicBlocks.pm
+++ plugins/DynamicBlocks/DynamicBlocks.pm
@@ -117,63 +117,23 @@ sub setUserBlock {
 }
 
 sub setUserGameBlock {
-#        my ($self, $user) = @_;
-#
-#	return 0 if !$user->{uid};
-#
-#        my $constants = getCurrentStatic();
-#        my $slashdb = getCurrentDB();
-#
-#	my $uid = $user->{uid};
-#	my $characters = $slashdb->sqlSelectAllHashrefArray( "*", "GAME_Characters", "UID = '$uid'" );
-##	my $gamestuff = $slashdb->sqlSelectAllHashrefArray( "*", "GAME_Characters", "UID = '$user->{uid}'" );
-#
-#        my $block;
-#                my $game_block =
-#                        slashDisplay('gameblock', {
-#                                characters => $characters,
-#                        }, { Page => 'dynamicblocks', Return => 1 });
-#
-#                $block->{block} = $game_block;
-#                $block->{url} = '~' . strip_paramattr($user->{nickname}) . '/game';
-#                $block->{title} =  strip_literal($user->{nickname}) . "'s Game";
-#		$block->{description} = 'Gameblock';
-#	return (keys %$block) ? $block : 0;
-        my ($self, $user) = @_;
+	my ($self, $user) = @_;
 
 	return 0 if !$user->{uid};
 
-        my $slashdb = getCurrentDB();
+	my $slashdb = getCurrentDB();
 
-        my $achievements_reader = getObject("Slash::Achievements");
-        my $ach_obtained = $achievements_reader->getAchievement('achievement_obtained');
-        my $obtained_aid = $ach_obtained->{'achievement_obtained'}{aid};
-
-        my $achievements =
-                $slashdb->sqlSelectAllHashrefArray(
-                        'aid, createtime',
-                        'user_achievements',
-                        "uid = " . $user->{uid} . " and aid != " . $obtained_aid . " order by createtime desc limit 3"
-                );
 	my $char = $slashdb->sqlSelectHashref( "*", "GAME_Characters", "UID = '$user->{uid}'" );
 
-        foreach my $achievement (@$achievements) {
-                $achievement->{description} = $slashdb->sqlSelect('description', 'achievements', 'aid = ' . $achievement->{aid});
-        }
+	my $block;
+	my $game_block = slashDisplay('creategame', {
+			char => $char,
+		}, { Page => 'dynamicblocks', Return => 1 });
 
-        my $block;
-        if (scalar @$achievements) {
-                my $achievements_block =
-                        slashDisplay('creategame', {
-                                achievements => $achievements,
-                                char => $char,
-                        }, { Page => 'dynamicblocks', Return => 1 });
-
-                $block->{block} = $achievements_block;
-                $block->{url} = '~' . strip_paramattr($user->{nickname}) . '/hack';
-                $block->{title} = strip_literal($user->{nickname}) . "'s SlashHack Character";
-		$block->{description} = 'SlashHack';
-        }
+	$block->{block} = $game_block;
+	$block->{url} = '~' . strip_paramattr($user->{nickname}) . '/hack';
+	$block->{title} = strip_literal($user->{nickname}) . "'s SlashHack Character";
+	$block->{description} = 'SlashHack';
 
         return (keys %$block) ? $block : 0;
 }
