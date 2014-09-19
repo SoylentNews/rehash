75b0e7c32a9aad5a4486ddd9abdbe207221151e3
TheMightyBuzzard <themightybuzzard@gmail.com>
2014-09-19 09:29:13 -0500

    Show accepted but not published article titles

--- plugins/Submit/mysql_dump.sql
+++ plugins/Submit/mysql_dump.sql
@@ -4,4 +4,5 @@
 
 INSERT INTO vars (name, value, description) VALUES ('submissions_link_relnofollow', '0', 'Add a rel="nofollow" to the submitter\'s vanity link for all submissions?');
 INSERT INTO vars (name, value, description) VALUES ('submit_redirect_submit2', '0', 'Redirect submit to submit2');
+INSERT INTO vars (name, value, description) VALUES ('future_headlines','2','Who to show future headlines to on submissions list page. 0=Everyone, 1=Registered Users, 2=Subscribers');
 
