e7219d1ea557aa2ef86ea56a5bb1ad1668ef2111
TheMightyBuzzard <themightybuzzard@gmail.com>
2014-10-06 08:44:20 -0500

    Added m=story&op=latest to pull the &latest=N or 10 stories.

--- plugins/API/api.pl
+++ plugins/API/api.pl
@@ -83,13 +83,47 @@ sub user {
 	return $ops->{$op}{function}->($form, $slashdb, $user, $constants);
 }
 
+sub story {
+	my ($form, $slashdb, $user, $constants) = @_;
+	my $op = lc($form->{op});
+
+	my $ops = {
+		default		=> {
+			function	=> \&nullop,
+			seclev		=> 1,
+		},
+		latest		=> {
+			function	=> \&getLatestStories,
+			seclev		=> 1,
+		},
+	};
+	return $ops->{$op}{function}->($form, $slashdb, $user, $constants);
+}
+
+sub getLatestStories {
+	my ($form, $slashdb, $user, $constants) = @_;
+	my $options;
+	($options->{limit}, $options->{limit_extra}) = (($form->{limit} || 10), 0);
+	my $stories = $slashdb->getStoriesEssentials($options);
+	foreach my $story (@$stories) {
+		($story->{introtext}, $story->{bodytext}, $story->{title}, $story->{relatedtext}) = $slashdb->sqlSelect("introtext, bodytext, title, relatedtext", "story_text", "stoid = $story->{stoid}");
+		$story->{bodytext} = $story->{introtext} unless $story->{bodytext};
+		$story->{body_length} = length($story->{bodytext});
+		delete $story->{is_future};
+		delete $story->{hitparade};
+		delete $story->{primaryskid};
+	}
+	print STDERR "\n",scalar(@$stories),"\n";
+	my $json = JSON->new->utf8->allow_nonref;	
+	return $json->encode($stories);
+}
+
 sub nullop {
 	return "";
 }
 
 sub maxUid {
 	my ($form, $slashdb, $user, $constants) = @_;
-	my $slashdb = getCurrentDB();
 	my $json = JSON->new->utf8->allow_nonref;
 	my $max = {};
 	$max->{max_uid} = $slashdb->sqlSelect(
@@ -100,7 +134,6 @@ sub maxUid {
 
 sub nameToUid {
 	my ($form, $slashdb, $user, $constants) = @_;
-	my $slashdb = getCurrentDB();
 	my $json = JSON->new->utf8->allow_nonref;
 	my $nick = $slashdb->sqlQuote($form->{nick});
 	my $uid = {};
@@ -113,7 +146,6 @@ sub nameToUid {
 
 sub uidToName {
 	my ($form, $slashdb, $user, $constants) = @_;
-	my $slashdb = getCurrentDB();
 	my $json = JSON->new->utf8->allow_nonref;
 	my $uid = $form->{uid};
 	my $nick = {};
