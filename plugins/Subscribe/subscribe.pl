bd3bc74767664173d86811b1b6ecc727e212e913
paulej72 <ericpaul@princeton.edu>
2015-02-22 14:05:52 -0500

    gor to try something

--- plugins/Subscribe/subscribe.pl
+++ plugins/Subscribe/subscribe.pl
@@ -328,26 +328,26 @@ sub confirm {
 	my($form, $slashdb, $user, $constants) = @_;
 
 	my $type = $form->{subscription_type};
-	
 	my $days = $form->{subscription_days};
 	my $amount;
-	switch($days) {
+	
+	switch ($days) {
 		case ($constants->{subscribe_monthly_days}) {
-			if ($form->{monthly_amount} >= $constants->{subscribe_monthly_amount}){
+			if ($form->{monthly_amount} >= $constants->{subscribe_monthly_amount}) {
 				$amount = $form->{monthly_amount};
 			} else {
 				$amount = $constants->{subscribe_monthly_amount};
 			}
 		}
-		case ($constants->{subscribe_semiannual_days}){
-			if ($form->{semiannual_amount} >= $constants->{subscribe_semiannual_amount}){
+		case ($constants->{subscribe_semiannual_days}) {
+			if ($form->{semiannual_amount} >= $constants->{subscribe_semiannual_amount}) {
 				$amount = $form->{semiannual_amount};
 			} else {
 				$amount = $constants->{subscribe_semiannual_amount};
 			}
 		}
 		else {
-			if ($form->{annual_amount} >= $constants->{subscribe_annual_amount}){
+			if ($form->{annual_amount} >= $constants->{subscribe_annual_amount}) {
 				$amount = $form->{annual_amount};
 			} else {
 				$amount = $constants->{subscribe_annual_amount};
