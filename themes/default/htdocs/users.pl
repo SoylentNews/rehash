ffeb33445250e3a54ba6d9ff61ecfd43fd07d06c
Michael Casadevall <mcasadevall@soylentnews.org>
2015-06-06 23:52:58 +0000

    Remove more bits of cookie_location
    Signed-off-by: Michael Casadevall <mcasadevall@soylentnews.org>

--- themes/default/htdocs/users.pl
+++ themes/default/htdocs/users.pl
@@ -1958,12 +1958,6 @@ sub changePasswd {
 	my $session = $slashdb->getDescriptions('session_login');
 	my $session_select = createSelect('session_login', $session, $user_edit->{session_login}, 1);
 
-	my $clocation = $slashdb->getDescriptions('cookie_location');
-	my @clocation_order = grep { exists $clocation->{$_} } qw(none classbid subnetid ipid);
-	my $clocation_select = createSelect('cookie_location', $clocation,
-		$user_edit->{cookie_location}, 1, 0, \@clocation_order
-	);
-
 	my $got_oldpass = 0;
 	if ($form->{oldpass}) {
 		my $return_uid = $slashdb->getUserAuthenticate($id, $form->{oldpass}, 1);
@@ -1975,7 +1969,6 @@ sub changePasswd {
 		admin_flag		=> $suadmin_flag,
 		title			=> $title,
 		session 		=> $session_select,
-		clocation 		=> $clocation_select,
 		admin_block		=> $admin_block,
 		got_oldpass		=> $got_oldpass
 	});
@@ -2531,7 +2524,6 @@ sub savePasswd {
 	if (! $error_flag) {
 		$user_edits_table->{passwd} = $form->{pass1} if $form->{pass1};
 		$user_edits_table->{session_login} = $form->{session_login};
-		$user_edits_table->{cookie_location} = $form->{cookie_location};
 
 		# changed pass, so delete all logtokens
 		$slashdb->deleteLogToken($form->{uid}, 1);
