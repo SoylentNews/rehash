4d0d38631c05752939e8264d062b408c7c895310
paulej72 <ericpaul@princeton.edu>
2015-06-18 22:22:25 -0400

    balanceTags still sucks ass

--- plugins/Admin/admin.pl
+++ plugins/Admin/admin.pl
@@ -1189,7 +1189,7 @@ sub editStory {
 			$storyref->{$field} = balanceTags($storyref->{$field});
 			$storyref->{$field} =~ s|</p>|</p>\n\n|g;
 			$storyref->{$field} =~ s|</blockquote>|</blockquote>\n\n|g;
-			$storyref->{$field} =~ s|<hr> </p>|</p>\n\n<hr>|g;
+			$storyref->{$field} =~ s|<hr> </p>|<hr>|g;
 		}
 
 		$form->{uid} ||= $user->{uid};
