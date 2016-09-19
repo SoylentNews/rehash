6a3e46030eed95f2b8d43f27d8f8d056e4bac561
mecctro <mecctro@users.noreply.github.com>
2016-09-19 02:40:13 -0400

    Update slashschema_create.sql
    Added non-existent "ajax_ops" table.

--- sql/mysql/slashschema_create.sql
+++ sql/mysql/slashschema_create.sql
@@ -76,6 +76,18 @@ CREATE TABLE accesslog_admin (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS accesslog_artcom;
+CREATE TABLE ajax_ops (
+	id mediumint(5) unsigned NOT NULL AUTO_INCREMENT,
+	op varchar(50) NOT NULL DEFAULT '',
+	class varchar(100) NOT NULL DEFAULT '',
+	subroutine varchar(100) NOT NULL DEFAULT '',
+	reskey_name varchar(64) NOT NULL DEFAULT '',
+	reskey_type varchar(64) NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY op (op)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS ajax_ops;
 CREATE TABLE accesslog_artcom (
 	uid mediumint UNSIGNED NOT NULL,
 	ts datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
