97b2d9d90debb598f550527dbf574fb425b45b5b
TheMightyBuzzard <themightybuzzard@gmail.com>
2016-09-27 14:29:21 +0000

    A rather hacky fix for #265

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -3279,6 +3279,8 @@ sub balanceTags {
 	}
 
 	$html =~ s/\s+$//s;
+	# Hacky fix for #265
+	$html =~ s/>\s+<\/a/><\/a/ig;
 
 	# add on any unclosed tags still on stack
 	$html .= join '', map { "</$_>" } grep { !exists $really_empty{$_} } reverse @stack;
