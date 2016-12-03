2f2f4bff0c5a3739d95c8812ce7ede5783c13dbb
paulej72 <ericpaul@princeton.edu>
2016-12-02 22:46:43 -0500

    Fixes for broken MySQL updates for Submit

---
+++ plugins/Submit/Submit.pm
@@ -0,0 +1,15 @@
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2005 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+
+package Slash::Submit;
+
+use strict;
+use Slash;
+use Slash::Utility;
+
+use base 'Slash::Plugin';
+
+our $VERSION = $Slash::Constants::VERSION;
+
+# This a blank file needed to setup perl lib for MySQL updates.
