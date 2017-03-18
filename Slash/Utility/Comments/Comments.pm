6572fe43aa422a1e26d14ac16f22d67243d2a7cf
paulej72 <ericpaul@princeton.edu>
2017-03-18 15:18:37 -0400

    Move expandAll into its own file

--- Slash/Utility/Comments/Comments.pm
+++ Slash/Utility/Comments/Comments.pm
@@ -2369,35 +2369,7 @@ sub printCommComments {
 		$html_out .= "</div>";
 	}
 		
-	$html_out .= <<'EOF';	
-<script>
-
-function expandAll(cid) {
-    var checks = document.querySelectorAll('#tree_' + cid + ' input[type="checkbox"]');
-    for(var i =0; i< checks.length;i++){
-        var check = checks[i];
-        if(!check.disabled){
-            check.checked = false;
-        }
-    }
-}
-
-function setJSButtonsOn() {  
-    var titles = document.querySelectorAll('.commentTop .title h4.noJS');
-    for(var i =0; i< titles.length;i++){
-        var title = titles[i];
-        title.classList.remove("noJS");
-    }
-
-    var buttons = document.querySelectorAll('.expandAll');
-    for(var i =0; i< buttons.length;i++){
-        var button = buttons[i];
-        button.style.display = 'inline';
-    }
-}
-window.onload = setJSButtonsOn;
-</script>
-EOF
+	$html_out .= "<script src=\"$constants->real_rootdir/expandAll.js\" type=\"text/javascript\"></script>";	
 
 	return $html_out;
 }
---
+++ themes/default/htdocs/expandAll.js
@@ -0,0 +1,24 @@
+function expandAll(cid) {
+    var checks = document.querySelectorAll('#tree_' + cid + ' input[type="checkbox"]');
+    for(var i =0; i< checks.length;i++){
+        var check = checks[i];
+        if(!check.disabled){
+            check.checked = false;
+        }
+    }
+}
+
+function setJSButtonsOn() {  
+    var titles = document.querySelectorAll('.commentTop .title h4.noJS');
+    for(var i =0; i< titles.length;i++){
+        var title = titles[i];
+        title.classList.remove("noJS");
+    }
+
+    var buttons = document.querySelectorAll('.expandAll');
+    for(var i =0; i< buttons.length;i++){
+        var button = buttons[i];
+        button.style.display = 'inline';
+    }
+}
+window.onload = setJSButtonsOn;
