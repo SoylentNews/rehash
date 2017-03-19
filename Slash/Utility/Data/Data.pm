dec09e8480a1876598641351d7380103bdbdfe29
paulej72 <ericpaul@princeton.edu>
2017-03-19 17:45:53 -0400

    try this to fix not getting new cid for each spoiler.

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -1928,11 +1928,23 @@ sub processCustomTagsPost {
 									"<label class=\"spoiler_on\" title=\"Show spoiler\" for=\"spoiler_$id\">*SPOILER* (click to hide)</label>\n" .
 									"<div class=\"spoiler_text\">";
 
-		$str =~ s/$open/$open_new/g;
+		$str =~ s/$open/_newSpoilerHead()/ge;
 		$str =~ s/$close/<\/div><\/blockquote>/g;
 	}
 
 	return $str;
+}
+
+sub _newSpoilerHead {
+	
+	my $id = sprintf("%08X", rand(0xFFFFFFFF));
+		
+		my $open_new = "<blockquote class=\"spoiler\"><input id=\"spoiler_$id\" type=\"checkbox\" class=\"spoiler\" autocomplete=\"off\"/>\n" .
+									"<label class=\"spoiler_off\" title=\"Show spoiler\" for=\"spoiler_$id\">*SPOILER* (click to show)</label>\n" .
+									"<label class=\"spoiler_on\" title=\"Show spoiler\" for=\"spoiler_$id\">*SPOILER* (click to hide)</label>\n" .
+									"<div class=\"spoiler_text\">";
+
+	return $open_new;
 }
 
 sub _nick2Link {
