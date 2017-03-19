a528bc5efa9b73d5867f4c2231e8c96a4109c510
paulej72 <ericpaul@princeton.edu>
2017-03-19 17:03:58 -0400

    try this

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -1923,13 +1923,13 @@ sub processCustomTagsPost {
 		
 		my $id = sprintf("%08X", rand(0xFFFFFFFF));
 		
-		my $open_new = "<div class=\"spoiler\"><input id=\"spoiler_$id\" type=\"checkbox\" class=\"spoiler\" autocomplete=\"off\"/>\n" .
+		my $open_new = "<p class=\"spoiler\"><input id=\"spoiler_$id\" type=\"checkbox\" class=\"spoiler\" autocomplete=\"off\"/>\n" .
 									"<label class=\"spoiler_off\" title=\"Show spoiler\" for=\"spoiler_$id\">*SPOILER* (click to show)</label>\n" .
 									"<label class=\"spoiler_on\" title=\"Show spoiler\" for=\"spoiler_$id\">*SPOILER* (click to hide)</label>\n" .
 									"<div class=\"spoiler_text\">";
 
 		$str =~ s/$open/$open_new/g;
-		$str =~ s/$close/<\/div><\/div>/g;
+		$str =~ s/$close/<\/div><\/p>/g;
 	}
 
 	return $str;
