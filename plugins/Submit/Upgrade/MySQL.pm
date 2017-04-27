a94140de739c51c0ad64486d2c52f1cd79b4cb31
paulej72 <ericpaul@princeton.edu>
2017-04-26 21:38:16 -0400

    change var name

--- plugins/Submit/Upgrade/MySQL.pm
+++ plugins/Submit/Upgrade/MySQL.pm
@@ -41,7 +41,7 @@ sub upgradeDB() {
 	my $subscribe_schema_ver = $schema_versions->{db_schema_plugin_Submit};
 	my $upgrades_done = 0;
 
-	if ($subscribe_schema_ver < 1 ) {
+	if ($submit_schema_ver < 1 ) {
 		print "upgrading Submit to v1 ...\n";
 		if(!$slashdb->sqlDo("ALTER TABLE submissions ADD COLUMN dept varchar (100) null")) {
 			return 0;
@@ -52,11 +52,11 @@ sub upgradeDB() {
 		if (!$slashdb->sqlDo("INSERT INTO site_info (name, value, description) VALUES ('db_schema_plugin_Submit', 1, 'Version of submit plugin schema')")) {
 			return 0;
 		};
-		$subscribe_schema_ver = 1;
+		$submit_schema_ver = 1;
 		$upgrades_done++;
 	}
 	
-	if ($subscribe_schema_ver < 2 ) {
+	if ($submit_schema_ver < 2 ) {
 		print "Upgrading Submit to v2 ...\n";
 		print "Running: ALTER TABLE submissions MODIFY COLUMN note varchar(30) DEFAULT '' NOT NULL \n";
 		if(!$slashdb->sqlDo("ALTER TABLE submissions MODIFY COLUMN note varchar(30) DEFAULT '' NOT NULL")) {
@@ -67,11 +67,11 @@ sub upgradeDB() {
 			return 0;
 		};
 		print "Upgrade complete \n";
-		$subscribe_schema_ver = 2;
+		$submit_schema_ver = 2;
 		$upgrades_done++;
 	}
 	
-		if ($subscribe_schema_ver < 3 ) {
+		if ($submit_schema_ver < 3 ) {
 		print "Upgrading Submit to v3 ...\n";
 		print "Running: REPLACE INTO site_info (name, value, description) VALUES ('submissions_all_page_size', 250, 'Max number of submissions to show for admins and users') \n";
 		if (!$slashdb->sqlDo("REPLACE INTO site_info (name, value, description) VALUES ('submissions_all_page_size', 250, 'Max number of submissions to show for admins and users')")) {
@@ -86,12 +86,12 @@ sub upgradeDB() {
 			return 0;
 		};
 		print "Upgrade complete \n";
-		$subscribe_schema_ver = 3;
+		$submit_schema_ver = 3;
 		$upgrades_done++;
 	}
 
 	if (!$upgrades_done) {
-		print "No upgrades needed for Submit V$subscribe_schema_ver\n";
+		print "No upgrades needed for Submit V$submit_schema_ver\n";
 	}
 
 	return 1;
