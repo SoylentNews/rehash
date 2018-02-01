7d78631a716a0a0e4ef1e03461e6072bd9caac46
TheMightyBuzzard <themightybuzzard@gmail.com>
2018-02-01 00:58:56 +0000

    Still need to move db stuff to our new db updating system

--- plugins/Twitter/tasks/update_twitter.pl
+++ plugins/Twitter/tasks/update_twitter.pl
@@ -1,21 +1,20 @@
 #!/usr/bin/perl
-# $Id$
 
 use strict;
 use warnings;
-use utf8;
 use Slash;
 use Slash::Constants ':slashd';
+use Slash::Twitter;
 use vars qw( %task $me %redirects );
-use Net::Twitter;
 
 $task{$me}{timespec} = '0-59/10 * * * *';
 $task{$me}{timespec_panic_1} = ''; # not that important
 $task{$me}{fork} = SLASHD_NOWAIT;
 $task{$me}{code} = sub {
 	my($virtual_user, $constants, $slashdb, $user, $info, $gSkin) = @_;
+	my $tw = getObject('Slash::Twitter');
 	my $reader = getObject('Slash::DB', { db_type => 'reader' });
-	my $max_items = $constants->{rss_max_items_outgoing} || 10;
+	my $max_items = $constants->{twit_max_items_outgoing} || 10;
 	my $stories = $reader->getBackendStories({ limit => $max_items });
 
 	if ($stories && @$stories) {
@@ -27,69 +26,31 @@ $task{$me}{code} = sub {
 		foreach my $story (@$stories) {
 			# Stories are counted based on a combination of sid and title.
 			# If the title is updated by an Ed, it will get pushed again.
-			my $qtitle = $reader->sqlQuote($story->{title};
+			my $qtitle = $reader->sqlQuote($story->{title});
 			$story->{link} = $reader->getSkin($story->{primaryskid})->{rootdir} . "/article.pl?sid=$story->{sid}";
-			my $count = $reader->sqlSelect(
+			$story->{link} = $constants->{absolutedir_secure} ? "https:$story->{link}" : "http:$story->{link}";
+			my $stale = $reader->sqlSelect(
 				"count(*)",
 				"twitter_log",
-				"sid = $story->{sid} AND title = $qtitle"
+				"sid = \"$story->{sid}\" AND title = $qtitle"
 			);
+			print "stale: $stale\n";
 			# Only want stories we haven't pushed before.
 			# And don't push anything the first time we run.
 			if($totalcount == 0) {
-				log_story_pushed($slashdb, $story, 0);
-			elsif($count > 0) { next; }
+				$tw->log_story_pushed($slashdb, $story, 0);
+			}
+			elsif($stale) { next; }
 			else {
-				log_story_pushed($slashdb, $story, 1) if push_story_to_twitter($constants, $story);
+				$tw->log_story_pushed($slashdb, $story, 1) if $tw->push_story_to_twitter($constants, $story);
 			}
 		}
 	}
 	else { return; }
 
-	trim_stories_table($slashdb, $constants);
+	$tw->trim_stories_table($slashdb, $constants);
 
 	return;
 };
-
-sub push_story_to_twitter() {
-	my ($constants, $story) = @_;
-	my $nt = Net::Twitter->new(
-		traits => ['API::RESTv1_1', 'OAuth'],
-		consumer_key => $constants->{twit_consumer_key},
-		consumer_secret => $constants->{twit_consumer_secret},
-		access_token => $constants->{twit_access_token},
-		access_token_secret => $constants->{twit_access_token_secret},
-	);
-	if(! $nt->authorized ) {
-		print STDERR "push_story_to_twitter(): Not authorized\n";
-		return 0;
-	}
-	$nt->update("$story->{title} - $$story->{link}");
-	return 1;
-}
-
-sub log_story_pushed() {
-	my ($slashdb, $story, $shown) = @_;
-	my $data = {
-		sid		=> $story->{sid},
-		title	=> $story->{title},
-		time		=> "NOW()",
-		shown	=> $shown == 0 ? "FALSE" : "TRUE"
-	};
-	my $rows = $slashdb->sqlInsert(
-		"twitter_log",
-		$data
-	);
-	if($rows == 0) {
-		print "Failed to log sid $story->{sid} as pushed to Twitter.\n";
-	}
-	return;
-}
-
-sub trim_stories_table() {
-	my ($slashdb, $constants) = @_;
-	$slashdb->sqlDo("DELETE FROM twitter_log WHERE time < DATE_SUB(NOW(), INTERVAL $constants->{discussion_archive_delay} DAY)");
-	return;
-}
 
 1;
