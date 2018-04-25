d32ce7488c63087d0ae5652519610b3fafa30f96
TheMightyBuzzard <themightybuzzard@gmail.com>
2018-04-25 11:30:44 -0500

    Stupid copy pasta error

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -7876,9 +7876,9 @@ sub getNetIDKarma {
 	my ($fulldays, $halfdays, $quarterdays) = ($constants->{bad_karma_full_weight}, $constants->{bad_karma_half_weight}, $constants->{bad_karma_quarter_weight});
 
 	my $positive = "select ifnull(sum(karma), 0) from comments where $typeclause and karma > 0";
-	my $fullweight = "select ifnull(sum(karma), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $fulldays day)";
-	my $halfweight = "select ifnull(cast(sum(karma) / 2 as signed), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $halfdays day) and date < (now() - interval $fulldays day)";
-	my $quarterweight = "select ifnull(cast(sum(karma) / 4 as signed), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $quarterdays day) and date < (now() - interval $halfdays day)";
+	my $fullweight = "select ifnull(sum(karma), 0) from comments where $typeclause and karma < 0 and date >= now() - interval $fulldays day";
+	my $halfweight = "select ifnull(cast(sum(karma) / 2 as signed), 0) from comments where $typeclause and karma < 0 and date >= now() - interval $halfdays day and date < now() - interval $fulldays day";
+	my $quarterweight = "select ifnull(cast(sum(karma) / 4 as signed), 0) from comments where $typeclause and karma < 0 and date >= now() - interval $quarterdays day and date < now() - interval $halfdays day";
 	my $karmaclause = "($positive) + ($fullweight) + ($halfweight) + ($quarterweight)";
 	my $what = "count(*) as 'count', ($karmaclause) as karma";
 
@@ -13783,7 +13783,7 @@ sub upgradeCoreDB() {
 			return 0;
 		}
 		print "Running: REPLACE INTO vars VALUES ('bad_karma_half_weight', 14, 'Number of days comments negative karma should have half value for ipid/subnetid')\n";
-		if(!$self->sqlDo("REPLACE INTO vars VALUES ('bad_karma_full_weight', 14, 'Number of days comments negative karma should have half value for ipid/subnetid')")) {
+		if(!$self->sqlDo("REPLACE INTO vars VALUES ('bad_karma_half_weight', 14, 'Number of days comments negative karma should have half value for ipid/subnetid')")) {
 			return 0;
 		}
 		print "Running: REPLACE INTO vars VALUES ('bad_karma_quarter_weight', 30, 'Number of days comments negative karma should have quarter value for ipid/subnetid')\n";
