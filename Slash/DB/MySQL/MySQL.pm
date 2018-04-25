248d9501095d8f949e94b52d26bc5d49933f2815
TheMightyBuzzard <themightybuzzard@gmail.com>
2018-04-25 10:53:37 -0500

    Stupid "days" vs "day" error

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -7876,9 +7876,9 @@ sub getNetIDKarma {
 	my ($fulldays, $halfdays, $quarterdays) = ($constants->{bad_karma_full_weight}, $constants->{bad_karma_half_weight}, $constants->{bad_karma_quarter_weight});
 
 	my $positive = "select ifnull(sum(karma), 0) from comments where $typeclause and karma > 0";
-	my $fullweight = "select ifnull(sum(karma), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $fulldays days)";
-	my $halfweight = "select ifnull(cast(sum(karma) / 2 as signed), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $halfdays days) and date < (now() - interval $fulldays days)";
-	my $quarterweight = "select ifnull(cast(sum(karma) / 4 as signed), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $quarterdays days) and date < (now() - interval $halfdays days)";
+	my $fullweight = "select ifnull(sum(karma), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $fulldays day)";
+	my $halfweight = "select ifnull(cast(sum(karma) / 2 as signed), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $halfdays day) and date < (now() - interval $fulldays day)";
+	my $quarterweight = "select ifnull(cast(sum(karma) / 4 as signed), 0) from comments where $typeclause and karma < 0 and date >= (now() - interval $quarterdays day) and date < (now() - interval $halfdays day)";
 	my $karmaclause = "($positive) + ($fullweight) + ($halfweight) + ($quarterweight)";
 	my $what = "count(*) as 'count', ($karmaclause) as karma";
 
